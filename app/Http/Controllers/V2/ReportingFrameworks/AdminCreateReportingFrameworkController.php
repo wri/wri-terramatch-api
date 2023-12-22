f3e77635b880b108b068365ca645b358af305dfc
= <=>
2023-12-22 15:53:20 +0000

    feat: add and remove framework_key model from form table

--- app/Http/Controllers/V2/ReportingFrameworks/AdminCreateReportingFrameworkController.php
+++ app/Http/Controllers/V2/ReportingFrameworks/AdminCreateReportingFrameworkController.php
@@ -13,10 +13,19 @@ use Spatie\Permission\Models\Permission;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Artisan;
+use App\Models\V2\Forms\Form;
+use App\Models\V2\Nurseries\Nursery;
+use App\Models\V2\Nurseries\NurseryReport;
+use App\Models\V2\Projects\Project;
+use App\Models\V2\Projects\ProjectReport;
+use App\Models\V2\Sites\Site;
+use App\Models\V2\Sites\SiteReport;
 
 
 class AdminCreateReportingFrameworkController extends Controller {
    public function __invoke(CreateReportingFrameworkRequest $frameworkRequest): ReportingFrameworkResource {
+       
+        // $this->authorize('create', Framework::class);
 
        $framework = Framework::create([
            'name' => $frameworkRequest->name,
@@ -47,8 +56,58 @@ class AdminCreateReportingFrameworkController extends Controller {
         ['permission_id' => $PermissionAdded->id, 'role_id' => $adminTerrafund],
        ]);
 
+        Form::isUuid($frameworkRequest->project_form_uuid)->update([
+            'framework_key' => $framework->slug,
+            'model' => Project::class,
+        ]);
+        Form::isUuid($frameworkRequest->project_report_form_uuid)->update([
+            'framework_key' => $framework->slug,
+            'model' => ProjectReport::class,
+        ]);
+        Form::isUuid($frameworkRequest->site_form_uuid)->update([
+            'framework_key' => $framework->slug,
+            'model' => Site::class,
+        ]);
+        Form::isUuid($frameworkRequest->site_report_form_uuid)->update([
+            'framework_key' => $framework->slug,
+            'model' => SiteReport::class,
+        ]);
+        Form::isUuid($frameworkRequest->nursery_form_uuid)->update([
+            'framework_key' => $framework->slug,
+            'model' => Nursery::class,
+        ]);
+        Form::isUuid($frameworkRequest->nursery_report_form_uuid)->update([
+            'framework_key' => $framework->slug,
+            'model' => NurseryReport::class,
+        ]);
+
+        $this->detachOldForms($framework);
+
+
        Artisan::call('cache:clear');
 
        return new ReportingFrameworkResource($framework);
+   }
+
+   private function detachOldForms(Framework $framework)
+   {
+       $fields = ['project_form_uuid', 'project_report_form_uuid', 'site_form_uuid', 'site_report_form_uuid', 'nursery_form_uuid', 'nursery_report_form_uuid'];
+
+       $uuids = [];
+       foreach ($fields as $field) {
+           if (! empty($framework->$field)) {
+               $uuids[] = $framework->$field;
+           }
+       }
+
+       $toClear = Form::whereNotIn('uuid', $uuids)
+           ->where('framework_key', $framework->slug)
+           ->get();
+
+       foreach ($toClear as $record) {
+           $record->framework_key = null;
+           $record->model = null;
+           $record->save();
+       }
    }
 }
--- app/Http/Controllers/V2/ReportingFrameworks/AdminDeleteReportingFrameworkController.php
+++ app/Http/Controllers/V2/ReportingFrameworks/AdminDeleteReportingFrameworkController.php
@@ -13,6 +13,13 @@ use Spatie\Permission\Models\Permission;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Artisan;
+use App\Models\V2\Forms\Form;
+use App\Models\V2\Nurseries\Nursery;
+use App\Models\V2\Nurseries\NurseryReport;
+use App\Models\V2\Projects\Project;
+use App\Models\V2\Projects\ProjectReport;
+use App\Models\V2\Sites\Site;
+use App\Models\V2\Sites\SiteReport;
 
 class AdminDeleteReportingFrameworkController extends Controller {
     public function __invoke(String $uuid)
@@ -27,6 +34,31 @@ class AdminDeleteReportingFrameworkController extends Controller {
 
         // Remove role_has_permissions entries
         DB::table('role_has_permissions')->where('permission_id', $permissionToDelete->id)->delete();
+
+        Form::isUuid($frameworkToDelete->project_form_uuid)->update([
+            'framework_key' => null,
+            'model' => null,
+        ]);
+        Form::isUuid($frameworkToDelete->project_report_form_uuid)->update([
+            'framework_key' => null,
+            'model' => null,
+        ]);
+        Form::isUuid($frameworkToDelete->site_form_uuid)->update([
+            'framework_key' => null,
+            'model' => null,
+        ]);
+        Form::isUuid($frameworkToDelete->site_report_form_uuid)->update([
+            'framework_key' => null,
+            'model' => null,
+        ]);
+        Form::isUuid($frameworkToDelete->nursery_form_uuid)->update([
+            'framework_key' => null,
+            'model' => null,
+        ]);
+        Form::isUuid($frameworkToDelete->nursery_report_form_uuid)->update([
+            'framework_key' => null,
+            'model' => null,
+        ]);
 
         Log::info("Removed $frameworkToDelete->name reporting framework $uuid.");
 
