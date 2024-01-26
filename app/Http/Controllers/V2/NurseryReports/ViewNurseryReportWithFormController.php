668f7964d486078aa6816b15d85674db83e61917
Jose Carlos Laura Ramirez <pachonjcl@gmail.com>
2024-01-26 18:58:19 +0000

    fix: add lang query params usage to other for routes

--- app/Http/Controllers/V2/NurseryReports/ViewNurseryReportWithFormController.php
+++ app/Http/Controllers/V2/NurseryReports/ViewNurseryReportWithFormController.php
@@ -7,12 +7,17 @@ use App\Http\Resources\V2\NurseryReports\NurseryReportWithSchemaResource;
 use App\Models\V2\Forms\Form;
 use App\Models\V2\Nurseries\NurseryReport;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\App;
 
 class ViewNurseryReportWithFormController extends Controller
 {
     public function __invoke(Request $request, NurseryReport $nurseryReport): NurseryReportWithSchemaResource
     {
         $this->authorize('read', $nurseryReport);
+
+        if ($request->query('lang')) {
+            App::setLocale($request->query('lang'));
+        }
 
         $schema = Form::where('framework_key', $nurseryReport->framework_key)
             ->where('model', NurseryReport::class)
--- app/Http/Controllers/V2/SiteReports/ViewSiteReportWithFormController.php
+++ app/Http/Controllers/V2/SiteReports/ViewSiteReportWithFormController.php
@@ -7,12 +7,17 @@ use App\Http\Resources\V2\SiteReports\SiteReportWithSchemaResource;
 use App\Models\V2\Forms\Form;
 use App\Models\V2\Sites\SiteReport;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\App;
 
 class ViewSiteReportWithFormController extends Controller
 {
     public function __invoke(Request $request, SiteReport $siteReport): SiteReportWithSchemaResource
     {
         $this->authorize('read', $siteReport);
+
+        if ($request->query('lang')) {
+            App::setLocale($request->query('lang'));
+        }
 
         $schema = Form::where('framework_key', $siteReport->framework_key)
             ->where('model', SiteReport::class)
