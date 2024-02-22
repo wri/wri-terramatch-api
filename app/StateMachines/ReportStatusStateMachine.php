a3729a5f7819607a09eaff97bb2b9c367d205cfc
Nathan Curtis <nathan.curtis@gmail.com>
2024-02-22 14:21:48 -0800

    [TM-561] Switch back to using the nothing_to_report flag.

--- app/StateMachines/ReportStatusStateMachine.php
+++ app/StateMachines/ReportStatusStateMachine.php
@@ -8,7 +8,6 @@ class ReportStatusStateMachine extends StateMachine
 {
     public const DUE = 'due';
     public const STARTED = 'started';
-    public const NOTHING_TO_REPORT = 'nothing-to-report';
     public const AWAITING_APPROVAL = 'awaiting-approval';
     public const NEEDS_MORE_INFORMATION = 'needs-more-information';
     public const APPROVED = 'approved';
@@ -20,18 +19,12 @@ class ReportStatusStateMachine extends StateMachine
 
     public function transitions(): array
     {
-        // TODO (NJC):
-        //   is started -> nothing-to-report valid?
-
-        $states = [
-            self::DUE => [self::STARTED, self::NOTHING_TO_REPORT],
-            self::STARTED => [self::NOTHING_TO_REPORT, self::AWAITING_APPROVAL],
+        return [
+            self::DUE => [self::STARTED],
+            self::STARTED => [self::AWAITING_APPROVAL],
             self::AWAITING_APPROVAL => [self::APPROVED, self::NEEDS_MORE_INFORMATION],
             self::NEEDS_MORE_INFORMATION => [self::APPROVED],
         ];
-
-        $filterToStates = fn ($toStates) => array_filter($toStates, $this->model->isStatusStateSupported(...));
-        return array_map($filterToStates, $states);
     }
 
     public function defaultState(): ?string
