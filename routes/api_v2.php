e1ffa2fa56f6366d3146baa57357408a53504876
Limber Mamani <limbermv2@gmail.com>
2024-02-23 16:29:24 -0400

    fix: TM-665 remove files from other branch

--- routes/api_v2.php
+++ routes/api_v2.php
@@ -15,7 +15,6 @@ use App\Http\Controllers\V2\BaselineMonitoring\BaselineMonitoringSiteController;
 use App\Http\Controllers\V2\CoreTeamLeader\DeleteCoreTeamLeaderController;
 use App\Http\Controllers\V2\CoreTeamLeader\StoreCoreTeamLeaderController;
 use App\Http\Controllers\V2\CoreTeamLeader\UpdateCoreTeamLeaderController;
-use App\Http\Controllers\V2\Dashboard\TotalTerrafundHeaderDashboardController;
 use App\Http\Controllers\V2\Dashboard\VolunteersAndAverageSurvivalRateController;
 use App\Http\Controllers\V2\Disturbances\DeleteDisturbanceController;
 use App\Http\Controllers\V2\Disturbances\GetDisturbancesForEntityController;
@@ -683,6 +682,5 @@ Route::resource('files', FilePropertiesController::class);
 //Route::delete('file/{uuid}', [FilePropertiesController::class, 'destroy']);
 
 Route::prefix('dashboard')->group(function () {
-    Route::get('/total-section-header', TotalTerrafundHeaderDashboardController::class);
     Route::get('/volunteers-survival-rate', VolunteersAndAverageSurvivalRateController::class);
 });
