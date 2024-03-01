fc625c783b2bbef070fb62f789c2986f37256b0e
Limber Mamani <limbermv2@gmail.com>
2024-03-01 10:47:09 -0400

    fix: logic improved

--- app/Helpers/TerrafundDashboardQueryHelper.php
+++ app/Helpers/TerrafundDashboardQueryHelper.php
@@ -8,18 +8,19 @@ class TerrafundDashboardQueryHelper
 {
     public static function buildQueryFromRequest($request)
     {
-        $projects = Project::where('framework_key', 'terrafund')
-            ->whereHas('organisation', function ($query) {
-                $query->whereIn('type', ['for-profit-organization', 'non-profit-organization']);
-            });
+        $query = Project::query();
+        $query = $query->where('framework_key', 'terrafund');
+        $query = $query->whereHas('organisation', function ($query) {
+            $query->whereIn('type', ['for-profit-organization', 'non-profit-organization']);
+        });
         if ($request->has('country')) {
             $country = $request->input('country');
-            $projects->where('country', $country);
+            $query->where('country', $country);
         } elseif ($request->has('uuid')) {
             $projectId = $request->input('uuid');
-            $projects->where('uuid', $projectId);
+            $query->where('uuid', $projectId);
         }
 
-        return $projects;
+        return $query;
     }
 }
