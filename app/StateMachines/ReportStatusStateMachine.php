fbe976a8bfca1ddafb11f437863322703f0e949a
Nathan Curtis <nathan.curtis@gmail.com>
2024-03-06 13:29:59 -0800

    [TM-715] Allow writing a report that had previously been marked as nothing_to_report.

--- app/StateMachines/ReportStatusStateMachine.php
+++ app/StateMachines/ReportStatusStateMachine.php
@@ -11,11 +11,14 @@ class ReportStatusStateMachine extends EntityStatusStateMachine
 
     public function transitions(): array
     {
+        $parentTransitions = parent::transitions();
+        // Reports can go from awaiting approval to started if the nothing_to_report flag is true (see validations below)
+        $parentTransitions[self::AWAITING_APPROVAL][] = self::STARTED;
         return array_merge(
             [
                 self::DUE => [self::STARTED, self::AWAITING_APPROVAL],
             ],
-            parent::transitions()
+            $parentTransitions
         );
     }
 
@@ -26,7 +29,10 @@ class ReportStatusStateMachine extends EntityStatusStateMachine
 
     public function validatorForTransition($from, $to, $model): ?Validator
     {
-        if ($from === self::DUE && $to === self::AWAITING_APPROVAL) {
+        if (
+            ($from === self::DUE && $to === self::AWAITING_APPROVAL) ||
+            ($from === self::AWAITING_APPROVAL && $to === self::STARTED)
+        ) {
             return ValidatorFacade::make([
                 'nothing_to_report' => $model->nothing_to_report,
             ], [
