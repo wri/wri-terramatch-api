bac7bb9ce0aaccb0eb3a53cf803237f259f6d430
Nathan Curtis <nathan.curtis@gmail.com>
2024-03-27 15:37:33 -0700

    [TM-702] Fix report project/org relationships.

--- app/Models/V2/Sites/SiteReport.php
+++ app/Models/V2/Sites/SiteReport.php
@@ -14,7 +14,9 @@ use App\Models\Traits\HasV2MediaCollections;
 use App\Models\Traits\UsesLinkedFields;
 use App\Models\V2\Disturbance;
 use App\Models\V2\Invasive;
+use App\Models\V2\Organisation;
 use App\Models\V2\Polygon;
+use App\Models\V2\Projects\Project;
 use App\Models\V2\ReportModel;
 use App\Models\V2\Seeding;
 use App\Models\V2\Tasks\Task;
@@ -35,6 +37,8 @@ use OwenIt\Auditing\Contracts\Auditable as AuditableContract;
 use Spatie\MediaLibrary\HasMedia;
 use Spatie\MediaLibrary\InteractsWithMedia;
 use Spatie\MediaLibrary\MediaCollections\Models\Media;
+use Znck\Eloquent\Relations\BelongsToThrough;
+use Znck\Eloquent\Traits\BelongsToThrough as BelongsToThroughTrait;
 
 class SiteReport extends Model implements HasMedia, AuditableContract, ReportModel
 {
@@ -51,6 +55,7 @@ class SiteReport extends Model implements HasMedia, AuditableContract, ReportMod
     use Auditable;
     use HasUpdateRequests;
     use HasEntityResources;
+    use BelongsToThroughTrait;
 
     protected $auditInclude = [
         'status',
@@ -158,9 +163,13 @@ class SiteReport extends Model implements HasMedia, AuditableContract, ReportMod
         return $this->belongsTo(Site::class);
     }
 
-    public function project(): BelongsTo
+    public function project(): BelongsToThrough
     {
-        return empty($this->site) ? $this->site : $this->site->project();
+        return $this->belongsToThrough(
+            Project::class,
+            Site::class,
+            foreignKeyLookup: [Project::class => 'project_id', Site::class => 'site_id']
+        );
     }
 
     public function task(): BelongsTo
@@ -168,9 +177,13 @@ class SiteReport extends Model implements HasMedia, AuditableContract, ReportMod
         return $this->belongsTo(Task::class);
     }
 
-    public function organisation(): BelongsTo
+    public function organisation(): BelongsToThrough
     {
-        return  empty($this->project) ? $this->project : $this->project->organisation();
+        return $this->belongsToThrough(
+            Organisation::class,
+            [Project::class, Site::class],
+            foreignKeyLookup: [Project::class => 'project_id', Site::class => 'site_id']
+        );
     }
 
     public function polygons(): MorphMany
@@ -306,8 +319,8 @@ class SiteReport extends Model implements HasMedia, AuditableContract, ReportMod
     public function toSearchableArray()
     {
         return [
-            'project_name' => $this->site->project->name,
-            'organisation_name' => $this->organisation->name,
+            'project_name' => $this->project?->name,
+            'organisation_name' => $this->organisation?->name,
         ];
     }
 
