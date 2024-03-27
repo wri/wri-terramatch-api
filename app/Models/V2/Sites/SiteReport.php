292df7970db20d3b0f7e2862008be2385405fb15
Nathan Curtis <nathan.curtis@gmail.com>
2024-03-27 12:42:34 -0700

    [TM-748] re-instate ability to put site reports into the nothing to report state.

--- app/Models/V2/Sites/SiteReport.php
+++ app/Models/V2/Sites/SiteReport.php
@@ -351,6 +351,11 @@ class SiteReport extends Model implements HasMedia, AuditableContract, ReportMod
         return new SiteReportResource($this);
     }
 
+    public function supportsNothingToReport(): bool
+    {
+        return true;
+    }
+
     public function parentEntity(): BelongsTo
     {
         return $this->site();
