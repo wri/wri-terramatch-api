b7e704ef74495af44578d7c64675c6c33cd061c2
Nathan Curtis <nathan.curtis@gmail.com>
2024-04-10 13:43:22 -0700

    [TM-812] Streamline the process of updating from form values.

--- app/Models/V2/EntityModel.php
+++ app/Models/V2/EntityModel.php
@@ -19,7 +19,7 @@ interface EntityModel extends UpdateRequestableModel, ApprovalFlow
 
     public function getFormConfig(): ?array;
 
-    public function mapEntityAnswers(array $input, Form $form, array $cfg): array;
+    public function updateFromForm(array $formValues): array;
 
     public function createResource(): JsonResource;
 
