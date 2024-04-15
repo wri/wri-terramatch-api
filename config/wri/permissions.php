3a466c3e62192bb761cff630040bca3bb760039f
Nathan Curtis <nathan.curtis@gmail.com>
2024-04-15 13:23:41 -0700

    [TM-812] Create permissions for the new terrafund-enterprises framework.

--- config/wri/permissions.php
+++ config/wri/permissions.php
@@ -3,6 +3,7 @@
 return [
     'framework-ppc' => 'Framework PPC',
     'framework-terrafund' => 'Framework Terrafund',
+    'framework-terrafund-enterprises' => 'Framework Terrafund Enterprises',
     'custom-forms-manage' => 'Manage custom forms',
     'users-manage' => 'Manage users',
     'monitoring-manage' => 'Manage monitoring',
