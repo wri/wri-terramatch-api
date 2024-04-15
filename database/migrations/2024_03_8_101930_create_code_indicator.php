a66b7b2cf492cd5b9fce9838c556a7ed8d1fd636
Jose Carlos Laura Ramirez <pachonjcl@gmail.com>
2024-04-15 17:11:41 -0400

    move code from migration to seeder

--- database/migrations/2024_03_8_101930_create_code_indicator.php
+++ database/migrations/2024_03_8_101930_create_code_indicator.php
@@ -26,52 +26,6 @@ class CreateCodeIndicator extends Migration
         $table->softDeletes();
         $table->timestamps();
       });
-      $indicators = [
-        [
-            'name' => 'Tree cover (TTC)',
-            'unit' => 'Percent',
-            'description' => 'Percent tree cover of each site polygon',
-            'is_active' => 1
-        ],
-        [
-            'name' => 'Tree cover loss',
-            'unit' => 'Ha',
-            'description' => 'Tree cover loss in hectares',
-            'is_active' => 1
-        ],
-        [
-            'name' => 'Tree cover loss from fires',
-            'unit' => 'Ha',
-            'description' => 'Tree cover loss from fires in hectares',
-            'is_active' => 1
-        ],
-        [
-            'name' => 'Hectares under restoration by WWF ecoregion',
-            'unit' => 'Ha',
-            'description' => 'Area value for each ecoregion type',
-            'is_active' => 1
-        ],
-        [
-            'name' => 'Hectares under restoration by intervention type',
-            'unit' => 'Ha',
-            'description' => 'Area value for each intervention type',
-            'is_active' => 1
-        ],
-        [
-            'name' => 'Tree count',
-            'unit' => 'Count',
-            'description' => 'Tree count number and confidence value',
-            'is_active' => 1
-        ],
-      ];
-      $now = now(); 
-      foreach ($indicators as $indicator) {
-          $indicator['uuid'] = Str::uuid();
-          $indicator['uuid_primary'] = $indicator['uuid'];
-          $indicator['created_at'] = $now;
-          $indicator['updated_at'] = $now;
-          DB::table('code_indicator')->insert($indicator);
-      }
     }
 
     /**
