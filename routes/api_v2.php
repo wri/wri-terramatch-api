9937a614c48c3a282cd96ce34b8e0ed75f40bbd0
Nathan Curtis <nathan.curtis@gmail.com>
2024-04-17 14:20:00 -0700

    [TM-803] Make the model interface binding middleware a bit more generically useful.

--- routes/api_v2.php
+++ routes/api_v2.php
@@ -189,6 +189,7 @@ use App\Http\Controllers\V2\Workdays\GetWorkdaysForEntityController;
 use App\Http\Controllers\V2\Workdays\StoreWorkdayController;
 use App\Http\Controllers\V2\Workdays\UpdateWorkdayController;
 use App\Http\Middleware\ModelInterfaceBindingMiddleware;
+use App\Models\V2\EntityModel;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -292,13 +293,10 @@ Route::prefix('admin')->middleware(['admin'])->group(function () {
 
     Route::get('/{entity}/export/{framework}', ExportAllMonitoredEntitiesController::class);
 
-    Route::prefix('{modelSlug}')
-        ->whereIn('modelSlug', ModelInterfaceBindingMiddleware::ENTITY_TYPES_PLURAL)
-        ->middleware('modelInterface')
-        ->group(function () {
-            Route::put('/{entity}/{status}', AdminStatusEntityController::class);
-            Route::delete('/{entity}', AdminSoftDeleteEntityController::class);
-        });
+    ModelInterfaceBindingMiddleware::with(EntityModel::class, function () {
+        Route::put('/{entity}/{status}', AdminStatusEntityController::class);
+        Route::delete('/{entity}', AdminSoftDeleteEntityController::class);
+    });
 
     Route::get('nursery-reports', AdminIndexNurseryReportsController::class);
     Route::get('site-reports', AdminIndexSiteReportsController::class);
@@ -405,14 +403,11 @@ Route::prefix('forms')->group(function () {
     Route::get('/', IndexFormController::class);
     Route::get('/{form}', ViewFormController::class)->middleware('i18n');
 
-    Route::prefix('{modelSlug}')
-        ->whereIn('modelSlug', ModelInterfaceBindingMiddleware::ENTITY_TYPES_PLURAL)
-        ->middleware('modelInterface')
-        ->group(function () {
-            Route::get('/{entity}', ViewEntityWithFormController::class)->middleware('i18n');
-            Route::put('/{entity}', UpdateEntityWithFormController::class);
-            Route::put('/{entity}/submit', SubmitEntityWithFormController::class);
-        });
+    ModelInterfaceBindingMiddleware::with(EntityModel::class, function () {
+        Route::get('/{entity}', ViewEntityWithFormController::class)->middleware('i18n');
+        Route::put('/{entity}', UpdateEntityWithFormController::class);
+        Route::put('/{entity}/submit', SubmitEntityWithFormController::class);
+    });
 
     Route::prefix('projects')->group(function () {
         Route::post('', CreateProjectWithFormController::class);
@@ -545,12 +540,9 @@ Route::prefix('{modelSlug}')
         Route::put('/{report}/nothing-to-report', NothingToReportReportController::class);
     });
 
-Route::prefix('{modelSlug}')
-    ->whereIn('modelSlug', ModelInterfaceBindingMiddleware::ENTITY_TYPES_PLURAL)
-    ->middleware('modelInterface')
-    ->group(function () {
-        Route::get('/{entity}', ViewEntityController::class);
-    });
+ModelInterfaceBindingMiddleware::with(EntityModel::class, function () {
+    Route::get('/{entity}', ViewEntityController::class);
+});
 
 Route::prefix('project-reports')->group(function () {
     Route::get('/{projectReport}/files', ViewProjectReportGalleryController::class);
@@ -606,12 +598,9 @@ Route::prefix('update-requests')->group(function () {
     Route::get('/{updateRequest}', AdminViewUpdateRequestController::class);
     Route::delete('/{updateRequest}', AdminSoftDeleteUpdateRequestController::class);
 
-    Route::prefix('/{modelSlug}')
-        ->whereIn('modelSlug', ModelInterfaceBindingMiddleware::ENTITY_TYPES_SINGULAR)
-        ->middleware('modelInterface')
-        ->group(function () {
-            Route::get('/{entity}', EntityUpdateRequestsController::class);
-        });
+    ModelInterfaceBindingMiddleware::with(EntityModel::class, function () {
+        Route::get('/{entity}', EntityUpdateRequestsController::class);
+    });
 });
 
 Route::get('/funding-programme', [FundingProgrammeController::class, 'index'])->middleware('i18n');
