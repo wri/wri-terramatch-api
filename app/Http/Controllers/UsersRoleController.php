708553c4daf668997f09bc5bd5f4fb488fa50e1f
Limber Mamani <limbermv2@gmail.com>
2024-04-19 13:46:02 -0400

    feat: add UserRoleValidator

--- app/Http/Controllers/UsersRoleController.php
+++ app/Http/Controllers/UsersRoleController.php
@@ -6,7 +6,7 @@ use App\Helpers\JsonResponseHelper;
 use App\Jobs\UserVerificationJob;
 use App\Models\User as UserModel;
 use App\Resources\UserResource;
-use App\Validators\UserValidator;
+use App\Validators\UserRoleValidator;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
@@ -20,7 +20,7 @@ class UsersRoleController extends Controller
         $url = $request->get('callback_url') ? $request->get('callback_url') : null;
         $request->request->remove('callback_url');
         $data = $request->json()->all();
-        UserValidator::validate('CREATE', $data);
+        UserRoleValidator::validate('CREATE', $data);
         $data['role'] = 'user';
         $user = new UserModel($data);
         $user->saveOrFail();
---
+++ app/Validators/UserRoleValidator.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Validators;
+
+class UserRoleValidator extends Validator
+{
+    public const INVITE = [
+        'email_address' => 'required|string|email|between:1,255|unique:users,email_address',
+        'role' => 'in:user,terrafund_admin',
+    ];
+
+    /**
+     * This property deliberately omits the avatar validation rules. The upload
+     * endpoint cannot be used until you're logged in. It wouldn't make sense to
+     * require it here.
+     */
+    public const CREATE = [
+        'first_name' => 'required|string|between:1,255',
+        'last_name' => 'required|string|between:1,255',
+        'email_address' => 'required|string|email|between:1,255|unique:users,email_address',
+        'password' => 'required|string|min:8|contain_upper|contain_lower|contain_number',
+        'job_role' => 'required|string|between:1,255',
+        'role_id' => 'required|string|between:1,255',
+        'facebook' => 'sometimes|nullable|string|soft_url|starts_with_facebook|between:1,255',
+        'twitter' => 'sometimes|nullable|string|soft_url|starts_with_twitter|between:1,255',
+        'linkedin' => 'sometimes|nullable|string|soft_url|starts_with_linkedin|between:1,255',
+        'instagram' => 'sometimes|nullable|string|soft_url|starts_with_instagram|between:1,255',
+        'phone_number' => 'required|string|between:1,255',
+        'whatsapp_phone' => 'sometimes|nullable|string|between:1,255',
+    ];
+
+    /**
+     * This property deliberately omits the avatar validation rules. The upload
+     * endpoint cannot be used until you're logged in. It wouldn't make sense to
+     * require it here.
+     */
+    public const ACCEPT = [
+        'first_name' => 'required|string|between:1,255',
+        'last_name' => 'required|string|between:1,255',
+        'email_address' => 'required|string|email|exists:users,email_address',
+        'password' => 'required|string|min:8|contain_upper|contain_lower|contain_number',
+        'job_role' => 'required|string|between:1,255',
+        'role_id' => 'required|string|between:1,255',
+        'facebook' => 'sometimes|nullable|string|soft_url|starts_with_facebook|between:1,255',
+        'twitter' => 'sometimes|nullable|string|soft_url|starts_with_twitter|between:1,255',
+        'linkedin' => 'sometimes|nullable|string|soft_url|starts_with_linkedin|between:1,255',
+        'instagram' => 'sometimes|nullable|string|soft_url|starts_with_instagram|between:1,255',
+        'phone_number' => 'required|string|between:1,255',
+        'whatsapp_phone' => 'sometimes|string|between:1,255',
+    ];
+
+    public const UPDATE = [
+        'first_name' => 'sometimes|required|string|between:1,255',
+        'last_name' => 'sometimes|required|string|between:1,255',
+        'email_address' => 'sometimes|required|string|email|between:1,255',
+        'password' => 'sometimes|required|string|min:10|contain_upper|contain_lower|contain_number',
+        'job_role' => 'sometimes|required|string|between:1,255',
+        'role_id' => 'required|string|between:1,255',
+        'facebook' => 'sometimes|present|nullable|string|soft_url|starts_with_facebook|between:1,255',
+        'twitter' => 'sometimes|present|nullable|string|soft_url|starts_with_twitter|between:1,255',
+        'linkedin' => 'sometimes|present|nullable|string|soft_url|starts_with_linkedin|between:1,255',
+        'instagram' => 'sometimes|present|nullable|string|soft_url|starts_with_instagram|between:1,255',
+        'avatar' => 'sometimes|present|nullable|integer|exists:uploads,id',
+        'phone_number' => 'sometimes|required|string|between:1,255',
+        'whatsapp_phone' => 'sometimes|string|between:1,255',
+    ];
+
+    public const UPDATE_ROLE = [
+        'role' => 'required|string|in:user,terrafund_admin',
+    ];
+
+    public const RESEND = [
+        'uuid' => 'required|string',
+    ];
+}
