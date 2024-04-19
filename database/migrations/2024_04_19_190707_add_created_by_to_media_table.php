22e866b9b044e55d3c17b44106f14daa81eb814c
Nathan Curtis <nathan.curtis@gmail.com>
2024-04-19 12:14:09 -0700

    [TM-804] Store the created_by user id when uploading media.

---
+++ database/migrations/2024_04_19_190707_add_created_by_to_media_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use App\Models\V2\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('media', function (Blueprint $table) {
+            $table->foreignIdFor(User::class, 'created_by')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('media', function (Blueprint $table) {
+            $table->dropColumn(['created_by']);
+        });
+    }
+};
