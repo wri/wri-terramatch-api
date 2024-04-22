24a2390fdccfe71f96a2b1ec3907f5d65548419a
Jose Carlos Laura Ramirez <pachonjcl@gmail.com>
2024-04-22 16:27:33 -0400

    remove char

--- routes/api_v2.php
+++ routes/api_v2.php
@@ -616,7 +616,7 @@ Route::prefix('terrafund')->group(function () {
     Route::post('/upload-geojson', [TerrafundCreateGeometryController::class, 'uploadGeoJSONFile']);
     Route::post('/upload-shapefile', [TerrafundCreateGeometryController::class, 'uploadShapefile']);
     Route::post('/upload-kml', [TerrafundCreateGeometryController::class, 'uploadKMLFile']);
-    Route::post('/polygon/{uuid}', [TerrafundCreateGeometryController::class, 'processGeometry']);=
+    Route::post('/polygon/{uuid}', [TerrafundCreateGeometryController::class, 'processGeometry']);
     Route::get('/geojson/complete', [TerrafundCreateGeometryController::class, 'getPolygonAsGeoJSON']);
     Route::get('/validation/self-intersection', [TerrafundCreateGeometryController::class, 'checkSelfIntersection']);
     Route::get('/validation/size-limit', [TerrafundCreateGeometryController::class, 'validatePolygonSize']);
