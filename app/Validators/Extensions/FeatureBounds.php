9b5938a8b6dec741831f761308f626487333df0c
Nathan Curtis <nathan.curtis@gmail.com>
2024-04-29 13:52:17 -0700

    [TM-799] Convert bounds validation to a laravel validator.

---
+++ app/Validators/Extensions/FeatureBounds.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Validators\Extensions;
+
+class FeatureBounds extends Extension
+{
+    public static $name = 'feature_bounds';
+
+    public static $message = [
+        'FEATURE_BOUNDS',
+        'The {{attribute}} field must have valid feature polygon bounds.',
+        ['attribute' => ':attribute'],
+        'The :attribute field must have valid feature polygon bounds.',
+    ];
+
+    public static function passes($attribute, $value, $parameters, $validator): bool
+    {
+        $type = data_get($value, 'geometry.type');
+        if ($type === 'Polygon') {
+            return self::hasValidPolygonBounds(data_get($value, 'geometry.coordinates'));
+        } else if ($type === 'MultiPolygon') {
+            foreach (data_get($value, 'geometry.coordinates') as $coordinates) {
+                if (! self::hasValidPolygonBounds($coordinates)) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    private static function hasValidPolygonBounds($coordinates): bool
+    {
+        foreach ($coordinates as $coordinate) {
+            $latitude = $coordinate[1];
+            $longitude = $coordinate[0];
+            if ($latitude < -90 || $latitude > 90) {
+                return false;
+            }
+            if ($longitude < -180 || $longitude > 180) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
