d9f71afb40d0208aaf547a55967cb6d7b97c956f
JORGE <egrojmonroy@gmail.com>
2024-05-07 13:21:57 -0400

    swagger to create site polygon add status fillable

--- app/Http/Controllers/V2/Terrafund/TerrafundEditGeometryController.php
+++ app/Http/Controllers/V2/Terrafund/TerrafundEditGeometryController.php
@@ -14,217 +14,231 @@ use Illuminate\Support\Str;
 
 class TerrafundEditGeometryController extends Controller
 {
-    public function getSitePolygonData(string $uuid)
-    {
-        try {
-            $sitePolygon = SitePolygon::where('poly_id', $uuid)->first();
+  public function getSitePolygonData(string $uuid)
+  {
+    try {
+      $sitePolygon = SitePolygon::where('poly_id', $uuid)->first();
 
-            if (! $sitePolygon) {
-                return response()->json(['message' => 'No site polygons found for the given UUID.'], 404);
-            }
+      if (!$sitePolygon) {
+        return response()->json(['message' => 'No site polygons found for the given UUID.'], 404);
+      }
 
-            return response()->json(['site_polygon' => $sitePolygon]);
-        } catch (\Exception $e) {
-            return response()->json(['message' => $e->getMessage()], 500);
-        }
+      return response()->json(['site_polygon' => $sitePolygon]);
+    } catch (\Exception $e) {
+      return response()->json(['message' => $e->getMessage()], 500);
     }
+  }
 
-    public function updateEstAreainSitePolygon($polygonGeometry, $geometry)
-    {
-        try {
-            $sitePolygon = SitePolygon::where('poly_id', $polygonGeometry->uuid)->first();
+  public function updateEstAreainSitePolygon($polygonGeometry, $geometry)
+  {
+    try {
+      $sitePolygon = SitePolygon::where('poly_id', $polygonGeometry->uuid)->first();
 
-            if ($sitePolygon) {
-                $geojson = json_encode($geometry);
-                $areaSqDegrees = DB::selectOne("SELECT ST_Area(ST_GeomFromGeoJSON('$geojson')) AS area")->area;
-                $latitude = DB::selectOne("SELECT ST_Y(ST_Centroid(ST_GeomFromGeoJSON('$geojson'))) AS latitude")->latitude;
-                $unitLatitude = 111320;
-                $areaSqMeters = $areaSqDegrees * pow($unitLatitude * cos(deg2rad($latitude)), 2);
-                $areaHectares = $areaSqMeters / 10000;
+      if ($sitePolygon) {
+        $geojson = json_encode($geometry);
+        $areaSqDegrees = DB::selectOne("SELECT ST_Area(ST_GeomFromGeoJSON('$geojson')) AS area")->area;
+        $latitude = DB::selectOne("SELECT ST_Y(ST_Centroid(ST_GeomFromGeoJSON('$geojson'))) AS latitude")->latitude;
+        $unitLatitude = 111320;
+        $areaSqMeters = $areaSqDegrees * pow($unitLatitude * cos(deg2rad($latitude)), 2);
+        $areaHectares = $areaSqMeters / 10000;
 
-                $sitePolygon->calc_area = $areaHectares;
-                $sitePolygon->save();
+        $sitePolygon->calc_area = $areaHectares;
+        $sitePolygon->save();
 
-                Log::info("Updated area for site polygon with UUID: $sitePolygon->uuid");
-            } else {
-                Log::warning("Updating Area: Site polygon with UUID $polygonGeometry->uuid not found.");
-            }
-        } catch (\Exception $e) {
-            Log::error('Error updating area in site polygon: ' . $e->getMessage());
-        }
+        Log::info("Updated area for site polygon with UUID: $sitePolygon->uuid");
+      } else {
+        Log::warning("Updating Area: Site polygon with UUID $polygonGeometry->uuid not found.");
+      }
+    } catch (\Exception $e) {
+      Log::error('Error updating area in site polygon: ' . $e->getMessage());
     }
+  }
 
-    public function updateProjectCentroid($polygonGeometry)
-    {
-        try {
-            $sitePolygon = SitePolygon::where('poly_id', $polygonGeometry->uuid)->first();
+  public function updateProjectCentroid($polygonGeometry)
+  {
+    try {
+      $sitePolygon = SitePolygon::where('poly_id', $polygonGeometry->uuid)->first();
 
-            if ($sitePolygon) {
-                $project = Project::where('uuid', $sitePolygon->project_id)->first();
+      if ($sitePolygon) {
+        $project = Project::where('uuid', $sitePolygon->project_id)->first();
 
-                if ($project) {
-                    $geometryHelper = new GeometryHelper();
-                    $centroid = $geometryHelper->centroidOfProject($project->uuid);
+        if ($project) {
+          $geometryHelper = new GeometryHelper();
+          $centroid = $geometryHelper->centroidOfProject($project->uuid);
 
-                    if ($centroid === null) {
-                        Log::warning("Invalid centroid for project UUID: $project->uuid");
-                    }
-                } else {
-                    Log::warning("Project with UUID $sitePolygon->project_id not found.");
-                }
-            } else {
-                Log::warning("Site polygon with UUID $polygonGeometry->uuid not found.");
-            }
-        } catch (\Exception $e) {
-            Log::error('Error updating project centroid: ' . $e->getMessage());
+          if ($centroid === null) {
+            Log::warning("Invalid centroid for project UUID: $project->uuid");
+          }
+        } else {
+          Log::warning("Project with UUID $sitePolygon->project_id not found.");
         }
+      } else {
+        Log::warning("Site polygon with UUID $polygonGeometry->uuid not found.");
+      }
+    } catch (\Exception $e) {
+      Log::error('Error updating project centroid: ' . $e->getMessage());
     }
+  }
 
-    public function deletePolygonAndSitePolygon(string $uuid)
-    {
-        try {
-            $polygonGeometry = PolygonGeometry::where('uuid', $uuid)->first();
-            if (! $polygonGeometry) {
-                return response()->json(['message' => 'No polygon geometry found for the given UUID.'], 404);
-            }
-            $sitePolygon = SitePolygon::where('poly_id', $uuid)->first();
-            $projectUuid = $sitePolygon->project_id;
-            if (! $projectUuid) {
-                return response()->json(['message' => 'No project found for the given UUID.'], 404);
-            }
-            if ($sitePolygon) {
-                Log::info("Deleting associated site polygon for UUID: $uuid");
-                $sitePolygon->delete();
-            }
-            $geometryHelper = new GeometryHelper();
-            $geometryHelper->updateProjectCentroid($projectUuid);
-            $polygonGeometry->delete();
-            Log::info("Polygon geometry and associated site polygon deleted successfully for UUID: $uuid");
+  public function deletePolygonAndSitePolygon(string $uuid)
+  {
+    try {
+      $polygonGeometry = PolygonGeometry::where('uuid', $uuid)->first();
+      if (!$polygonGeometry) {
+        return response()->json(['message' => 'No polygon geometry found for the given UUID.'], 404);
+      }
+      $sitePolygon = SitePolygon::where('poly_id', $uuid)->first();
+      $projectUuid = $sitePolygon->project_id;
+      if (!$projectUuid) {
+        return response()->json(['message' => 'No project found for the given UUID.'], 404);
+      }
+      if ($sitePolygon) {
+        Log::info("Deleting associated site polygon for UUID: $uuid");
+        $sitePolygon->delete();
+      }
+      $geometryHelper = new GeometryHelper();
+      $geometryHelper->updateProjectCentroid($projectUuid);
+      $polygonGeometry->delete();
+      Log::info("Polygon geometry and associated site polygon deleted successfully for UUID: $uuid");
 
-            return response()->json(['message' => 'Polygon geometry and associated site polygon deleted successfully.', 'uuid' => $uuid]);
-        } catch (\Exception $e) {
-            Log::error('An error occurred: ' . $e->getMessage());
+      return response()->json(['message' => 'Polygon geometry and associated site polygon deleted successfully.', 'uuid' => $uuid]);
+    } catch (\Exception $e) {
+      Log::error('An error occurred: ' . $e->getMessage());
 
-            // Return error response if an exception occurs
-            return response()->json(['error' => 'An error occurred: ' . $e->getMessage()], 500);
-        }
+      // Return error response if an exception occurs
+      return response()->json(['error' => 'An error occurred: ' . $e->getMessage()], 500);
     }
+  }
 
-    public function updateGeometry(string $uuid, Request $request)
-    {
-        try {
-            Log::info("Updating geometry for polygon with UUID: $uuid");
+  public function updateGeometry(string $uuid, Request $request)
+  {
+    try {
+      Log::info("Updating geometry for polygon with UUID: $uuid");
 
-            $polygonGeometry = PolygonGeometry::where('uuid', $uuid)->first();
-            if (! $polygonGeometry) {
-                return response()->json(['message' => 'No polygon geometry found for the given UUID.'], 404);
-            }
-            $geometry = json_decode($request->input('geometry'));
-            $geom = DB::raw("ST_GeomFromGeoJSON('" . json_encode($geometry) . "')");
-            $polygonGeometry->geom = $geom;
-            $polygonGeometry->save();
-            Log::info('ABOUT TO CREATE');
-            $this->updateEstAreainSitePolygon($polygonGeometry, $geometry);
-            $this->updateProjectCentroid($polygonGeometry);
+      $polygonGeometry = PolygonGeometry::where('uuid', $uuid)->first();
+      if (!$polygonGeometry) {
+        return response()->json(['message' => 'No polygon geometry found for the given UUID.'], 404);
+      }
+      $geometry = json_decode($request->input('geometry'));
+      $geom = DB::raw("ST_GeomFromGeoJSON('" . json_encode($geometry) . "')");
+      $polygonGeometry->geom = $geom;
+      $polygonGeometry->save();
+      Log::info('ABOUT TO CREATE');
+      $this->updateEstAreainSitePolygon($polygonGeometry, $geometry);
+      $this->updateProjectCentroid($polygonGeometry);
 
-            return response()->json(['message' => 'Geometry updated successfully.', 'geometry' => $geometry, 'uuid' => $uuid]);
-        } catch (\Exception $e) {
-            return response()->json(['error' => 'An error occurred: ' . $e->getMessage()], 500);
-        }
+      return response()->json(['message' => 'Geometry updated successfully.', 'geometry' => $geometry, 'uuid' => $uuid]);
+    } catch (\Exception $e) {
+      return response()->json(['error' => 'An error occurred: ' . $e->getMessage()], 500);
     }
+  }
 
-    public function getPolygonGeojson(string $uuid)
-    {
-        $geometryQuery = PolygonGeometry::isUuid($uuid);
-        if (! $geometryQuery->exists()) {
-            return response()->json(['message' => 'No polygon geometry found for the given UUID.'], 404);
-        }
-        $geojsonData = json_decode($geometryQuery->select(DB::raw('ST_AsGeoJSON(geom) as geojson'))->first()->geojson, true);
+  public function getPolygonGeojson(string $uuid)
+  {
+    $geometryQuery = PolygonGeometry::isUuid($uuid);
+    if (!$geometryQuery->exists()) {
+      return response()->json(['message' => 'No polygon geometry found for the given UUID.'], 404);
+    }
+    $geojsonData = json_decode($geometryQuery->select(DB::raw('ST_AsGeoJSON(geom) as geojson'))->first()->geojson, true);
 
-        return response()->json([
-          'geojson' => $geojsonData,
+    return response()->json([
+      'geojson' => $geojsonData,
+    ]);
+  }
+
+  public function updateSitePolygon(string $uuid, Request $request)
+  {
+    try {
+      $sitePolygon = SitePolygon::where('uuid', $uuid)->first();
+      if (!$sitePolygon) {
+        return response()->json(['message' => 'No site polygons found for the given UUID.'], 404);
+      }
+      $validatedData = $request->validate([
+        'poly_name' => 'nullable|string',
+        'plantstart' => 'nullable|date',
+        'plantend' => 'nullable|date',
+        'practice' => 'nullable|string',
+        'distr' => 'nullable|string',
+        'num_trees' => 'nullable|integer',
+        'calc_area' => 'nullable|numeric',
+        'target_sys' => 'nullable|string',
+      ]);
+
+      $sitePolygon->update($validatedData);
+
+      return response()->json(['message' => 'Site polygon updated successfully'], 200);
+    } catch (\Exception $e) {
+      // Handle other exceptions
+      return response()->json(['error' => 'An error occurred: ' . $e->getMessage()], 500);
+    }
+  }
+
+  public function createSitePolygon(string $uuid, string $siteUuid, Request $request)
+  {
+    try {
+      if ($request->getContent() === '{}') {
+        $validatedData = [
+          'poly_name' => null,
+          'plantstart' => null,
+          'plantend' => null,
+          'practice' => null,
+          'distr' => null,
+          'num_trees' => null,
+          'target_sys' => null,
+        ];
+      } else {
+        $validatedData = $request->validate([
+          'poly_name' => 'nullable|string',
+          'plantstart' => 'nullable|date',
+          'plantend' => 'nullable|date',
+          'practice' => 'nullable|string',
+          'distr' => 'nullable|string',
+          'num_trees' => 'nullable|integer',
+          'target_sys' => 'nullable|string',
         ]);
+      }
+
+      $polygonGeometry = PolygonGeometry::where('uuid', $uuid)->first();
+      if (!$polygonGeometry) {
+        return response()->json(['message' => 'No polygon geometry found for the given UUID.'], 404);
+      }
+      $areaSqDegrees = DB::selectOne('SELECT ST_Area(geom) AS area FROM polygon_geometry WHERE uuid = :uuid', ['uuid' => $uuid])->area;
+      $latitude = DB::selectOne('SELECT ST_Y(ST_Centroid(geom)) AS latitude FROM polygon_geometry WHERE uuid = :uuid', ['uuid' => $uuid])->latitude;
+      $areaSqMeters = $areaSqDegrees * pow(111320 * cos(deg2rad($latitude)), 2);
+      $areaHectares = $areaSqMeters / 10000;
+      $sitePolygon = new SitePolygon([
+        'poly_name' => $validatedData['poly_name'],
+        'plantstart' => $validatedData['plantstart'],
+        'plantend' => $validatedData['plantend'],
+        'practice' => $validatedData['practice'],
+        'distr' => $validatedData['distr'],
+        'num_trees' => $validatedData['num_trees'],
+        'calc_area' => $areaHectares,
+        'target_sys' => $validatedData['target_sys'],
+        'status' => 'Submitted',
+        'site_id' => $siteUuid
+      ]);
+      $sitePolygon->poly_id = $uuid;
+      $sitePolygon->uuid = Str::uuid();
+      $sitePolygon->save();
+
+      return response()->json(['message' => 'Site polygon created successfully', 'uuid' => $sitePolygon, 'area' => $areaHectares], 201);
+    } catch (\Exception $e) {
+      // Handle other exceptions
+      return response()->json(['error' => 'An error occurred: ' . $e->getMessage()], 500);
     }
+  }
 
-    public function updateSitePolygon(string $uuid, Request $request)
-    {
-        try {
-            $sitePolygon = SitePolygon::where('uuid', $uuid)->first();
-            if (! $sitePolygon) {
-                return response()->json(['message' => 'No site polygons found for the given UUID.'], 404);
-            }
-            $validatedData = $request->validate([
-              'poly_name' => 'nullable|string',
-              'plantstart' => 'nullable|date',
-              'plantend' => 'nullable|date',
-              'practice' => 'nullable|string',
-              'distr' => 'nullable|string',
-              'num_trees' => 'nullable|integer',
-              'calc_area' => 'nullable|numeric',
-              'target_sys' => 'nullable|string',
-            ]);
+  public function getPolygonBbox(string $uuid)
+  {
+    try {
+      $bboxCoordinates = GeometryHelper::getPolygonsBbox([$uuid]);
 
-            $sitePolygon->update($validatedData);
+      return response()->json(['bbox' => $bboxCoordinates]);
+    } catch (\Exception $e) {
+      Log::error($e->getMessage());
 
-            return response()->json(['message' => 'Site polygon updated successfully'], 200);
-        } catch (\Exception $e) {
-            // Handle other exceptions
-            return response()->json(['error' => 'An error occurred: ' . $e->getMessage()], 500);
-        }
+      return response()->json(['error' => 'An error occurred while fetching the bounding box coordinates'], 404);
     }
-
-    public function createSitePolygon(string $uuid, Request $request)
-    {
-        try {
-            $validatedData = $request->validate([
-              'poly_name' => 'nullable|string',
-              'plantstart' => 'nullable|date',
-              'plantend' => 'nullable|date',
-              'practice' => 'nullable|string',
-              'distr' => 'nullable|string',
-              'num_trees' => 'nullable|integer',
-              'target_sys' => 'nullable|string',
-            ]);
-
-            $polygonGeometry = PolygonGeometry::where('uuid', $uuid)->first();
-            if (! $polygonGeometry) {
-                return response()->json(['message' => 'No polygon geometry found for the given UUID.'], 404);
-            }
-            $areaSqDegrees = DB::selectOne('SELECT ST_Area(geom) AS area FROM polygon_geometry WHERE uuid = :uuid', ['uuid' => $uuid])->area;
-            $latitude = DB::selectOne('SELECT ST_Y(ST_Centroid(geom)) AS latitude FROM polygon_geometry WHERE uuid = :uuid', ['uuid' => $uuid])->latitude;
-            $areaSqMeters = $areaSqDegrees * pow(111320 * cos(deg2rad($latitude)), 2);
-            $areaHectares = $areaSqMeters / 10000;
-            $sitePolygon = new SitePolygon([
-              'poly_name' => $validatedData['poly_name'],
-              'plantstart' => $validatedData['plantstart'],
-              'plantend' => $validatedData['plantend'],
-              'practice' => $validatedData['practice'],
-              'distr' => $validatedData['distr'],
-              'num_trees' => $validatedData['num_trees'],
-              'calc_area' => $areaHectares, // Assign the calculated area
-              'target_sys' => $validatedData['target_sys'],
-            ]);
-            $sitePolygon->poly_id = $uuid;
-            $sitePolygon->uuid = Str::uuid();
-            $sitePolygon->save();
-
-            return response()->json(['message' => 'Site polygon created successfully', 'uuid' => $sitePolygon, 'area' => $areaHectares], 201);
-        } catch (\Exception $e) {
-            // Handle other exceptions
-            return response()->json(['error' => 'An error occurred: ' . $e->getMessage()], 500);
-        }
-    }
-
-    public function getPolygonBbox(string $uuid)
-    {
-        try {
-            $bboxCoordinates = GeometryHelper::getPolygonsBbox([$uuid]);
-
-            return response()->json(['bbox' => $bboxCoordinates]);
-        } catch (\Exception $e) {
-            Log::error($e->getMessage());
-
-            return response()->json(['error' => 'An error occurred while fetching the bounding box coordinates'], 404);
-        }
-    }
+  }
 }
