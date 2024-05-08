a25a558b556c93c80a1fb07ba63e6aaca8ec1f65
Nathan Curtis <nathan.curtis@gmail.com>
2024-05-08 16:46:36 -0700

    [TM-799] Set up tests for all validators that don't require site/project context.

--- app/Validators/SitePolygonValidator.php
+++ app/Validators/SitePolygonValidator.php
@@ -9,6 +9,21 @@ class SitePolygonValidator extends Validator
         'features.*' => 'polygon_feature_bounds',
     ];
 
+    public const SPIKES = [
+        'features' => 'required|array',
+        'features.*.geometry' => 'polygon_spikes',
+    ];
+
+    public const POLYGON_SIZE = [
+        'features' => 'required|array',
+        'features.*' => 'polygon_size',
+    ];
+
+    public const SELF_INTERSECTION = [
+        'features' => 'required|array',
+        'features.*' => 'polygon_self_intersection',
+    ];
+
     public const WITHIN_COUNTRY = [
         '*' => 'required|string|uuid|has_polygon_site|within_country',
     ];
@@ -18,22 +33,24 @@ class SitePolygonValidator extends Validator
     ];
 
     public const SCHEMA = [
-        'poly_name' => 'required',
-        'plantstart' => 'required',
-        'plantend' => 'required',
-        'practice' => 'required',
-        'target_sys' => 'required',
-        'distr' => 'required',
-        'num_trees' => 'required',
+        'features' => 'required|array',
+        'features.*.properties.poly_name' => 'required',
+        'features.*.properties.plantstart' => 'required',
+        'features.*.properties.plantend' => 'required',
+        'features.*.properties.practice' => 'required',
+        'features.*.properties.target_sys' => 'required',
+        'features.*.properties.distr' => 'required',
+        'features.*.properties.num_trees' => 'required',
     ];
 
     public const DATA = [
-        'poly_name' => 'required|string|not_in:null,NULL',
-        'plantstart' => 'required|date|',
-        'plantend' => 'required|date|',
-        'practice' => 'required|string|not_in:null,NULL',
-        'target_sys' => 'required|string|not_in:null,NULL',
-        'distr' => 'required|string|not_in:null,NULL',
-        'num_trees' => 'required|integer|',
+        'features' => 'required|array',
+        'features.*.properties.poly_name' => 'required|string|not_in:null,NULL',
+        'features.*.properties.plantstart' => 'required|date|',
+        'features.*.properties.plantend' => 'required|date|',
+        'features.*.properties.practice' => 'required|string|not_in:null,NULL',
+        'features.*.properties.target_sys' => 'required|string|not_in:null,NULL',
+        'features.*.properties.distr' => 'required|string|not_in:null,NULL',
+        'features.*.properties.num_trees' => 'required|integer|',
     ];
 }
