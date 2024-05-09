3c746225de140657373574f24dffe422cc7ffe2d
Limber Mamani <limbermv2@gmail.com>
2024-05-08 22:27:44 -0400

    feat: add entity conditional params

--- app/Http/Controllers/V2/AuditStatus/GetAuditStatusController.php
+++ app/Http/Controllers/V2/AuditStatus/GetAuditStatusController.php
@@ -5,15 +5,23 @@ namespace App\Http\Controllers\V2\AuditStatus;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\V2\AuditStatusResource;
 use App\Models\V2\AuditStatus\AuditStatus;
+use Illuminate\Http\Request;
 
 class GetAuditStatusController extends Controller
 {
-    public function __invoke(string $id = null)
+    public function __invoke(Request $request, string $id = null)
     {
         if ($id != null) {
             $auditStatus = AuditStatus::where('id', $id)->first();
 
             return new AuditStatusResource($auditStatus);
+        } else if ($request->has('entity')) {
+            $auditStatus = AuditStatus::where('entity_uuid', $request->input('entity'))
+                ->orderBy('updated_at', 'desc')
+                ->orderBy('created_at', 'desc')
+                ->get();
+
+            return AuditStatusResource::collection($auditStatus);
         } else {
             $auditStatus = AuditStatus::orderBy('updated_at', 'desc')
                 ->orderBy('created_at', 'desc')
