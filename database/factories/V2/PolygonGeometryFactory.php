0171a34e56ef55efde8388379efa7430569f5fc0
Nathan Curtis <nathan.curtis@gmail.com>
2024-05-09 20:42:33 -0700

    [TM-799] Test the not overlapping validation. Not yet passing; probably needs new test files.

---
+++ database/factories/V2/PolygonGeometryFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories\V2;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\DB;
+
+class PolygonGeometryFactory extends Factory
+{
+    public function definition()
+    {
+        return [
+            'uuid' => $this->faker->uuid(),
+        ];
+    }
+
+    public function geojson(string|array $geojson)
+    {
+        $geom = DB::raw("ST_GeomFromGeoJSON('$geojson')");
+        return $this->state(function (array $attributes) use ($geom) {
+           return [
+               'geom' => $geom
+           ];
+        });
+    }
+}
