b7ae22818365c85f249eaa4ad750266c8f9a68d0
Limber Mamani <154026979+LimberHope@users.noreply.github.com>
2024-05-21 14:43:33 -0400

    feat: charge files to s3 (#214)

--- app/Models/Traits/SaveAuditAttachmentTrait.php
+++ app/Models/Traits/SaveAuditAttachmentTrait.php
@@ -7,11 +7,12 @@ use Illuminate\Support\Facades\Auth;
 
 trait SaveAuditAttachmentTrait
 {
-    public function saveAuditAttachment($entity_uuid = null, $attachment = null)
+    public function saveAuditAttachment($entity_uuid = null, $attachment = null, $url_file = null)
     {
         return AuditAttachment::create([
             'entity_id' => $entity_uuid,
             'attachment' => $attachment,
+            'url_file' => $url_file,
             'date_created' => now(),
             'created_by' => Auth::user()->email_address,
         ]);
