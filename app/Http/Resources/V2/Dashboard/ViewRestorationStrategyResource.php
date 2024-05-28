2ba4cdb5541529645600a1d181f92f53899a05f8
cesarLima1 <cesmaulim@gmail.com>
2024-05-28 12:29:05 -0400

    [TM-626] start up restoration strategies controller

---
+++ app/Http/Resources/V2/Dashboard/ViewRestorationStrategyResource.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Resources\V2\Dashboard;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ViewRestorationStrategyResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'restorationStrategies' => $this->resource['restorationStrategies'] ?? null,
+            'landUseTypes' => $this->resource['landUseTypes'] ?? null,
+            'landTenures' => $this->resource['landTenures'] ?? null,
+        ];
+    }
+}
