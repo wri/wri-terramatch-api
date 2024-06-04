7acd44a2061410c3f73c135e019e9382f71be5b3
cesarLima1 <cesmaulim@gmail.com>
2024-06-04 10:32:03 -0400

    add bbox to entity endpoint

--- app/Http/Controllers/V2/Entities/EntityTypeController.php
+++ app/Http/Controllers/V2/Entities/EntityTypeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\V2\Entities;
 
+use App\Helpers\GeometryHelper;
 use App\Http\Controllers\Controller;
 use App\Models\V2\Projects\Project;
 use App\Models\V2\Sites\Site;
@@ -19,22 +20,28 @@ class EntityTypeController extends Controller
             $project = Project::where('uuid', $uuid)->first();
             if ($project) {
                 $sitePolygons = $project->sitePolygons;
+                $polygonsUuids = $sitePolygons->pluck('poly_id');
+                $bboxCoordinates = GeometryHelper::getPolygonsBbox($polygonsUuids);
 
                 return response()->json([
                     'type' => 'project',
                     'uuid' => $uuid,
                     'polygonsData' => $sitePolygons,
+                    'bbox' => $bboxCoordinates,
                 ]);
             }
 
             $site = Site::where('uuid', $uuid)->first();
             if ($site) {
                 $sitePolygons = $site->sitePolygons;
+                $polygonsUuids = $sitePolygons->pluck('poly_id');
+                $bboxCoordinates = GeometryHelper::getPolygonsBbox($polygonsUuids);
 
                 return response()->json([
                     'type' => 'site',
                     'uuid' => $uuid,
                     'polygonsData' => $sitePolygons,
+                    'bbox' => $bboxCoordinates,
                 ]);
             }
 
