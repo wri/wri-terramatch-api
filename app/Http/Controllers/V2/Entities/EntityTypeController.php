3afb6efc69392cebc6d1b6df640b2aa9d3f4fbaf
JORGE <egrojmonroy@gmail.com>
2024-06-05 11:48:35 -0400

    sort and filter for polygon data in entity type

--- app/Http/Controllers/V2/Entities/EntityTypeController.php
+++ app/Http/Controllers/V2/Entities/EntityTypeController.php
@@ -16,33 +16,12 @@ class EntityTypeController extends Controller
     {
         try {
             $uuid = $request->input('uuid');
+            $type = $this->getEntityType($uuid);
 
-            $project = Project::where('uuid', $uuid)->first();
-            if ($project) {
-                $sitePolygons = $project->sitePolygons;
-                $polygonsUuids = $sitePolygons->pluck('poly_id');
-                $bboxCoordinates = GeometryHelper::getPolygonsBbox($polygonsUuids);
-
-                return response()->json([
-                    'type' => 'project',
-                    'uuid' => $uuid,
-                    'polygonsData' => $sitePolygons,
-                    'bbox' => $bboxCoordinates,
-                ]);
-            }
-
-            $site = Site::where('uuid', $uuid)->first();
-            if ($site) {
-                $sitePolygons = $site->sitePolygons;
-                $polygonsUuids = $sitePolygons->pluck('poly_id');
-                $bboxCoordinates = GeometryHelper::getPolygonsBbox($polygonsUuids);
-
-                return response()->json([
-                    'type' => 'site',
-                    'uuid' => $uuid,
-                    'polygonsData' => $sitePolygons,
-                    'bbox' => $bboxCoordinates,
-                ]);
+            if ($type === 'project') {
+                return $this->handleProjectEntity($uuid, $request);
+            } elseif ($type === 'site') {
+                return $this->handleSiteEntity($uuid, $request);
             }
 
             return response()->json([
@@ -50,12 +29,75 @@ class EntityTypeController extends Controller
                 'uuid' => $uuid,
             ]);
         } catch (Exception $e) {
-
             Log::error($e);
 
             return response()->json([
                 'error' => 'An error occurred while processing your request.',
             ], 500);
         }
+    }
+
+    private function getEntityType($uuid)
+    {
+        $project = Project::where('uuid', $uuid)->first();
+        if ($project) {
+            return 'project';
+        }
+
+        $site = Site::where('uuid', $uuid)->first();
+        if ($site) {
+            return 'site';
+        }
+
+        return 'unknown';
+    }
+
+    private function handleProjectEntity($uuid, Request $request)
+    {
+        $project = Project::where('uuid', $uuid)->first();
+        $sitePolygons = $this->getSitePolygonsWithFiltersAndSorts($project->sitePolygons(), $request);
+        $polygonsUuids = $sitePolygons->pluck('poly_id');
+        $bboxCoordinates = GeometryHelper::getPolygonsBbox($polygonsUuids);
+
+        return response()->json([
+            'type' => 'project',
+            'uuid' => $uuid,
+            'polygonsData' => $sitePolygons,
+            'bbox' => $bboxCoordinates,
+        ]);
+    }
+
+    private function handleSiteEntity($uuid, Request $request)
+    {
+        $site = Site::where('uuid', $uuid)->first();
+        $sitePolygons = $this->getSitePolygonsWithFiltersAndSorts($site->sitePolygons(), $request);
+        $polygonsUuids = $sitePolygons->pluck('poly_id');
+        $bboxCoordinates = GeometryHelper::getPolygonsBbox($polygonsUuids);
+
+        return response()->json([
+            'type' => 'site',
+            'uuid' => $uuid,
+            'polygonsData' => $sitePolygons,
+            'bbox' => $bboxCoordinates,
+        ]);
+    }
+
+    private function getSitePolygonsWithFiltersAndSorts($sitePolygonsQuery, Request $request)
+    {
+        if ($request->has('status')) {
+            $statusValues = explode(',', $request->input('status'));
+            $sitePolygonsQuery->whereIn('site_polygon.status', $statusValues);
+        }
+
+        $sortFields = $request->input('sort', []);
+        foreach ($sortFields as $field => $direction) {
+            if ($field === 'status') {
+                $sitePolygonsQuery->orderByRaw('FIELD(site_polygon.status, "draft", "submitted", "needs-more-information", "approved") ' . $direction);
+            } else {
+                $sitePolygonsQuery->orderBy($field, $direction);
+            }
+        }
+
+        return $sitePolygonsQuery->get();
     }
 }
