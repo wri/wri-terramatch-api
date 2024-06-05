3b5da918c82daf0bcee3ffc5a3d854ea4f343fc5
Limber Mamani <limbermv2@gmail.com>
2024-06-05 09:41:09 -0400

    fix endpoint PolygonsApproved

--- app/Http/Controllers/V2/Polygons/GetPolygonsApprovedController.php
+++ app/Http/Controllers/V2/Polygons/GetPolygonsApprovedController.php
@@ -11,10 +11,9 @@ class GetPolygonsApprovedController extends Controller
 {
     public function __invoke(Request $request, string $uuid = null)
     {
-        $PolygonsCount = SitePolygon::selectRaw('COUNT(*) as total, SUM(CASE WHEN status = "approved" THEN 1 ELSE 0 END) as approved_count')
-            ->where('site_id', $uuid)
+        $nonApproved = SitePolygon::where('site_id', $uuid)
+            ->where('status', '!=', 'approved')
             ->first();
-        $checkApproved = !($PolygonsCount->total == $PolygonsCount->approved_count);
-        return new PolygonsApprovedResource(["check_polygons" => $checkApproved]);
+        return new PolygonsApprovedResource(["check_polygons" => $nonApproved != null]);
     }
 }
