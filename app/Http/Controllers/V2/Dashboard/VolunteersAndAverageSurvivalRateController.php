35b20f35ad907c5c7ac74f147046295c9aec1700
Limber Mamani <limbermv2@gmail.com>
2024-06-06 15:32:01 -0400

    [TM-624,625] feat: add volunteers average survival rate controller

---
+++ app/Http/Controllers/V2/Dashboard/VolunteersAndAverageSurvivalRateController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Http\Controllers\V2\Dashboard;
+
+use App\Helpers\TerrafundDashboardQueryHelper;
+use App\Http\Controllers\Controller;
+use App\Http\Resources\V2\Dashboard\VolunteersAndAverageResource;
+use Illuminate\Http\Request;
+
+class VolunteersAndAverageSurvivalRateController extends Controller
+{
+    public function __invoke(Request $request): VolunteersAndAverageResource
+    {
+        $projects = TerrafundDashboardQueryHelper::buildQueryFromRequest($request)->get();
+
+        $response = (object)[
+            'total_volunteers' => $this->getTotalVolunteerSum($projects),
+            'men_volunteers' => $this->getVolunteersSum($projects, 'volunteer_men'),
+            'women_volunteers' => $this->getVolunteersSum($projects, 'volunteer_women'),
+            'youth_volunteers' => $this->getVolunteersSum($projects, 'volunteer_youth'),
+            'non_youth_volunteers' => $this->getVolunteersSum($projects, 'volunteer_non_youth'),
+            'non_profit_survival_rate' => $this->getAverageSurvivalRate($projects, 'non-profit-organization'),
+            'enterprise_survival_rate' => $this->getAverageSurvivalRate($projects, 'for-profit-organization'),
+            'number_of_sites' => $this->numberOfSites($projects),
+            'number_of_nurseries' => $this->numberOfNurseries($projects),
+        ];
+
+        return new VolunteersAndAverageResource($response);
+    }
+
+    public function getTotalVolunteerSum($projects)
+    {
+        return $projects->sum(function ($project) {
+            return $project->reports()->sum('volunteer_total');
+        });
+    }
+
+    public function getVolunteersSum($projects, $volunteerType)
+    {
+        return $projects->sum(function ($project) use ($volunteerType) {
+            return $project->reports()->sum($volunteerType);
+        });
+    }
+
+    public function getAverageSurvivalRate($projects, $typeOrganisation)
+    {
+        $average = $projects->filter(function ($project) use ($typeOrganisation) {
+            return $project->organisation->type === $typeOrganisation;
+        })->flatMap(function ($project) {
+            return $project->reports;
+        })->avg('pct_survival_to_date');
+
+        return intval($average);
+    }
+
+    public function numberOfSites($projects)
+    {
+        return $projects->sum(function ($project) {
+            return $project->sites->count();
+        });
+    }
+
+    public function numberOfNurseries($projects)
+    {
+        return $projects->sum(function ($project) {
+            return $project->nurseries->count();
+        });
+    }
+}
--- routes/api_v2.php
+++ routes/api_v2.php
@@ -15,6 +15,7 @@ use App\Http\Controllers\V2\BaselineMonitoring\BaselineMonitoringSiteController;
 use App\Http\Controllers\V2\CoreTeamLeader\DeleteCoreTeamLeaderController;
 use App\Http\Controllers\V2\CoreTeamLeader\StoreCoreTeamLeaderController;
 use App\Http\Controllers\V2\CoreTeamLeader\UpdateCoreTeamLeaderController;
+use App\Http\Controllers\V2\Dashboard\VolunteersAndAverageSurvivalRateController;
 use App\Http\Controllers\V2\Dashboard\GetJobsCreatedController;
 use App\Http\Controllers\V2\Disturbances\DeleteDisturbanceController;
 use App\Http\Controllers\V2\Disturbances\GetDisturbancesForEntityController;
@@ -655,4 +656,5 @@ Route::resource('files', FilePropertiesController::class);
 
 Route::prefix('dashboard')->group(function () {
     Route::get('/jobs-created', GetJobsCreatedController::class);
+    Route::get('/volunteers-survival-rate', VolunteersAndAverageSurvivalRateController::class);
 });
