09eda84db2ad8e9d83c5df42be196a58aa0a6c02
Jose Carlos Laura Ramirez <pachonjcl@gmail.com>
2024-06-05 22:51:08 -0400

    add attachment table

---
+++ database/migrations/2024_06_06_024930_create_attachment_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::dropIfExists('attachment');
+        Schema::create('attachment', function (Blueprint $table) {
+            $table->id();
+            $table->string('entity_id')->nullable();
+            $table->string('file_name')->nullable();
+            $table->string('file_url')->nullable();
+            $table->date('date_created')->nullable();
+            $table->string('created_by')->nullable();
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('attachment');
+    }
+};
