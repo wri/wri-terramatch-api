07c78515b4f2b1242d439651017bd898d684a8fb
Limber Mamani <limbermv2@gmail.com>
2024-06-07 16:21:08 -0400

    [TM-968] remove the resource class

--- app/Http/Controllers/V2/Dashboard/GetProjectsController.php
+++ app/Http/Controllers/V2/Dashboard/GetProjectsController.php
@@ -4,12 +4,11 @@ namespace App\Http\Controllers\V2\Dashboard;
 
 use App\Helpers\TerrafundDashboardQueryHelper;
 use App\Http\Controllers\Controller;
-use App\Http\Resources\V2\Dashboard\GetProjectsResource;
 use Illuminate\Http\Request;
 
 class GetProjectsController extends Controller
 {
-    public function __invoke(Request $request): GetProjectsResource
+    public function __invoke(Request $request)
     {
         $projects = TerrafundDashboardQueryHelper::buildQueryFromRequest($request)
             ->whereNotNull('long')
@@ -17,8 +16,6 @@ class GetProjectsController extends Controller
             ->select('uuid', 'long', 'lat', 'name')
             ->get();
 
-        return new GetProjectsResource([
-            'data' => $projects,
-        ]);
+        return response()->json(['data' => $projects]);
     }
 };
