1654899327d54c6e46565dfddc22ea0f89577ff8
Limber Mamani <limbermv2@gmail.com>
2024-06-07 12:29:08 -0400

    [TM-968] feat: Add project location controller for centroids

---
+++ app/Http/Controllers/V2/Dashboard/GetProjectsController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers\V2\Dashboard;
+
+use App\Helpers\TerrafundDashboardQueryHelper;
+use App\Http\Controllers\Controller;
+use App\Http\Resources\V2\Dashboard\GetProjectsResource;
+use Illuminate\Http\Request;
+
+class GetProjectsController extends Controller
+{
+    public function __invoke(Request $request): GetProjectsResource
+    {
+        $projects = TerrafundDashboardQueryHelper::buildQueryFromRequest($request)
+            ->whereNotNull('long')
+            ->whereNotNull('lat')
+            ->select('uuid', 'long', 'lat', 'name')
+            ->get();
+
+        return new GetProjectsResource([
+            'data' => $projects,
+        ]);
+    }
+};
