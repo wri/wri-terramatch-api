188da5360b008c577f6fecef885c07f6cac97233
Jose Carlos Laura Ramirez <pachonjcl@gmail.com>
2024-06-10 00:59:53 -0400

    Add endpoint for listing SitePolygons for a given project

---
+++ app/Http/Controllers/V2/Polygons/ViewAllSitesPolygonsForProjectController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\V2\Polygons;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\V2\SitePolygon\SitePolygonResource;
+use App\Models\V2\Projects\Project;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\ResourceCollection;
+
+class ViewAllSitesPolygonsForProjectController extends Controller
+{
+    public function __invoke(Request $request, Project $project): ResourceCollection
+    {
+        $sitePolygons = $project->with('sitePolygons')->get()->pluck('sitePolygons')->flatten();
+
+        return SitePolygonResource::collection($sitePolygons);
+    }
+}
