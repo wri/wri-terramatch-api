6d1f3c2c2ba63beef2bef64e317cd0e2185274af
Limber Mamani <limbermv2@gmail.com>
2024-06-10 21:32:39 -0400

    change parameter UUID -> site

--- app/Http/Controllers/V2/Sites/SiteCheckApproveController.php
+++ app/Http/Controllers/V2/Sites/SiteCheckApproveController.php
@@ -3,16 +3,15 @@
 namespace App\Http\Controllers\V2\Sites;
 
 use App\Http\Controllers\Controller;
-use App\Models\V2\Sites\Site;
 use App\Models\V2\Sites\SitePolygon;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class SiteCheckApproveController extends Controller
 {
-    public function __invoke(Request $request, Site $site): JsonResource
+    public function __invoke(Request $request, string $site): JsonResource
     {
-        $nonApproved = SitePolygon::where('site_id', $site->uuid)
+        $nonApproved = SitePolygon::where('site_id', $site)
             ->where('status', '!=', 'approved')
             ->first();
 
