9da2c77495a9f85cf684ab180305df22031a136e
Jose Carlos Laura Ramirez <pachonjcl@gmail.com>
2024-06-12 13:41:36 -0400

    [TM-857] Audit Log and Comment Feature for Project/Site/Polygons (#254)
    * add status table
    
    * add attachment table
    
    * add audit-status + site-polygon endpoints
    
    * add swagger definitions for audit status and polygon endpoints
    
    * fix: problem with parameter in open api definition
    
    * fix openapi definitions and test execution
    
    * implement polymorphic association in Audit Status
    
    * fix merge errors and remove duplicated definition
    
    * add AuditStatus attachments with MediaModel
    
    * remove unused methods and files
    
    * remove GetPolygonByProjectController
    
    * replace auditable_id and auditable_type with entity_name
    
    * add AuditStatusRequest to improve endpoint readability
    
    * improve swagger definition
    
    * update swagger definition
    
    * replace EntityModel by AuditableModel
    
    * rename variable
    
    * remove unused table
    
    * remove unused definition and update swagger
    
    * change AuditStatus upload file policy
    
    * address corrections on AuditableModel
    
    * use ModelInterfaceBindingMiddleware on store audit controller
    
    * fix typo and update references in swagger
    
    * rename status table to audit-statuese
    
    * change table name to use underscore
    
    * [TM-843] add endpoint to determine wether a site can be approved (#274)
    
    * add endpoint to determine wether a site can be approved
    
    * improve code for checking site approval
    
    * [TM-944] Enable polygon audit log (#273)
    
    * Add endpoint for listing SitePolygons for a given project
    
    * fix problem with parameters missing in definition
    
    * [TM-941] Audit Log request support (#275)
    
    * add endpoint for updating status
    
    * change EntityModel -> AuditableModel and add missing policy
    
    * move criteria site to helper for reuse
    
    * add criteria check for site polygon approval
    
    * move endpoint to auditable path
    
    * add a policy for updating Site Polygons
    
    * fix problem with parameters missing in definitio
    
    * improve query for criteria site

--- app/Models/V2/Sites/SitePolygon.php
+++ app/Models/V2/Sites/SitePolygon.php
@@ -3,6 +3,8 @@
 namespace App\Models\V2\Sites;
 
 use App\Models\Traits\HasUuid;
+use App\Models\V2\AuditableModel;
+use App\Models\V2\AuditStatus\AuditStatus;
 use App\Models\V2\PolygonGeometry;
 use App\Models\V2\Projects\Project;
 use App\Models\V2\User;
@@ -11,6 +13,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasOne;
+use Illuminate\Database\Eloquent\Relations\MorphMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Znck\Eloquent\Relations\BelongsToThrough;
 use Znck\Eloquent\Traits\BelongsToThrough as BelongsToThroughTrait;
@@ -18,7 +21,7 @@ use Znck\Eloquent\Traits\BelongsToThrough as BelongsToThroughTrait;
 /**
  * @method static forPolygonGeometry($value):  Builder
  */
-class SitePolygon extends Model
+class SitePolygon extends Model implements AuditableModel
 {
     use HasUuid;
     use SoftDeletes;
@@ -71,5 +74,20 @@ class SitePolygon extends Model
     public function createdBy(): HasOne
     {
         return $this->hasOne(User::class, 'id', 'created_by');
+    }
+
+    public function getRouteKeyName()
+    {
+        return 'uuid';
+    }
+
+    public function auditStatuses(): MorphMany
+    {
+        return $this->morphMany(AuditStatus::class, 'auditable');
+    }
+
+    public function getAuditableNameAttribute(): string
+    {
+        return $this->poly_name;
     }
 }
