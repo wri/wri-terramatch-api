86df3de30dd705df5c89f05e7f6b24ec5025a3ef
Limber Mamani <154026979+LimberHope@users.noreply.github.com>
2024-06-12 16:18:16 -0400

    [TM-628,629] feat: add top 10 project with the most planted  and top … (#256)
    * [TM-628,629] feat: add top 10 project with the most planted  and top 20 tree species planted controller
    
    * [TM-628,629] Optimization of the method for retrieving top_tree_species_planted
    
    * [TM-628,629] fix: change by ->groupBy(DB::raw('BINARY name'), 'name')

--- routes/api_v2.php
+++ routes/api_v2.php
@@ -22,6 +22,7 @@ use App\Http\Controllers\V2\Dashboard\ActiveCountriesTableController;
 use App\Http\Controllers\V2\Dashboard\CountriesController;
 use App\Http\Controllers\V2\Dashboard\GetProjectsController;
 use App\Http\Controllers\V2\Dashboard\ProjectProfileDetailsController;
+use App\Http\Controllers\V2\Dashboard\TopProjectsAndTopTreeSpeciesController;
 use App\Http\Controllers\V2\Dashboard\CountryDataController;
 use App\Http\Controllers\V2\Dashboard\GetJobsCreatedController;
 use App\Http\Controllers\V2\Dashboard\GetPolygonsController;
@@ -694,4 +695,5 @@ Route::prefix('dashboard')->group(function () {
     Route::get('/countries', CountriesController::class);
     Route::get('/get-projects', GetProjectsController::class);
     Route::get('/project-details/{project}', ProjectProfileDetailsController::class);
+    Route::get('/top-trees-planted', TopProjectsAndTopTreeSpeciesController::class);
 });
