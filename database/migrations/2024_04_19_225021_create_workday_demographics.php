b94ed2a1b3e181ca67ea5ed14b519cc53220ac0a
cesarLima1 <cesmaulim@gmail.com>
2024-06-13 15:24:02 -0400

    [TM-961] verify existence of workday_demographics table in migration

--- database/migrations/2024_04_19_225021_create_workday_demographics.php
+++ database/migrations/2024_04_19_225021_create_workday_demographics.php
@@ -5,23 +5,25 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration {
+return new class () extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
-        Schema::create('workday_demographics', function (Blueprint $table) {
-            $table->id();
-            $table->foreignIdFor(Workday::class);
-            $table->string('type');
-            $table->string('subtype')->nullable();
-            $table->string('name')->nullable();
-            $table->integer('amount');
+        if (! Schema::hasTable('workday_demographics')) {
+            Schema::create('workday_demographics', function (Blueprint $table) {
+                $table->id();
+                $table->foreignIdFor(Workday::class);
+                $table->string('type');
+                $table->string('subtype')->nullable();
+                $table->string('name')->nullable();
+                $table->integer('amount');
 
-            $table->softDeletes();
-            $table->timestamps();
-        });
+                $table->softDeletes();
+                $table->timestamps();
+            });
+        }
     }
 
     /**
