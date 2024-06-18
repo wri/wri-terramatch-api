4f5f31d271dbe3852200b540a8518a7f5eb1a578
Limber Mamani <limbermv2@gmail.com>
2024-06-18 10:53:21 -0400

    [TM-671] fix lint

--- database/migrations/2024_06_07_163000_create_project_pipeline_table.php
+++ database/migrations/2024_06_07_163000_create_project_pipeline_table.php
@@ -24,6 +24,6 @@ return new class extends Migration {
 
     public function down()
     {
-        Schema::dropIfExists('project_pipeline');
+        Schema::dropIfExists('project_pipelines');
     }
 };
