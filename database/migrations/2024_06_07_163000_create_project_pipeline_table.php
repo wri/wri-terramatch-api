7d70cbcc39192b1863e6806f29f06497302002d9
Limber Mamani <limbermv2@gmail.com>
2024-06-18 10:56:21 -0400

    [TM-671] fix lint

--- database/migrations/2024_06_07_163000_create_project_pipeline_table.php
+++ database/migrations/2024_06_07_163000_create_project_pipeline_table.php
@@ -1,11 +1,11 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
-use App\Models\User;
 
-return new class extends Migration {
+return new class () extends Migration {
     public function up()
     {
         Schema::create('project_pipelines', function (Blueprint $table) {
