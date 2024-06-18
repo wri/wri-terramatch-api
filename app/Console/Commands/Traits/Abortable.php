7900f0d9a3211a4757c835d6a077971e913ed4e5
Nathan Curtis <nathan.curtis@gmail.com>
2024-06-18 12:10:57 -0700

    [TM-882] Distinguish between errors that should stop the script and those that shouldn't.

--- app/Console/Commands/Traits/Abortable.php
+++ app/Console/Commands/Traits/Abortable.php
@@ -2,21 +2,52 @@
 
 namespace App\Console\Commands\Traits;
 
-use JetBrains\PhpStorm\NoReturn;
-
 trait Abortable
 {
-    #[NoReturn]
-    protected function abort(string $message, int $exitCode = 1): void
+    protected function executeAbortableScript(callable $execute): void
     {
-        echo $message;
-        exit($exitCode);
+        try {
+            $execute();
+        } catch (AbortException $e) {
+            $this->logException($e);
+            exit($e->exitCode);
+        }
     }
 
-    protected function assert(bool $condition, string $message, int $exitCode = 1): void
+    protected function logException(AbortException $e): void
     {
+        switch ($e->level) {
+            case ExceptionLevel::Warning:
+                $this->warn($e->getMessage());
+
+                break;
+
+            case ExceptionLevel::Error:
+                $this->error($e->getMessage());
+
+                break;
+        }
+    }
+
+    /**
+     * @throws AbortException
+     */
+    protected function abort(string $message, ExceptionLevel $level = ExceptionLevel::Error, $exitCode = 1): void
+    {
+        throw new AbortException($level, $message, $exitCode);
+    }
+
+    /**
+     * @throws AbortException
+     */
+    protected function assert(
+        bool $condition,
+        string $message,
+        ExceptionLevel $level = ExceptionLevel::Error,
+        int $exitCode = 1
+    ): void {
         if (! $condition) {
-            $this->abort($message, $exitCode);
+            $this->abort($message, $level, $exitCode);
         }
     }
 }
