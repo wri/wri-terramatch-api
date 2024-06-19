0511b7d67972b1bf27cc1c6f7e3330eb12bdaa96
Jose Carlos Laura Ramirez <pachonjcl@gmail.com>
2024-06-19 06:30:16 -0400

    attempt to fix test

--- app/Http/Controllers/V2/User/AdminUserController.php
+++ app/Http/Controllers/V2/User/AdminUserController.php
@@ -120,12 +120,12 @@ class AdminUserController extends Controller
 
         $data = $request->all();
 
-        if (! empty($request->get('user_type')) && (Auth::user()->hasRole('admin-super') || Auth::user()->role === 'admin')) {
+        if (! empty($request->get('primary_role')) && (Auth::user()->hasRole('admin-super') || Auth::user()->role === 'admin')) {
             $v1User = V1User::find($user->id);
-            $v1User->syncRoles([$request->get('user_type')]);
-            $user->syncRoles([$request->get('user_type')]);
+            $v1User->syncRoles([$request->get('primary_role')]);
+            $user->syncRoles([$request->get('primary_role')]);
 
-            switch ($request->get('user_type')) {
+            switch ($request->get('primary_role')) {
                 case 'admin-super':
                 case 'admin-ppc':
                     $data['role'] = 'admin';
