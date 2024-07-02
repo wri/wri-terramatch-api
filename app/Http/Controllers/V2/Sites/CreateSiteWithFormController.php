84e6928427a228b9cea2b2f1ddae1a5281d3a913
Limber Mamani <limbermv2@gmail.com>
2024-07-02 12:24:55 -0400

    [TM-951] feat: add stateMachine to site polygon

--- app/Http/Controllers/V2/Sites/CreateSiteWithFormController.php
+++ app/Http/Controllers/V2/Sites/CreateSiteWithFormController.php
@@ -7,7 +7,7 @@ use App\Http\Requests\V2\Forms\CreateEntityFormRequest;
 use App\Models\V2\Forms\Form;
 use App\Models\V2\Projects\Project;
 use App\Models\V2\Sites\Site;
-use App\StateMachines\EntityStatusStateMachine;
+use App\StateMachines\SiteStatusStateMachine;
 use Illuminate\Http\JsonResponse;
 
 class CreateSiteWithFormController extends Controller
@@ -26,7 +26,7 @@ class CreateSiteWithFormController extends Controller
         $site = Site::create([
             'framework_key' => $project->framework_key,
             'project_id' => $project->id,
-            'status' => EntityStatusStateMachine::STARTED,
+            'status' => SiteStatusStateMachine::DRAFT,
         ]);
 
         return $site->createSchemaResource();
