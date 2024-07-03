66c773f719afd9bcba2b3dc63e0c68e9267cfbd4
Jose Carlos Laura Ramirez <pachonjcl@gmail.com>
2024-07-03 10:02:28 -0400

    improve state machine

--- app/StateMachines/SiteStatusStateMachine.php
+++ app/StateMachines/SiteStatusStateMachine.php
@@ -4,25 +4,22 @@ namespace App\StateMachines;
 
 class SiteStatusStateMachine extends EntityStatusStateMachine
 {
-    public const DRAFT = 'draft';
-    public const AWAITING_APPROVAL = 'awaiting-approval';
-    public const APPROVED = 'approved';
-    public const NEEDS_MORE_INFORMATION = 'needs-more-information';
     public const RESTORATION_IN_PROGRESS = 'restoration-in-progress';
 
     public function transitions(): array
     {
+        $parentTransitions = parent::transitions();
+
         return [
-                self::DRAFT => [self::AWAITING_APPROVAL],
-                self::AWAITING_APPROVAL => [self::APPROVED, self::NEEDS_MORE_INFORMATION],
-                self::NEEDS_MORE_INFORMATION => [self::APPROVED, self::RESTORATION_IN_PROGRESS],
-                self::RESTORATION_IN_PROGRESS => [self::NEEDS_MORE_INFORMATION, self::APPROVED],
-                self::APPROVED => [self::NEEDS_MORE_INFORMATION, self::RESTORATION_IN_PROGRESS],
+            $parentTransitions,
+            parent::AWAITING_APPROVAL => [self::RESTORATION_IN_PROGRESS, parent::NEEDS_MORE_INFORMATION],
+            parent::NEEDS_MORE_INFORMATION => [self::RESTORATION_IN_PROGRESS, parent::AWAITING_APPROVAL],
+            self::RESTORATION_IN_PROGRESS => [parent::NEEDS_MORE_INFORMATION, parent::APPROVED],
         ];
     }
 
     public function defaultState(): ?string
     {
-        return self::DRAFT;
+        return parent::STARTED;
     }
 }
