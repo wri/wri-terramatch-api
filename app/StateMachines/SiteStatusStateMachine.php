de34183ee28362d319ae018e88050d3739aeb47d
Limber Mamani <limbermv2@gmail.com>
2024-07-03 12:55:27 -0400

    fix test

--- app/StateMachines/SiteStatusStateMachine.php
+++ app/StateMachines/SiteStatusStateMachine.php
@@ -4,21 +4,16 @@ namespace App\StateMachines;
 
 class SiteStatusStateMachine extends EntityStatusStateMachine
 {
-    public const AWAITING_APPROVAL = 'awaiting-approval';
-    public const APPROVED = 'approved';
-    public const NEEDS_MORE_INFORMATION = 'needs-more-information';
     public const RESTORATION_IN_PROGRESS = 'restoration-in-progress';
 
     public function transitions(): array
     {
-        $parentTransitions = parent::transitions();
-
         return [
-            $parentTransitions,
-            parent::AWAITING_APPROVAL => [self::RESTORATION_IN_PROGRESS, parent::NEEDS_MORE_INFORMATION],
-            parent::NEEDS_MORE_INFORMATION => [self::RESTORATION_IN_PROGRESS, parent::AWAITING_APPROVAL],
-            self::RESTORATION_IN_PROGRESS => [parent::NEEDS_MORE_INFORMATION, parent::APPROVED],
-            self::APPROVED => [self::RESTORATION_IN_PROGRESS, self::AWAITING_APPROVAL],
+            self::STARTED => [self::AWAITING_APPROVAL],
+            self::AWAITING_APPROVAL => [self::APPROVED, self::NEEDS_MORE_INFORMATION],
+            self::NEEDS_MORE_INFORMATION => [self::APPROVED, self::RESTORATION_IN_PROGRESS],
+            self::RESTORATION_IN_PROGRESS => [self::NEEDS_MORE_INFORMATION, self::APPROVED],
+            self::APPROVED => [self::NEEDS_MORE_INFORMATION, self::RESTORATION_IN_PROGRESS],
         ];
     }
 
