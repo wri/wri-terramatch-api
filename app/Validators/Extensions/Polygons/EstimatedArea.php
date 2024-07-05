dd8e07dafbef128e60a8e68d76a62c470a7a86d7
cesarLima1 <cesmaulim@gmail.com>
2024-07-05 09:18:44 -0400

    [TM-973] generate csv with polygon validation

--- app/Validators/Extensions/Polygons/EstimatedArea.php
+++ app/Validators/Extensions/Polygons/EstimatedArea.php
@@ -54,4 +54,37 @@ class EstimatedArea extends Extension
             'total_area_project' => $project->total_hectares_restored_goal,
         ];
     }
+
+    public static function getAreaDataWithSiteID(string $siteUuid): array
+    {
+        $sitePolygon = SitePolygon::where('site_id', $siteUuid)->first();
+        if ($sitePolygon == null) {
+            return ['valid' => false, 'error' => 'Site polygon not found for the given polygon ID', 'status' => 404];
+        }
+
+        $project = $sitePolygon->project;
+        if ($project == null) {
+            return [
+                'valid' => false,
+                'error' => 'Project not found for the given SitePolygon',
+                'sitePolygonId' => $sitePolygon->uuid,
+                'status' => 404,
+            ];
+        }
+
+        if (empty($project->total_hectares_restored_goal)) {
+            return ['valid' => false, 'error' => 'Total hectares restored goal not set for the project', 'status' => 500];
+        }
+
+        $sumEstArea = $project->sitePolygons()->sum('calc_area');
+        $lowerBound = self::LOWER_BOUND_MULTIPLIER * $project->total_hectares_restored_goal;
+        $upperBound = self::UPPER_BOUND_MULTIPLIER * $project->total_hectares_restored_goal;
+        $valid = $sumEstArea >= $lowerBound && $sumEstArea <= $upperBound;
+
+        return [
+            'valid' => $valid,
+            'sum_area_project' => $sumEstArea,
+            'total_area_project' => $project->total_hectares_restored_goal,
+        ];
+    }
 }
