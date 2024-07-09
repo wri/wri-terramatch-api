0f7c5874a6a2e218f1b11dee5bcf96fe4059e178
Limber Mamani <limbermv2@gmail.com>
2024-07-09 15:18:34 -0400

    [TM-840] improve code

--- app/Http/Controllers/V2/Projects/DeleteProjectMonitoringPartnersController.php
+++ app/Http/Controllers/V2/Projects/DeleteProjectMonitoringPartnersController.php
@@ -11,11 +11,17 @@ class DeleteProjectMonitoringPartnersController extends Controller
 {
     public function __invoke(Request $request, Project $project, string $email)
     {
-        $monitoringPartner = ProjectInvite::with('user')
+        $projectUserInvites = ProjectInvite::with('user')
             ->where('project_id', $project->id)
             ->where('email_address', $email)
             ->get();
-        $monitoringPartner->each->delete();
+
+        $monitoringPartner = $projectUserInvites->first()->user;
+        if ($monitoringPartner) {
+            $project->users()->detach($monitoringPartner->id);
+        }
+
+        $projectUserInvites->each->delete();
 
         return response()->json(['message' => 'Monitoring Partner successfully removed.'], 200);
     }
