b164a09b689308a792a4896a88f17d2fa8433fa7
JORGE <egrojmonroy@gmail.com>
2024-07-11 15:07:09 -0400

    [TM-973] fix lint

--- tests/Unit/Validators/Extensions/UploadFilesForValidationsTest.php
+++ tests/Unit/Validators/Extensions/UploadFilesForValidationsTest.php
@@ -10,85 +10,88 @@ use Tests\TestCase;
 
 class UploadFilesForValidationsTest extends TestCase
 {
-  public const FILES_DIR = 'tests/Unit/Validators/Extensions/Polygons/TestFiles/';
-  public function test_upload_shapefile_validate()
-  {
-      $passFilePath = self::FILES_DIR . 'validate_shapefile_csv.zip';
-      $controller = new TerrafundCreateGeometryController();
-      $file = new UploadedFile(
-        $passFilePath,
-        'validate_shapefile_csv.zip',
-        'application/zip',
-        null,
-        true
-    );
+    public const FILES_DIR = 'tests/Unit/Validators/Extensions/Polygons/TestFiles/';
 
-      $request = Request::create(
-        '/api/v2/terrafund/upload-shapefile-validate',
-        'POST',
-        ['file' => $file],
-        [],
-        ['file' => $file]
-    );
+    public function test_upload_shapefile_validate()
+    {
+        $passFilePath = self::FILES_DIR . 'validate_shapefile_csv.zip';
+        $controller = new TerrafundCreateGeometryController();
+        $file = new UploadedFile(
+            $passFilePath,
+            'validate_shapefile_csv.zip',
+            'application/zip',
+            null,
+            true
+        );
 
-      $response = $controller->uploadShapefileWithValidation($request);
+        $request = Request::create(
+            '/api/v2/terrafund/upload-shapefile-validate',
+            'POST',
+            ['file' => $file],
+            [],
+            ['file' => $file]
+        );
 
-      $this->assertTrue($response->status() === 200, "Expected status 200, got {$response->status()}");
-      $this->assertTrue($response->headers->get('Content-Type') === 'text/csv', "Expected Content-Type 'text/csv', got {$response->headers->get('Content-Type')}");
-  }
-  public function test_upload_geojson_validate()
-  {
-      $passFilePath = self::FILES_DIR . 'validate_geojson.csv.geojson';
-      $controller = new TerrafundCreateGeometryController();
-      $tempFilePath = sys_get_temp_dir() . '/temp_' . basename($passFilePath);
-      File::copy($passFilePath, $tempFilePath);
-      $file = new UploadedFile(
-        $tempFilePath,
-        'validate_geojson.csv.geojson',
-        'application/geo+json',
-        null,
-        true
-      );
+        $response = $controller->uploadShapefileWithValidation($request);
 
-      $request = Request::create(
-        '/api/v2/terrafund/upload-geojson-validate',
-        'POST',
-        ['file' => $file],
-        [],
-        ['file' => $file]
-    );
+        $this->assertTrue($response->status() === 200, "Expected status 200, got {$response->status()}");
+        $this->assertTrue($response->headers->get('Content-Type') === 'text/csv', "Expected Content-Type 'text/csv', got {$response->headers->get('Content-Type')}");
+    }
 
-      $response = $controller->uploadGeoJSONFileWithValidation($request);
+    public function test_upload_geojson_validate()
+    {
+        $passFilePath = self::FILES_DIR . 'validate_geojson.csv.geojson';
+        $controller = new TerrafundCreateGeometryController();
+        $tempFilePath = sys_get_temp_dir() . '/temp_' . basename($passFilePath);
+        File::copy($passFilePath, $tempFilePath);
+        $file = new UploadedFile(
+            $tempFilePath,
+            'validate_geojson.csv.geojson',
+            'application/geo+json',
+            null,
+            true
+        );
 
-      $this->assertTrue($response->status() === 200, "Expected status 200, got {$response->status()}");
-      $this->assertTrue($response->headers->get('Content-Type') === 'text/csv', "Expected Content-Type 'text/csv', got {$response->headers->get('Content-Type')}");
-  }
-  public function test_upload_kml_validate()
-  {
-      $passFilePath = self::FILES_DIR . 'validate_kml_csv.kml';
-      $controller = new TerrafundCreateGeometryController();
-      $tempFilePath = sys_get_temp_dir() . '/temp_' . basename($passFilePath);
-      File::copy($passFilePath, $tempFilePath);
-  
-      $file = new UploadedFile(
-          $tempFilePath,
-          'validate_kml_csv.kml',
-          'application/vnd.google-earth.kml+xml',
-          null,
-          true
-      );
-  
-      $request = Request::create(
-        '/api/v2/terrafund/upload-kml-validate',
-        'POST',
-        ['file' => $file],
-        [],
-        ['file' => $file]
-    );
+        $request = Request::create(
+            '/api/v2/terrafund/upload-geojson-validate',
+            'POST',
+            ['file' => $file],
+            [],
+            ['file' => $file]
+        );
 
-      $response = $controller->uploadKMLFileWithValidation($request);
+        $response = $controller->uploadGeoJSONFileWithValidation($request);
 
-      $this->assertTrue($response->status() === 200, "Expected status 200, got {$response->status()}");
-      $this->assertTrue($response->headers->get('Content-Type') === 'text/csv', "Expected Content-Type 'text/csv', got {$response->headers->get('Content-Type')}");
-  }
-} 
+        $this->assertTrue($response->status() === 200, "Expected status 200, got {$response->status()}");
+        $this->assertTrue($response->headers->get('Content-Type') === 'text/csv', "Expected Content-Type 'text/csv', got {$response->headers->get('Content-Type')}");
+    }
+
+    public function test_upload_kml_validate()
+    {
+        $passFilePath = self::FILES_DIR . 'validate_kml_csv.kml';
+        $controller = new TerrafundCreateGeometryController();
+        $tempFilePath = sys_get_temp_dir() . '/temp_' . basename($passFilePath);
+        File::copy($passFilePath, $tempFilePath);
+
+        $file = new UploadedFile(
+            $tempFilePath,
+            'validate_kml_csv.kml',
+            'application/vnd.google-earth.kml+xml',
+            null,
+            true
+        );
+
+        $request = Request::create(
+            '/api/v2/terrafund/upload-kml-validate',
+            'POST',
+            ['file' => $file],
+            [],
+            ['file' => $file]
+        );
+
+        $response = $controller->uploadKMLFileWithValidation($request);
+
+        $this->assertTrue($response->status() === 200, "Expected status 200, got {$response->status()}");
+        $this->assertTrue($response->headers->get('Content-Type') === 'text/csv', "Expected Content-Type 'text/csv', got {$response->headers->get('Content-Type')}");
+    }
+}
