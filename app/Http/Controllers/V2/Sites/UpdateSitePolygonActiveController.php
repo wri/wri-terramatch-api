a6dee124e85e9ad5bf2b5d62d74f0ef1e1e1ae71
Jose Carlos Laura Ramirez <pachonjcl@gmail.com>
2024-07-19 15:33:13 -0400

    restore booleans

--- app/Http/Controllers/V2/Sites/UpdateSitePolygonActiveController.php
+++ app/Http/Controllers/V2/Sites/UpdateSitePolygonActiveController.php
@@ -18,9 +18,9 @@ class UpdateSitePolygonActiveController extends Controller
             if (! $sitePolygon) {
                 return response()->json(['error' => 'Site polygon not found'], 404);
             }
-            SitePolygon::where('primary_uuid', $sitePolygon->primary_uuid)->update(['is_active' => 0]);
+            SitePolygon::where('primary_uuid', $sitePolygon->primary_uuid)->update(['is_active' => false]);
 
-            $sitePolygon->is_active = 1;
+            $sitePolygon->is_active = true;
             $sitePolygon->save();
 
             return response()->json($sitePolygon);
