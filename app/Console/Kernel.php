9e7c0053959b245533c1768d9390e99011ad9bb2
Nathan Curtis <nathan.curtis@gmail.com>
2024-07-19 14:11:29 -0700

    [TM-1106] Move most scheduled commands to run on only one server.

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -12,8 +12,8 @@ class Kernel extends ConsoleKernel
 {
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('remove-verifications')->everyFiveMinutes();
-        $schedule->command('remove-password-resets')->everyFiveMinutes();
+        $schedule->command('remove-verifications')->everyFiveMinutes()->onOneServer();
+        $schedule->command('remove-password-resets')->everyFiveMinutes()->onOneServer();
         /**
          * 2023-08-03 - Stopped the remove uploads script because it's a more reliable
          * fix than attempting to alter the raw SQL statement
@@ -21,14 +21,16 @@ class Kernel extends ConsoleKernel
         // $schedule->command('remove-uploads')->everyFiveMinutes();
         $schedule->command('remove-export-files')->daily();
         $schedule->command('remove-elevator-videos')->everyFiveMinutes();
-        $schedule->command('remove-notifications')->everyFiveMinutes();
-        $schedule->command('remove-notifications-buffers')->everyFiveMinutes();
-        $schedule->command('remove-filter-records')->everyFiveMinutes();
-        $schedule->command('find-matches')->everyMinute();
-        $schedule->command('create-visibility-notifications')->everyFiveMinutes();
-        $schedule->command('check-queue-length')->everyFiveMinutes();
-        $schedule->command('send-upcoming-progress-update-notifications')->daily();
-        $schedule->command('generate-control-site-due-submissions')->weeklyOn(5, '00:00');
+        $schedule->command('remove-notifications')->everyFiveMinutes()->onOneServer();
+        $schedule->command('remove-notifications-buffers')->everyFiveMinutes()->onOneServer();
+        $schedule->command('remove-filter-records')->everyFiveMinutes()->onOneServer();
+        $schedule->command('find-matches')->everyMinute()->onOneServer();
+        $schedule->command('create-visibility-notifications')->everyFiveMinutes()->onOneServer();
+        // This one notifies to Slack, which is not an integration we currently have set up. Leaving it in place with
+        // plans to re-instate it soon.
+        // $schedule->command('check-queue-length')->everyFiveMinutes()->onOneServer();
+        $schedule->command('send-upcoming-progress-update-notifications')->daily()->onOneServer();
+        $schedule->command('generate-control-site-due-submissions')->weeklyOn(5, '00:00')->onOneServer();
 
         // PPC report jobs
         $schedule->job(new CreateTaskDueJob('ppc', 4, 4))->yearlyOn(3, 14);
@@ -46,8 +48,8 @@ class Kernel extends ConsoleKernel
         $schedule->job(new SendSiteAndNurseryRemindersJob('terrafund'))->yearlyOn(5, 30);
         $schedule->job(new SendSiteAndNurseryRemindersJob('terrafund'))->yearlyOn(11, 30);
 
-        $schedule->command('generate-application-export')->twiceDaily(13, 20);
-        $schedule->command('generate-admin-all-entity-records-export')->twiceDaily(13, 20);
+        $schedule->command('generate-application-export')->twiceDaily(13, 20)->onOneServer();
+        $schedule->command('generate-admin-all-entity-records-export')->twiceDaily(13, 20)->onOneServer();
     }
 
     protected function commands()
