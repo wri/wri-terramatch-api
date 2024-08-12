cb4c3670591f25f5eeeb968bea776d8466a39f28
Limber Mamani <limbermv2@gmail.com>
2024-08-12 12:43:47 -0400

    [TM-1176] add version_name field to site_polygon table and retain original polygon name

---
+++ database/migrations/2024_07_05_140225_add_version_name_field_to_site_polygon_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('site_polygon', function (Blueprint $table) {
+            $table->string('version_name')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('site_polygon', function (Blueprint $table) {
+            $table->dropColumn('version_name');
+        });
+    }
+};
