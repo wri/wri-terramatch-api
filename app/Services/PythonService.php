d82b6e8053cb2755cf0845f75c48786d8e4b6b4d
Jorge Monroy <egrojmonroy@gmail.com>
2024-08-14 09:00:12 -0400

    [TM-1185] add python script to clip overlapping polygons (#406)

--- app/Services/PythonService.php
+++ app/Services/PythonService.php
@@ -39,6 +39,37 @@ class PythonService
         return $result;
     }
 
+    public function clipPolygons($geojson): ?array
+    {
+        $inputGeojson = $this->getTemporaryFile('input.geojson');
+        $outputGeojson = $this->getTemporaryFile('output.geojson');
+
+        $writeHandle = fopen($inputGeojson, 'w');
+
+        try {
+            fwrite($writeHandle, json_encode($geojson));
+        } finally {
+            fclose($writeHandle);
+        }
+
+        $process = new Process(['python3', base_path() . '/resources/python/polygon-clip/app.py', $inputGeojson, $outputGeojson]);
+        $process->run(function ($type, $buffer) {
+            echo $buffer;
+        });
+        // $process->run();
+        if (! $process->isSuccessful()) {
+            Log::error('Error running clip script: ' . $process->getErrorOutput());
+
+            return null;
+        }
+
+        $result = json_decode(file_get_contents($outputGeojson), true);
+        unlink($inputGeojson);
+        unlink($outputGeojson);
+
+        return $result;
+    }
+
     protected function getTemporaryFile(string $prefix): string
     {
         return tempnam(sys_get_temp_dir(), $prefix);
