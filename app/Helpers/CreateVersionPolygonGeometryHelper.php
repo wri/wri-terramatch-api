5fe81f8e68bdb5b67ee2b939626588afc482c71f
Limber Mamani <limbermv2@gmail.com>
2024-08-15 15:47:50 -0400

    [TM-1182] move functions to helper

--- app/Helpers/CreateVersionPolygonGeometryHelper.php
+++ app/Helpers/CreateVersionPolygonGeometryHelper.php
@@ -2,9 +2,8 @@
 
 namespace App\Helpers;
 
+use App\Helpers\PolygonGeometryHelper as PolyHelper;
 use App\Models\V2\PolygonGeometry;
-use App\Models\V2\Projects\Project;
-use App\Models\V2\Sites\Site;
 use App\Models\V2\Sites\SitePolygon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -45,63 +44,14 @@ class CreateVersionPolygonGeometryHelper
             $newPolygonVersion = $sitePolygon->createCopy($user, $newGeometryVersion->uuid, false);
 
             if ($newPolygonVersion) {
-                self::updateEstAreainSitePolygon($newGeometryVersion, $geometry);
-                self::updateProjectCentroidFromPolygon($newGeometryVersion);
+                PolyHelper::updateEstAreainSitePolygon($newGeometryVersion, $geometry);
+                PolyHelper::updateProjectCentroidFromPolygon($newGeometryVersion);
                 $newPolygonVersion->changeStatusOnEdit();
             }
 
             return response()->json(['message' => 'Site polygon version created successfully.', 'geometry' => $geometry, 'uuid' => $uuid], 201);
         } catch (\Exception $e) {
             return response()->json(['error' => 'An error occurred: ' . $e->getMessage()], 500);
-        }
-    }
-
-    public static function updateEstAreainSitePolygon($polygonGeometry, $geometry)
-    {
-        try {
-            $sitePolygon = SitePolygon::where('poly_id', $polygonGeometry->uuid)->first();
-
-            if ($sitePolygon) {
-                $geojson = json_encode($geometry);
-                $areaSqDegrees = DB::selectOne("SELECT ST_Area(ST_GeomFromGeoJSON('$geojson')) AS area")->area;
-                $latitude = DB::selectOne("SELECT ST_Y(ST_Centroid(ST_GeomFromGeoJSON('$geojson'))) AS latitude")->latitude;
-                $unitLatitude = 111320;
-                $areaSqMeters = $areaSqDegrees * pow($unitLatitude * cos(deg2rad($latitude)), 2);
-                $areaHectares = $areaSqMeters / 10000;
-
-                $sitePolygon->calc_area = $areaHectares;
-                $sitePolygon->save();
-
-                Log::info("Updated area for site polygon with UUID: $sitePolygon->uuid");
-            } else {
-                Log::warning("Updating Area: Site polygon with UUID $polygonGeometry->uuid not found.");
-            }
-        } catch (\Exception $e) {
-            Log::error('Error updating area in site polygon: ' . $e->getMessage());
-        }
-    }
-
-    public static function updateProjectCentroidFromPolygon($polygonGeometry)
-    {
-        try {
-            $sitePolygon = SitePolygon::where('poly_id', $polygonGeometry->uuid)->first();
-
-            if ($sitePolygon) {
-                $relatedSite = Site::where('uuid', $sitePolygon->site_id)->first();
-                $project = Project::where('id', $relatedSite->project_id)->first();
-
-                if ($project) {
-                    $geometryHelper = new GeometryHelper();
-                    $geometryHelper->updateProjectCentroid($project->uuid);
-
-                } else {
-                    Log::warning("Project with UUID $relatedSite->project_id not found.");
-                }
-            } else {
-                Log::warning("Site polygon with UUID $polygonGeometry->uuid not found.");
-            }
-        } catch (\Exception $e) {
-            Log::error('Error updating project centroid: ' . $e->getMessage());
         }
     }
 }
