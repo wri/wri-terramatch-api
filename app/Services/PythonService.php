47bafd02f97d6326a2d7a4b2491f1c8f43300f45
JORGE <egrojmonroy@gmail.com>
2024-08-16 11:12:54 -0400

    [TM-1185] remove logs from python in response

--- app/Services/PythonService.php
+++ app/Services/PythonService.php
@@ -39,35 +39,67 @@ class PythonService
         return $result;
     }
 
-    public function clipPolygons($geojson): ?array
+    // public function clipPolygons($geojson): ?array
+    // {
+    //     $inputGeojson = $this->getTemporaryFile('input.geojson');
+    //     $outputGeojson = $this->getTemporaryFile('output.geojson');
+
+    //     $writeHandle = fopen($inputGeojson, 'w');
+
+    //     try {
+    //         fwrite($writeHandle, json_encode($geojson));
+    //     } finally {
+    //         fclose($writeHandle);
+    //     }
+
+    //     $process = new Process(['python3', base_path() . '/resources/python/polygon-clip/app.py', $inputGeojson, $outputGeojson]);
+    //     $process->run(function ($type, $buffer) {
+    //         echo $buffer;
+    //     });
+    //     // $process->run();
+    //     if (! $process->isSuccessful()) {
+    //         Log::error('Error running clip script: ' . $process->getErrorOutput());
+
+    //         return null;
+    //     }
+
+    //     $result = json_decode(file_get_contents($outputGeojson), true);
+    //     unlink($inputGeojson);
+    //     unlink($outputGeojson);
+
+    //     return $result;
+    // }
+
+    public function clipPolygons($geojson)
     {
-        $inputGeojson = $this->getTemporaryFile('input.geojson');
-        $outputGeojson = $this->getTemporaryFile('output.geojson');
+        $tempInputFile = tempnam(sys_get_temp_dir(), 'input_geojson_');
+        file_put_contents($tempInputFile, json_encode($geojson));
 
-        $writeHandle = fopen($inputGeojson, 'w');
+        $command = "python3 /path/to/app.py {$tempInputFile} /dev/null 2>&1";
 
-        try {
-            fwrite($writeHandle, json_encode($geojson));
-        } finally {
-            fclose($writeHandle);
+        $process = proc_open($command, [
+            0 => ['pipe', 'r'],
+            1 => ['pipe', 'w'],
+            2 => ['pipe', 'w']
+        ], $pipes);
+
+        $stdout = stream_get_contents($pipes[1]);
+        $stderr = stream_get_contents($pipes[2]);
+
+        fclose($pipes[0]);
+        fclose($pipes[1]);
+        fclose($pipes[2]);
+        proc_close($process);
+
+        unlink($tempInputFile);
+
+        // Log the stderr content (Python log messages)
+        if (!empty($stderr)) {
+            Log::warning("Python script warnings/errors: " . $stderr);
         }
 
-        $process = new Process(['python3', base_path() . '/resources/python/polygon-clip/app.py', $inputGeojson, $outputGeojson]);
-        $process->run(function ($type, $buffer) {
-            echo $buffer;
-        });
-        // $process->run();
-        if (! $process->isSuccessful()) {
-            Log::error('Error running clip script: ' . $process->getErrorOutput());
-
-            return null;
-        }
-
-        $result = json_decode(file_get_contents($outputGeojson), true);
-        unlink($inputGeojson);
-        unlink($outputGeojson);
-
-        return $result;
+        // Return only the stdout content (the actual JSON output)
+        return json_decode($stdout, true);
     }
 
     protected function getTemporaryFile(string $prefix): string
