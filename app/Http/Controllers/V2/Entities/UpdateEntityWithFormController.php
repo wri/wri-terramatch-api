b046a5e09f2e8da09114eb9f74e2e4918f5eba0a
Limber Mamani <limbermv2@gmail.com>
2024-08-20 16:20:50 -0400

    add exception

--- app/Http/Controllers/V2/Entities/UpdateEntityWithFormController.php
+++ app/Http/Controllers/V2/Entities/UpdateEntityWithFormController.php
@@ -11,57 +11,63 @@ use App\StateMachines\UpdateRequestStatusStateMachine;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Log;
+use Exception;
 
 class UpdateEntityWithFormController extends Controller
 {
     public function __invoke(EntityModel $entity, UpdateFormSubmissionRequest $formSubmissionRequest)
     {
-        // $this->authorize('update', $entity);
-        Log::info("next from  authorize");
-        $answers = array_map(function($item) {
-            if (is_string($item)) {
-                if (preg_match('/\b\w+\s*\([^)]*\)/', $item)) {
-                    return htmlspecialchars($item, ENT_QUOTES, 'UTF-8');
+        try {
+            $this->authorize('update', $entity);
+            Log::info("next from  authorize");
+            $answers = array_map(function($item) {
+                if (is_string($item)) {
+                    if (preg_match('/\b\w+\s*\([^)]*\)/', $item)) {
+                        return htmlspecialchars($item, ENT_QUOTES, 'UTF-8');
+                    }
                 }
+
+                return $item;
+            }, data_get($formSubmissionRequest->validate(), 'answers', []));
+            Log::info("next from verify");
+            $form = $entity->getForm();
+            if (empty($form)) {
+                return new JsonResponse('No form schema found for this framework.', 404);
             }
 
-            return $item;
-        }, $formSubmissionRequest->validated()['answers']);
-        // $answers = $formSubmissionRequest['answers'];
-        Log::info("next from verify");
-        $form = $entity->getForm();
-        if (empty($form)) {
-            return new JsonResponse('No form schema found for this framework.', 404);
-        }
+            /** @var UpdateRequest $updateRequest */
+            $updateRequest = $entity->updateRequests()->isUnapproved()->first();
+            $isAdmin = Auth::user()->can("framework-$entity->framework_key");
+            if ($entity->isEditable() || ($isAdmin && empty($updateRequest))) {
+                $entity->updateFromForm($answers);
+                if ($entity instanceof ReportModel) {
+                    $entity->updateInProgress($isAdmin);
+                }
 
-        /** @var UpdateRequest $updateRequest */
-        $updateRequest = $entity->updateRequests()->isUnapproved()->first();
-        $isAdmin = Auth::user()->can("framework-$entity->framework_key");
-        if ($entity->isEditable() || ($isAdmin && empty($updateRequest))) {
-            $entity->updateFromForm($answers);
-            if ($entity instanceof ReportModel) {
-                $entity->updateInProgress($isAdmin);
+                return $entity->createSchemaResource();
+            }
+
+            if (! empty($updateRequest)) {
+                $updateRequest->update([ 'content' => array_merge($updateRequest->content, $answers) ]);
+            } else {
+                UpdateRequest::create([
+                    'organisation_id' => $entity->organisation ? $entity->organisation->id : $entity->project->organisation_id,
+                    'project_id' => $entity->project ? $entity->project->id : $entity->id,
+                    'created_by_id' => Auth::user()->id,
+                    'framework_key' => $entity->framework_key,
+                    'updaterequestable_type' => get_class($entity),
+                    'updaterequestable_id' => $entity->id,
+                    'status' => UpdateRequestStatusStateMachine::DRAFT,
+                    'content' => $answers,
+                ]);
+                $entity->update(['update_request_status' => UpdateRequestStatusStateMachine::DRAFT]);
             }
 
             return $entity->createSchemaResource();
+        } catch (Exception $e) {
+            // Registrar el error y devolver una respuesta de error
+            Log::error('Error updating entity: ' . $e->getMessage());
+            return new JsonResponse('An error occurred while processing the request.', 500);
         }
-
-        if (! empty($updateRequest)) {
-            $updateRequest->update([ 'content' => array_merge($updateRequest->content, $answers) ]);
-        } else {
-            UpdateRequest::create([
-                'organisation_id' => $entity->organisation ? $entity->organisation->id : $entity->project->organisation_id,
-                'project_id' => $entity->project ? $entity->project->id : $entity->id,
-                'created_by_id' => Auth::user()->id,
-                'framework_key' => $entity->framework_key,
-                'updaterequestable_type' => get_class($entity),
-                'updaterequestable_id' => $entity->id,
-                'status' => UpdateRequestStatusStateMachine::DRAFT,
-                'content' => $answers,
-            ]);
-            $entity->update(['update_request_status' => UpdateRequestStatusStateMachine::DRAFT]);
-        }
-
-        return $entity->createSchemaResource();
     }
 }
