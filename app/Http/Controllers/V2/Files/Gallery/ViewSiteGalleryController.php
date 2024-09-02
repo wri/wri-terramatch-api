c6b5e445e6e9c30e38562b06229980992f429ae7
JORGE <egrojmonroy@gmail.com>
2024-09-02 15:12:12 -0400

    [TM-1237] search by name and geotagged value

--- app/Http/Controllers/V2/Files/Gallery/ViewSiteGalleryController.php
+++ app/Http/Controllers/V2/Files/Gallery/ViewSiteGalleryController.php
@@ -7,6 +7,7 @@ use App\Http\Resources\V2\Files\Gallery\GallerysCollection;
 use App\Models\V2\Sites\Site;
 use App\Models\V2\Sites\SiteReport;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 use Spatie\MediaLibrary\MediaCollections\Models\Media;
 use Spatie\QueryBuilder\AllowedFilter;
 use Spatie\QueryBuilder\QueryBuilder;
@@ -19,7 +20,8 @@ class ViewSiteGalleryController extends Controller
 
         $perPage = $request->query('per_page') ?? config('app.pagination_default', 15);
         $entity = $request->query('model_name');
-
+        $searchTerm = $request->query('search');
+        $isGeotagged = $request->query('is_geotagged');
         $models = [];
         ! empty($entity) && $entity != 'sites' ?: $models[] = ['type' => get_class($site), 'ids' => [$site->id]];
         ! empty($entity) && $entity != 'site-reports' ?: $models[] = ['type' => SiteReport::class, 'ids' => $site->reports()->pluck('id')->toArray()];
@@ -32,11 +34,21 @@ class ViewSiteGalleryController extends Controller
                 });
             }
         });
+        if (!empty($searchTerm)) {
+          $ids = Media::search($searchTerm)->get()->pluck('id')->toArray();
+          $mediaQueryBuilder->whereIn('media.id', $ids);
+        }
+        if ($isGeotagged === '1') {
+          $mediaQueryBuilder->whereNotNull('lat')->whereNotNull('lng');
+        } elseif ($isGeotagged === '2') {
+            $mediaQueryBuilder->whereNull('lat')->whereNull('lng');
+        }
+  
 
         $query = QueryBuilder::for($mediaQueryBuilder)
             ->allowedFilters([
                 AllowedFilter::exact('file_type'),
-                AllowedFilter::exact('is_public'),
+                AllowedFilter::exact('is_public')
             ]);
 
         $collection = $query->paginate($perPage)
