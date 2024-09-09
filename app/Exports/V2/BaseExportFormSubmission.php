d42e3547aad334a2fe1828b6afd31ca76f6873c8
Limber Mamani <154026979+LimberHope@users.noreply.github.com>
2024-09-09 15:45:04 -0400

    [TM-1195] accent issue (#451)
    * [TM-1195] change encoding export type
    
    * [TM-1195] add encoding utf 8 to exports
    
    * [TM-1195] add encoding utf 8 to answers exports
    
    * add UTF 8 to export
    
    * fix lint issue
    
    * [TM-1195] fix error in JSON Data Conversion and Encoding
    
    ---------
    
    Co-authored-by: Jose Carlos Laura Ramirez <pachonjcl@gmail.com>

--- app/Exports/V2/BaseExportFormSubmission.php
+++ app/Exports/V2/BaseExportFormSubmission.php
@@ -102,13 +102,21 @@ abstract class BaseExportFormSubmission implements WithHeadings, WithMapping
                     return $this->stringifyModel($answer, ['first_name', 'last_name', 'title', 'gender', 'percent_ownership', 'year_of_birth',]);
 
                 default:
-                    $utf8Answer = mb_convert_encoding($answer, 'UTF-8', mb_detect_encoding($answer));
+                    if (is_string($answer)) {
+                        $utf8Answer = mb_convert_encoding($answer, 'UTF-8', mb_detect_encoding($answer));
+                    } else {
+                        $utf8Answer = $answer;
+                    }
 
                     return json_encode($utf8Answer, JSON_UNESCAPED_UNICODE);
             }
         }
 
-        $utf8Answer = mb_convert_encoding($answer, 'UTF-8', mb_detect_encoding($answer));
+        if (is_string($answer)) {
+            $utf8Answer = mb_convert_encoding($answer, 'UTF-8', mb_detect_encoding($answer));
+        } else {
+            $utf8Answer = $answer;
+        }
 
         return json_encode($utf8Answer, JSON_UNESCAPED_UNICODE);
     }
