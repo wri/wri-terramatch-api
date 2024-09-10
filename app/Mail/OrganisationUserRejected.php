4be38e5fdc0f59883fe8b8d54c230679a07f2c0a
Jose Carlos Laura Ramirez <pachonjcl@gmail.com>
2024-09-10 09:19:35 -0400

    missing construct param

--- app/Mail/OrganisationUserRejected.php
+++ app/Mail/OrganisationUserRejected.php
@@ -3,12 +3,12 @@
 namespace App\Mail;
 
 use App\Models\V2\Organisation;
-use Illuminate\Support\Facades\Auth;
 
 class OrganisationUserRejected extends I18nMail
 {
     public function __construct(Organisation $organisation, $user)
     {
+        parent::__construct($user);
         $this->setSubjectKey('organisation-user-rejected.subject')
             ->setTitleKey('organisation-user-rejected.title')
             ->setBodyKey('organisation-user-rejected.body')
