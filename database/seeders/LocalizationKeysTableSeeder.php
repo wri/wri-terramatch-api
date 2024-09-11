d9b7e557abd4beae3609f883c5a71e715a64024d
Jose Carlos Laura Ramirez <pachonjcl@gmail.com>
2024-09-11 14:56:17 -0400

    change file name

--- database/seeders/LocalizationKeysTableSeeder.php
+++ database/seeders/LocalizationKeysTableSeeder.php
@@ -6,7 +6,7 @@ use App\Helpers\I18nHelper;
 use App\Models\V2\LocalizationKey;
 use Illuminate\Database\Seeder;
 
-class LocalizationKeysSeeder extends Seeder
+class LocalizationKeysTableSeeder extends Seeder
 {
     /**
      * Run the database seeds.
