5f134eca4708903c86a35b22ed7458ea59bee0ed
cesarLima1 <cesmaulim@gmail.com>
2024-09-19 11:58:38 -0400

    [TM-1308] add metadata to image in export image

--- app/Http/Controllers/V2/Exports/ExportImageController.php
+++ app/Http/Controllers/V2/Exports/ExportImageController.php
@@ -5,23 +5,53 @@ namespace App\Http\Controllers\V2\Exports;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
+use Spatie\MediaLibrary\MediaCollections\Models\Media;
+use Intervention\Image\ImageManagerStatic as Image;
+use App\Models\V2\User;
 
 class ExportImageController extends Controller
 {
     public function __invoke(Request $request)
     {
-        $request->validate([
-            'imageUrl' => 'required|url',
-        ]);
-
-        $imageUrl = $request->input('imageUrl');
-
+        $imageUuid = $request->input('uuid');
+        $media = Media::where('uuid', $imageUuid)->firstOrFail();
+        $imageUrl = $media->getFullUrl();
         $imageContent = Http::get($imageUrl)->body();
 
-        $filename = basename($imageUrl);
+        $image = Image::make($imageContent);
 
-        return response($imageContent)
+        $user = User::find($media->model_id);
+        $userName = $user ? $user->name : 'Unknown';
+
+        $metadata = [
+            'name' => $media->name,
+            'is_cover' => $media->is_cover ? true : false,
+            'is_public' => $media->is_public ? true : false,
+            'photographer' => $media->photographer,
+            'description' => $media->description,
+            'created_by' => $userName,
+            'created_at' => $media->created_at->toDateTimeString(),
+            'filename' => $media->file_name,
+            'geotagged' => ($media->lat !== null && $media->lng !== null) ? true : false,
+            'coordinates' => [
+                'lat' => $media->lat,
+                'lng' => $media->lng
+            ]
+        ];
+
+        foreach ($metadata as $key => $value) {
+            if (is_bool($value)) {
+                $value = $value ? 'true' : 'false';
+            } elseif (is_array($value)) {
+                $value = json_encode($value);
+            }
+            $image->exif($key, $value);
+        }
+
+        $modifiedImageContent = $image->encode('jpg')->getEncoded();
+
+        return response($modifiedImageContent)
             ->header('Content-Type', 'image/jpeg')
-            ->header('Content-Disposition', 'attachment; filename="' . $filename . '"');
+            ->header('Content-Disposition', 'attachment; filename="' . $media->file_name . '"');
     }
 }
