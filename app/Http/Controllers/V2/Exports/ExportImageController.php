0fe0cfb92828a9fa9492814b4cc8d90cda6b064e
JORGE <egrojmonroy@gmail.com>
2024-09-20 09:52:03 -0400

    [TM-1308] add other fields

--- app/Http/Controllers/V2/Exports/ExportImageController.php
+++ app/Http/Controllers/V2/Exports/ExportImageController.php
@@ -8,6 +8,8 @@ use Illuminate\Support\Facades\Http;
 use Spatie\MediaLibrary\MediaCollections\Models\Media;
 use Symfony\Component\Process\Exception\ProcessFailedException;
 use Symfony\Component\Process\Process;
+use App\Http\Resources\V2\User\UserLiteResource;
+use App\Models\V2\User;
 
 class ExportImageController extends Controller
 {
@@ -18,27 +20,43 @@ class ExportImageController extends Controller
         $imageUrl = $media->getFullUrl();
         $imageContent = Http::get($imageUrl)->body();
 
-        $tempImagePath = storage_path('app/temp_image.jpg');
+        $extension = pathinfo(parse_url($imageUrl, PHP_URL_PATH), PATHINFO_EXTENSION);
+
+        $tempDir = sys_get_temp_dir();
+        $tempImagePath = $tempDir . DIRECTORY_SEPARATOR . uniqid('image_', true) . '.' . $extension;
+        
         file_put_contents($tempImagePath, $imageContent);
-
+        $user =  new UserLiteResource(User::find($media->created_by));
+        $createdBy = $user->first_name . ' ' . $user->last_name;
         $metadata = [
-            'Title' => $media->name,
-            'Comment' => $media->description,
-            'Author' => $media->photographer,
-            'Latitude' => $media->lat,
-            'Longitude' => $media->lng,
-            'CreateDate' => $media->created_at->toDateTimeString(),
+          'name' => $media->name,
+          'is_cover' => $media->is_cover ? 'true' : 'false',
+          'is_public' => $media->is_public ? 'true' : 'false',
+          'photographer' => $media->photographer, 
+          'description' => $media->description,
+          'uploaded_by' => $media->uploaded_by,
+          'DateCaptured' => $media->date_captured,
+          'filename' => $media->file_name,
+          'geotagged' => ($media->lat !== null && $media->lng !== null) ? 'true' : 'false',
+          'coordinates' => json_encode([
+              'lat' => $media->lat,
+              'lng' => $media->lng
+          ]),
+          'created_by' => $createdBy
         ];
-
+        
         $process = new Process([
             'exiftool',
-            '-Title=' . $metadata['Title'],
-            '-Comment=' . $metadata['Comment'],
-            '-Author=' . $metadata['Author'],
-            '-GPSLatitude=' . $metadata['Latitude'],
-            '-GPSLongitude=' . $metadata['Longitude'],
-            '-CreateDate=' . $metadata['CreateDate'],
-            $tempImagePath,
+            '-name=' . escapeshellarg($metadata['name']),
+            '-is_cover=' . escapeshellarg($metadata['is_cover']),
+            '-is_public=' . escapeshellarg($metadata['is_public']),
+            '-photographer=' . escapeshellarg($metadata['photographer']),
+            '-description=' . escapeshellarg($metadata['description']),
+            '-data_captured=' . escapeshellarg($metadata['DateCaptured']),
+            '-file_name=' . escapeshellarg($metadata['filename']),
+            '-coordinates=' . escapeshellarg($media->lat),
+            '-geotagged=' . escapeshellarg($metadata['geotagged']),
+            '-created_by=' . escapeshellarg($metadata['created_by']),
         ]);
 
         $process->run();
