714def8495c80b4c39cd5c666c48ae76d984db70
Jorge Monroy <egrojmonroy@gmail.com>
2024-09-23 13:33:49 -0400

    [TM-1308] add metadata to image (#475)
    * [TM-1308] add metadata to image in export image
    
    * [TM-1308] add metada to image using exiftool
    
    * [TM-1308] add other fields
    
    * [TM-1308] add exiftool
    
    * [TM-1308] add fields to image metadata
    
    * [TM-1308] avoid null on user when exporting image
    
    * [TM-1308] avoid null on user when exporting image
    
    * [TM-1308] add mail with fix lint
    
    * [TM-1308] lint fix
    
    * xRevert "[TM-1308] lint fix"
    
    This reverts commit e96c2365f177aaff41cbf11c53934ef07d2e4597.
    
    * [TM-1308] revert changes in composer json and lock
    
    * [TM-1308] add exiftool
    
    * [TM-1308] update composer lock
    
    ---------
    
    Co-authored-by: cesarLima1 <cesmaulim@gmail.com>

--- app/Http/Controllers/V2/Exports/ExportImageController.php
+++ app/Http/Controllers/V2/Exports/ExportImageController.php
@@ -3,25 +3,88 @@
 namespace App\Http\Controllers\V2\Exports;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\V2\User\UserLiteResource;
+use App\Models\V2\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Log;
+use Spatie\MediaLibrary\MediaCollections\Models\Media;
+use Symfony\Component\Process\Exception\ProcessFailedException;
+use Symfony\Component\Process\Process;
 
 class ExportImageController extends Controller
 {
     public function __invoke(Request $request)
     {
-        $request->validate([
-            'imageUrl' => 'required|url',
-        ]);
-
-        $imageUrl = $request->input('imageUrl');
-
+        $imageUuid = $request->input('uuid');
+        $media = Media::where('uuid', $imageUuid)->firstOrFail();
+        $imageUrl = $media->getFullUrl();
         $imageContent = Http::get($imageUrl)->body();
 
-        $filename = basename($imageUrl);
+        $extension = pathinfo(parse_url($imageUrl, PHP_URL_PATH), PATHINFO_EXTENSION);
 
-        return response($imageContent)
-            ->header('Content-Type', 'image/jpeg')
-            ->header('Content-Disposition', 'attachment; filename="' . $filename . '"');
+        $tempDir = sys_get_temp_dir();
+        $tempImagePath = $tempDir . DIRECTORY_SEPARATOR . uniqid('image_', true) . '.' . $extension;
+
+        file_put_contents($tempImagePath, $imageContent);
+        $user = new UserLiteResource(User::find($media->created_by));
+        $createdBy = $user->resource ? $user->resource->first_name . ' ' . $user->resource->last_name : 'Unknown';
+
+        $metadata = [
+          'XMP-dc:Title=' . escapeshellarg($media->name),
+          'XMP-dc:Creator=' . escapeshellarg($createdBy),
+          'XMP-dc:Description=' . escapeshellarg($media->description ?? ''),
+          'DateTimeOriginal=' . escapeshellarg($media->created_at ?? ''),
+          'Artist=' . escapeshellarg($media->photographer ?? ''),
+        ];
+
+        if ($media->lat !== null && $media->lng !== null) {
+            $metadata[] = 'GPSLatitude=' . escapeshellarg($media->lat);
+            $metadata[] = 'GPSLongitude=' . escapeshellarg($media->lng);
+        }
+
+        $userComment = json_encode([
+            'IsCover' => $media->is_cover ? 'true' : 'false',
+            'IsPublic' => $media->is_public ? 'true' : 'false',
+            'UploadedBy' => $createdBy,
+            'Filename' => $media->file_name,
+            'Geotagged' => ($media->lat !== null && $media->lng !== null) ? 'true' : 'false',
+            'Name' => $media->name,
+            'Photographer' => $media->photographer ?? '',
+            'Description' => $media->description ?? '',
+            'Date Captured' => $media->created_at,
+            'Coordinates' => [
+                'Latitude' => $media->lat,
+                'Longitude' => $media->lng,
+            ],
+        ]);
+
+        $metadata[] = 'UserComment=' . escapeshellarg($userComment);
+
+        $command = ['exiftool', '-overwrite_original'];
+        foreach ($metadata as $tag) {
+            $command[] = '-' . $tag;
+        }
+        $command[] = $tempImagePath;
+
+        try {
+            $process = new Process($command);
+            $process->run();
+            if (! $process->isSuccessful()) {
+                throw new ProcessFailedException($process);
+            }
+
+            $updatedImageContent = file_get_contents($tempImagePath);
+
+            unlink($tempImagePath);
+
+            return response($updatedImageContent)
+                ->header('Content-Type', 'image/' . $extension)
+                ->header('Content-Disposition', 'attachment; filename="' . $media->file_name . '"');
+        } catch (\Exception $e) {
+            Log::error('Error updating metadata: ' . $e->getMessage());
+
+            return response()->json(['error' => 'An error occurred while processing the image'], 500);
+        }
     }
 }
