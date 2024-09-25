3a64841e438f7214b575e70b72887c59b159f13a
cesarLima1 <105736261+cesarLima1@users.noreply.github.com>
2024-09-25 14:28:35 -0400

    [TM-1320] Upload mp4 video (#478)
    * [TM-1320] add type to accept mp4 files
    
    * [TM-1320] add mp4 to mimes in upload request
    
    * [TM-1320] improve mp4 integration
    
    * [TM-1320] add mp4 integration

--- config/wri/file-handling.php
+++ config/wri/file-handling.php
@@ -5,10 +5,10 @@ return [
         'logo-image' => 'file|mimes:jpg,png',
         'cover-image' => 'file|mimes:jpg,png',
         'cover-image-with-svg' => 'file|mimes:jpg,png,svg',
-        'photos' => 'file|mimes:jpg,png',
+        'photos' => 'file|mimes:jpg,png,mp4',
         'pdf' => 'file|mimes:pdf',
         'documents' => 'file|mimes:pdf,xls,xlsx,csv,txt,doc',
-        'general-documents' => 'file|mimes:pdf,xls,xlsx,csv,txt,png,jpg,doc',
+        'general-documents' => 'file|mimes:pdf,xls,xlsx,csv,txt,png,jpg,doc,mp4',
         'spreadsheet' => 'file|mimes:pdf,xls,xlsx,csv,txt',
     ],
     'validation-file-types' => [
