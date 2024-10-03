f74a9b52591733dc9e63632f72aaab430f941acc
JORGE <egrojmonroy@gmail.com>
2024-10-03 11:12:30 -0400

    [TM-1127] add migration to create table delayed jobs

---
+++ database/migrations/2024_10_03_150604_create_delayed_jobs_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('delayed_jobs', function (Blueprint $table) {
+            $table->id();
+            $table->uuid('uuid')->unique();
+            $table->enum('status', ['pending', 'failed', 'succeeded']);
+            $table->integer('statusCode')->nullable();
+            $table->string('payload');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('delayed_jobs');
+    }
+};
