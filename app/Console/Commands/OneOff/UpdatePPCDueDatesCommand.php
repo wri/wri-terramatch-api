44aa84667e3d122e5892f37ce265834b5d50e64c
Limber Mamani <limbermv2@gmail.com>
2024-10-14 14:27:33 -0400

    [TM-1310] update logic to due_at

--- app/Console/Commands/OneOff/UpdatePPCDueDatesCommand.php
+++ app/Console/Commands/OneOff/UpdatePPCDueDatesCommand.php
@@ -5,6 +5,7 @@ namespace App\Console\Commands\OneOff;
 use App\Models\V2\Tasks\Task;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Log;
+use Carbon\Carbon;
 
 class UpdatePPCDueDatesCommand extends Command
 {
@@ -28,48 +29,29 @@ class UpdatePPCDueDatesCommand extends Command
     public function handle()
     {
         Log::info('update ppc due dates');
-        $datesToUpdate = [
-            '2022-12' => '2023-01-06 04:00:00.000',
-            '2023-01' => '2023-02-03 04:00:00.000',
-            '2023-02' => '2023-03-03 04:00:00.000',
-            '2023-03' => '2023-04-07 04:00:00.000',
-            // Q2 (April-June) 2024
-            '2024-04' => '2024-07-05 04:00:00.000',
-            '2024-05' => '2024-07-05 04:00:00.000',
-            '2024-06' => '2024-07-05 04:00:00.000',
-            // Q3 (July-Sept) 2024
-            '2024-07' => '2024-10-04 04:00:00.000',
-            '2024-08' => '2024-10-04 04:00:00.000',
-            '2024-09' => '2024-10-04 04:00:00.000',
-        ];
 
-        foreach ($datesToUpdate as $reportingPeriod => $correctDueDate) {
-            list($year, $month) = explode('-', $reportingPeriod);
-            $tasks = Task::where(function ($query) use ($reportingPeriod, $year, $month) {
-                $query->where('period_key', $reportingPeriod)
-                      ->orWhere('period_key', $year . '-' . intval($month));
-            })->whereHas('project', function ($query) {
-                $query->where('framework_key', 'ppc');
-            })->get();
-            foreach ($tasks as $task) {
-                $project = $task->project;
-
-                if ($project && $project->framework_key == 'ppc') {
-                    $task->due_at = $correctDueDate;
-                    $task->save();
-                }
-
-                $reports = collect($task->projectReport->get())->concat($task->siteReports)->concat($task->nurseryReports);
-
-                foreach ($reports as $report) {
-                    if ($report->framework_key !== 'ppc') {
-                        continue;
-                    }
-                    $report->due_at = $correctDueDate;
-                    $report->save();
-                }
+        $tasks = Task::whereHas('project', function ($query) {
+            $query->where('framework_key', 'ppc');
+        })->get();
+        foreach ($tasks as $task) {
+            $project = $task->project;
+            if ($project && $project->framework_key == 'ppc') {                    
+                $task->due_at = Carbon::parse($task->due_at)->setTimezone('UTC')->setHour(5);
+                $task->save();
             }
 
+            $reports = collect([$task->projectReport])->concat($task->siteReports)->concat($task->nurseryReports)->filter(function ($report) {
+                return $report !== null;
+            });
+
+            foreach ($reports as $report) {
+                if ($report->framework_key !== 'ppc') {
+                    continue;
+                }
+
+                $report->due_at = Carbon::parse($report->due_at)->setTimezone('UTC')->setHour(5);
+                $report->save();
+            }
         }
 
         $this->info('All due dates updated successfully.');
