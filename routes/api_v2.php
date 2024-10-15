863ec27f5ac65478e41458cdd566e9ec14851e06
JORGE <egrojmonroy@gmail.com>
2024-10-15 10:07:48 -0400

    [TM-1343] endpoint to get data for popup country

--- routes/api_v2.php
+++ routes/api_v2.php
@@ -729,6 +729,7 @@ Route::prefix('dashboard')->group(function () {
     Route::get('/project-data/{uuid}', [CountryDataController::class, 'getProjectData']);
     Route::get('/active-projects', ActiveProjectsTableController::class);
     Route::get('/total-section-header', TotalTerrafundHeaderDashboardController::class);
+    Route::get('/total-section-header/country', [TotalTerrafundHeaderDashboardController::class, 'getTotalDataForCountry']);
     Route::get('/active-countries', ActiveCountriesTableController::class);
     Route::get('/countries', CountriesController::class);
     Route::get('/get-projects', GetProjectsController::class);
