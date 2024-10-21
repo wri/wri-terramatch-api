157e08a7f400e9449bbac94a5d6949cd6fd93055
Limber Mamani <limbermv2@gmail.com>
2024-10-21 15:09:02 -0400

    [TM-1405] add constant to indicator

--- app/Http/Controllers/V2/Indicators/GetHectaresRestoredController.php
+++ app/Http/Controllers/V2/Indicators/GetHectaresRestoredController.php
@@ -12,25 +12,25 @@ class GetHectaresRestoredController extends Controller
     public function __invoke(Request $request)
     {
         $projectsToQuery = TerrafundDashboardQueryHelper::buildQueryFromRequest($request)->pluck('uuid')->toArray();
-        
-        $projectsPolygons = DB::select("
+        $HECTAREAS_BY_RESTORATION = '5';
+        $projectsPolygons = DB::select('
                 SELECT sp.uuid
                 FROM site_polygon sp
                 INNER JOIN v2_sites s ON sp.site_id = s.uuid
                 INNER JOIN v2_projects p ON s.project_id = p.id
-                WHERE p.uuid IN (". implode(',', array_fill(0, count($projectsToQuery), '?')) .")
-            ", $projectsToQuery);
+                WHERE p.uuid IN ('. implode(',', array_fill(0, count($projectsToQuery), '?')) .')
+            ', $projectsToQuery);
 
-        $polygonsUuids = array_map(function($polygon) {
+        $polygonsUuids = array_map(function ($polygon) {
             return $polygon->uuid;
         }, $projectsPolygons);
-        
-        $polygonsToOutputHectares = DB::select("
+
+        $polygonsToOutputHectares = DB::select('
                 SELECT *
                 FROM indicator_output_hectares
-                WHERE indicator_id = '5'
-                AND polygon_id IN (" . implode(',', array_fill(0, count($polygonsUuids), '?')) . ")
-            ", $polygonsUuids);
+                WHERE indicator_id = ?
+                AND polygon_id IN (' . implode(',', array_fill(0, count($polygonsUuids), '?')) . ')
+            ', array_merge([$HECTAREAS_BY_RESTORATION], $polygonsUuids));
 
         $HectaresRestored = [];
 
@@ -39,7 +39,7 @@ class GetHectaresRestoredController extends Controller
 
             if ($decodedValue) {
                 foreach ($decodedValue as $key => $value) {
-                    if (!isset($HectaresRestored[$key])) {
+                    if (! isset($HectaresRestored[$key])) {
                         $HectaresRestored[$key] = 0;
                     }
                     $HectaresRestored[$key] += $value;
