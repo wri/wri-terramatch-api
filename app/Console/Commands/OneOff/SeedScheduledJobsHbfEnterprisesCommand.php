149f6c634d4bd7b6154240235a23f55492eed0c2
Jose Carlos Laura Ramirez <pachonjcl@gmail.com>
2024-10-29 15:57:08 -0400

    [TM-1353] Add in logic for the generation of HBF and TerraFund Enterprise Reporting Tasks (#526)
    * [TM-1353] add OneOff command to seed hbf and enterprises reports
    
    * remove imports
    
    * update hours for to account for tz displacement
    
    * update name
    
    * add one-off command for backdated reports
    
    * fix typo

---
+++ app/Console/Commands/OneOff/SeedScheduledJobsHbfEnterprisesCommand.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Console\Commands\OneOff;
+
+use App\Models\V2\ScheduledJobs\TaskDueJob;
+use Carbon\Carbon;
+use Illuminate\Console\Command;
+
+class SeedScheduledJobsHbfEnterprisesCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'one-off:seed-scheduled-jobs-hbf-enterprises';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Creates the initial definition of scheduled jobs for hbf and enterprises frameworks';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        // HBF reports
+        // May-October, November 1, December 1
+        TaskDueJob::createTaskDue(
+            Carbon::createFromFormat('m', 11)->startOfMonth()->setDay(1),
+            'hbf',
+            Carbon::createFromFormat('m', 12)->startOfMonth()->setDay(1)->setHours(5),
+        );
+        // November-April, May 1, June 1
+        TaskDueJob::createTaskDue(
+            Carbon::createFromFormat('m', 5)->startOfMonth()->setDay(1),
+            'hbf',
+            Carbon::createFromFormat('m', 6)->startOfMonth()->setDay(3)->setHours(5),
+        );
+
+        // Enterprises reports
+        // January-June, July 1, July 30
+        TaskDueJob::createTaskDue(
+            Carbon::createFromFormat('m', 7)->startOfMonth()->setDay(1),
+            'enterprises',
+            Carbon::createFromFormat('m', 7)->startOfMonth()->setDay(30)->setHours(5),
+        );
+        // July-December, January 1, January 30
+        TaskDueJob::createTaskDue(
+            Carbon::createFromFormat('m', 1)->startOfMonth()->setDay(1),
+            'enterprises',
+            Carbon::createFromFormat('m', 1)->startOfMonth()->setDay(30)->setHours(5),
+        );
+
+    }
+}
