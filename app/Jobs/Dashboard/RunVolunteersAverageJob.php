f51c5fe6da4b3c3f5604218afcf2572342a1f4c6
Limber Mamani <154026979+LimberHope@users.noreply.github.com>
2024-11-01 10:17:54 -0400

    [TM-1431] add delayed job for volunteers average (#539)
    * add basic trait
    
    * add basic cache layer
    
    * [TM-1431] add delayed job
    
    * lint fix
    
    * changes
    
    ---------
    
    Co-authored-by: Jose Carlos Laura Ramirez <pachonjcl@gmail.com>

---
+++ app/Jobs/Dashboard/RunVolunteersAverageJob.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Jobs\Dashboard;
+
+use App\Models\DelayedJob;
+use App\Services\Dashboard\RunVolunteersAverageService;
+use Exception;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Redis;
+
+class RunVolunteersAverageJob implements ShouldQueue
+{
+    use Dispatchable;
+    use InteractsWithQueue;
+    use Queueable;
+    use SerializesModels;
+
+    public $timeout = 0;
+
+    protected $uuid;
+
+    protected $delayed_job_id;
+
+    protected $frameworks;
+
+    protected $landscapes;
+
+    protected $organisations;
+
+    protected $country;
+
+    protected $cacheParameter;
+
+    public function __construct(string $delayed_job_id, array $frameworks, array $landscapes, array $organisations, string $country, int $cacheParameter)
+    {
+        $this->delayed_job_id = $delayed_job_id;
+        $this->frameworks = $frameworks;
+        $this->landscapes = $landscapes;
+        $this->organisations = $organisations;
+        $this->country = $country;
+        $this->cacheParameter = $cacheParameter;
+    }
+
+    public function handle(RunVolunteersAverageService $runVolunteersAverageService)
+    {
+        try {
+            $delayedJob = DelayedJob::findOrFail($this->delayed_job_id);
+
+            $request = new Request(
+                [
+                    'filter' => [
+                        'country' => $this->country,
+                        'programmes' => $this->frameworks,
+                        'landscapes' => $this->landscapes,
+                        'organisations.type' => $this->organisations,
+                    ],
+                ]
+            );
+            $response = $runVolunteersAverageService->runVolunteersAverageJob($request);
+            Redis::set('volunteers-survival-rate-' . $this->cacheParameter, json_encode($response));
+
+
+            $delayedJob->update([
+                'status' => DelayedJob::STATUS_SUCCEEDED,
+                'payload' => ['message' => 'Volunteers Average Calculation completed'],
+                'status_code' => Response::HTTP_OK,
+            ]);
+
+        } catch (Exception $e) {
+            Log::error('Error in RunVolunteersAverageJob: ' . $e->getMessage());
+
+            DelayedJob::where('id', $this->delayed_job_id)->update([
+                'status' => DelayedJob::STATUS_FAILED,
+                'payload' => json_encode(['error' => $e->getMessage()]),
+                'status_code' => Response::HTTP_INTERNAL_SERVER_ERROR,
+            ]);
+        }
+    }
+}
