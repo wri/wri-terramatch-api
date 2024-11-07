7a3d9a87113008b5e1e0fd111d1bd7cc298bf4b2
Limber Mamani <limbermv2@gmail.com>
2024-11-07 10:15:59 -0400

    [TM-1455] add doc an docx to mimes

--- app/Http/Requests/V2/File/UploadRequest.php
+++ app/Http/Requests/V2/File/UploadRequest.php
@@ -22,7 +22,7 @@ class UploadRequest extends FormRequest
             ],
             'upload_file' => [
                 'sometimes',
-                'mimes:csv,txt,xls,xlsx,jpg,gif,png,pdf,tiff,svg,mp4',
+                'mimes:csv,txt,xls,xlsx,jpg,gif,png,pdf,tiff,svg,mp4,doc,docx',
             ],
             'collection' => [
                 'sometimes',
