569492618a11823bda1a0ca4dc1d7d26b767b77f
cesarLima1 <cesmaulim@gmail.com>
2024-11-07 17:08:23 -0400

    [TM-1407] add files to projectdetail controller response for dashboard

--- app/Http/Controllers/V2/Dashboard/ProjectProfileDetailsController.php
+++ app/Http/Controllers/V2/Dashboard/ProjectProfileDetailsController.php
@@ -3,32 +3,14 @@
 namespace App\Http\Controllers\V2\Dashboard;
 
 use App\Http\Controllers\Controller;
-use App\Models\V2\Forms\FormOptionListOption;
 use App\Models\V2\Projects\Project;
 use Illuminate\Http\Request;
+use App\Http\Resources\V2\Dashboard\ProjectProfileDetailsResource;
 
 class ProjectProfileDetailsController extends Controller
 {
     public function __invoke(Request $request, Project $project)
     {
-        $response = (object)[
-            'name' => $project->name,
-            'descriptionObjetive' => $project->objectives,
-            'country' => $this->getCountry($project->country),
-            'countrySlug' => $project->country,
-            'organisation' => $project->organisation->type,
-            'survivalRate' => $project->survival_rate,
-            'restorationStrategy' => $project->restoration_strategy,
-            'targetLandUse' => $project->land_use_types,
-            'landTenure' => $project->land_tenure_project_area,
-            'framework' => $project->framework_key,
-        ];
-
-        return response()->json($response);
-    }
-
-    public function getCountry($slug)
-    {
-        return FormOptionListOption::where('slug', $slug)->value('label');
+        return new ProjectProfileDetailsResource($project);
     }
 }
