a1f0fe195c5ef1ffc39c7a5c81c7c8d37b245a7c
JORGE <egrojmonroy@gmail.com>
2024-11-13 12:26:45 -0400

    [TM-1471] add lint

--- app/Console/Commands/BulkApproveProjectPolygonsCommand.php
+++ app/Console/Commands/BulkApproveProjectPolygonsCommand.php
@@ -2,64 +2,66 @@
 
 namespace App\Console\Commands;
 
-use App\Models\V2\Projects\Project;
-use Illuminate\Console\Command;
-use Illuminate\Support\Facades\File;
 use App\Models\Traits\SaveAuditStatusTrait;
+use App\Models\V2\Projects\Project;
 use App\Models\V2\User;
+use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\File;
 use Symfony\Component\Console\Helper\ProgressBar;
 use Symfony\Component\Console\Output\ConsoleOutput;
 
 class BulkApproveProjectPolygonsCommand extends Command
 {
-  use SaveAuditStatusTrait;
+    use SaveAuditStatusTrait;
 
-  protected $signature = 'bulk-approve-project-polygons {file}';
-  protected $description = 'Bulk approve site polygons for projects listed in a CSV file';
+    protected $signature = 'bulk-approve-project-polygons {file}';
 
-  public function handle(): void
-  {
-      $filePath = $this->argument('file');
+    protected $description = 'Bulk approve site polygons for projects listed in a CSV file';
 
-      if (! File::exists($filePath)) {
-          $this->error("CSV file not found at {$filePath}");
-          return;
-      }
-      $userEmail = 'noah.maghsadi@wri.org';
-      $user = User::where('email_address', $userEmail)->first();
-      Auth::login($user);
-      $data = array_map('str_getcsv', file($filePath));
-      $header = array_shift($data);
-      $output = new ConsoleOutput();
-      $progressBar = new ProgressBar($output, count($data));
-      $progressBar->setFormat('Processing: %current% [%bar%] %percent:3s%%');
+    public function handle(): void
+    {
+        $filePath = $this->argument('file');
 
-      $progressBar->start();
+        if (! File::exists($filePath)) {
+            $this->error("CSV file not found at {$filePath}");
 
-      $polygonsChanged = [];
+            return;
+        }
+        $userEmail = 'noah.maghsadi@wri.org';
+        $user = User::where('email_address', $userEmail)->first();
+        Auth::login($user);
+        $data = array_map('str_getcsv', file($filePath));
+        $header = array_shift($data);
+        $output = new ConsoleOutput();
+        $progressBar = new ProgressBar($output, count($data));
+        $progressBar->setFormat('Processing: %current% [%bar%] %percent:3s%%');
 
-      foreach ($data as $row) {
-          $uuid = $row[0];
-          $project = Project::isUuid($uuid)->first();
-          $this->info("\nProcessing project " . $uuid);
-          if ($project) {
-              foreach ($project->sitePolygons as $sitePolygon) {
-                  $sitePolygon->status = 'approved';
-                  $sitePolygon->save();
+        $progressBar->start();
 
-                  $this->saveAuditStatus(get_class($sitePolygon), $sitePolygon->id, $sitePolygon->status, 'Approved via bulk command', 'status');
+        $polygonsChanged = [];
 
-                  $polygonsChanged[] = $sitePolygon;
-              }
-          }
+        foreach ($data as $row) {
+            $uuid = $row[0];
+            $project = Project::isUuid($uuid)->first();
+            $this->info("\nProcessing project " . $uuid);
+            if ($project) {
+                foreach ($project->sitePolygons as $sitePolygon) {
+                    $sitePolygon->status = 'approved';
+                    $sitePolygon->save();
 
-          $progressBar->advance();
-      }
+                    $this->saveAuditStatus(get_class($sitePolygon), $sitePolygon->id, $sitePolygon->status, 'Approved via bulk command', 'status');
 
-      $progressBar->finish();
+                    $polygonsChanged[] = $sitePolygon;
+                }
+            }
 
-      // Print summary of the number of polygons approved
-      $this->info("\n" . count($polygonsChanged) . ' polygons were updated.');
-  }
+            $progressBar->advance();
+        }
+
+        $progressBar->finish();
+
+        // Print summary of the number of polygons approved
+        $this->info("\n" . count($polygonsChanged) . ' polygons were updated.');
+    }
 }
