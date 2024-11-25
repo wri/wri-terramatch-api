3e25ad0ccc23ad8ebdfd9362289b92e80de46aba
JORGE <egrojmonroy@gmail.com>
2024-11-25 14:11:28 -0400

    [TM-1503] remove useless attribut

---
+++ database/migrations/2024_11_25_181031_remove_date_created_from_criteria_site.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class RemoveDateCreatedFromCriteriaSite extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('criteria_site', function (Blueprint $table) {
+            $table->dropColumn('date_created');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('criteria_site', function (Blueprint $table) {
+            $table->timestamp('date_created')->nullable(); // Adjust as necessary
+        });
+    }
+}
