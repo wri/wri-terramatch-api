8d141978457bb0cc626008189fc8d4794567b9f0
JORGE <egrojmonroy@gmail.com>
2024-11-28 16:21:55 -0400

    [TM-1523] lint

--- database/migrations/2024_11_28_200341_add_index_to_site_polygon_poly_id.php
+++ database/migrations/2024_11_28_200341_add_index_to_site_polygon_poly_id.php
@@ -4,19 +4,18 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-  public function up(): void
-  {
-      Schema::table('site_polygon', function (Blueprint $table) {
-          $table->index('poly_id', 'idx_site_polygon_poly_id');
-      });
-  }
+return new class extends Migration {
+    public function up(): void
+    {
+        Schema::table('site_polygon', function (Blueprint $table) {
+            $table->index('poly_id', 'idx_site_polygon_poly_id');
+        });
+    }
 
-  public function down(): void
-  {
-      Schema::table('site_polygon', function (Blueprint $table) {
-          $table->dropIndex('idx_site_polygon_poly_id');
-      });
-  }
+    public function down(): void
+    {
+        Schema::table('site_polygon', function (Blueprint $table) {
+            $table->dropIndex('idx_site_polygon_poly_id');
+        });
+    }
 };
