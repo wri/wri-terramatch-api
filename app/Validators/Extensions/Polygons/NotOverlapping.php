9fa19bf036ab1a5363bf19180bbb064a8b94322f
Jorge Monroy <egrojmonroy@gmail.com>
2024-11-29 14:58:16 -0400

    [TM-1523] improve validation (#592)
    * [TM-1460] add migration to add index
    
    * [TM-1460] add index to polygon geometry and validate overlapping with bbox
    
    * [TM-1460] lint
    
    * [TM-1460] add overlap check with bboxes
    
    * [TM-1523] finish bbox precalculus for overlapping check
    
    * [TM-1523] add index to sitepolygon
    
    * [TM-1523] add initial factory with non empty geometry,for index

--- app/Validators/Extensions/Polygons/NotOverlapping.php
+++ app/Validators/Extensions/Polygons/NotOverlapping.php
@@ -24,19 +24,21 @@ class NotOverlapping extends Extension
     public static function getIntersectionData(string $polygonUuid): array
     {
         $sitePolygon = SitePolygon::forPolygonGeometry($polygonUuid)->first();
-        if ($sitePolygon === null) {
-            return [
-                'valid' => false,
-                'error' => 'Site polygon not found for the given polygon ID',
-                'status' => 404,
-            ];
+        if (! $sitePolygon) {
+            return ['valid' => false, 'error' => 'Site polygon not found', 'status' => 404];
         }
 
         $relatedPolyIds = $sitePolygon->project->sitePolygons()
             ->where('poly_id', '!=', $polygonUuid)
             ->pluck('poly_id');
-        $intersects = PolygonGeometry::join('site_polygon', 'polygon_geometry.uuid', '=', 'site_polygon.poly_id')
+
+        $bboxFilteredPolyIds = PolygonGeometry::join('site_polygon', 'polygon_geometry.uuid', '=', 'site_polygon.poly_id')
             ->whereIn('polygon_geometry.uuid', $relatedPolyIds)
+            ->whereRaw('ST_Intersects(ST_Envelope(polygon_geometry.geom), (SELECT ST_Envelope(geom) FROM polygon_geometry WHERE uuid = ?))', [$polygonUuid])
+            ->pluck('polygon_geometry.uuid');
+
+        $intersects = PolygonGeometry::join('site_polygon', 'polygon_geometry.uuid', '=', 'site_polygon.poly_id')
+            ->whereIn('polygon_geometry.uuid', $bboxFilteredPolyIds)
             ->select([
                 'polygon_geometry.uuid',
                 'site_polygon.poly_name',
@@ -50,28 +52,27 @@ class NotOverlapping extends Extension
 
         $mainPolygonArea = PolygonGeometry::where('uuid', $polygonUuid)
             ->value(DB::raw('ST_Area(geom)'));
-        $extra_info = [];
-        foreach ($intersects as $intersect) {
-            if ($intersect->intersects) {
+
+        $extra_info = $intersects
+            ->filter(fn ($intersect) => $intersect->intersects)
+            ->map(function ($intersect) use ($mainPolygonArea) {
                 $minArea = min($mainPolygonArea, $intersect->area);
-                if ($minArea > 0) {
-                    $percentage = ($intersect->intersection_area / $minArea) * 100;
-                    $percentage = round($percentage, 2);
-                } else {
-                    $percentage = 100;
-                }
-                $extra_info[] = [
+                $percentage = $minArea > 0
+                    ? round(($intersect->intersection_area / $minArea) * 100, 2)
+                    : 100;
+
+                return [
                     'poly_uuid' => $intersect->uuid,
                     'poly_name' => $intersect->poly_name,
                     'percentage' => $percentage,
                     'intersectSmaller' => ($intersect->area < $mainPolygonArea),
                 ];
-            }
-        }
-
+            })
+            ->values()
+            ->toArray();
 
         return [
-            'valid' => ! $intersects->contains('intersects', 1),
+            'valid' => empty($extra_info),
             'uuid' => $polygonUuid,
             'project_id' => $sitePolygon->project_id,
             'extra_info' => $extra_info,
