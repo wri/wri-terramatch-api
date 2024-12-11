379d097024af69915fe166ca4869aff44410f146
Limber Mamani <limbermv2@gmail.com>
2024-12-11 12:11:33 -0400

    remove log

--- app/Http/Controllers/V2/Entities/ViewEntityWithFormController.php
+++ app/Http/Controllers/V2/Entities/ViewEntityWithFormController.php
@@ -4,13 +4,11 @@ namespace App\Http\Controllers\V2\Entities;
 
 use App\Http\Controllers\Controller;
 use App\Models\V2\EntityModel;
-use Illuminate\Support\Facades\Log;
 
 class ViewEntityWithFormController extends Controller
 {
     public function __invoke(EntityModel $entity)
     {
-        Log::info('ViewEntityWithFormController invoked');
         $this->authorize('read', $entity);
 
         return $entity->createSchemaResource();
