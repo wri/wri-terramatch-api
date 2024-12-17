b40c0d7c4ed72396d562b4d3d4f2b51c8a0b9d10
Limber Mamani <limbermv2@gmail.com>
2024-12-17 11:46:13 -0400

    [TM-1466] remove cached functinality from Redis

--- app/Http/Controllers/V2/MonitoredData/RunIndicatorAnalysisController.php
+++ app/Http/Controllers/V2/MonitoredData/RunIndicatorAnalysisController.php
@@ -8,7 +8,6 @@ use App\Jobs\RunIndicatorAnalysisJob;
 use App\Models\DelayedJob;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
-use Illuminate\Support\Facades\Redis;
 
 class RunIndicatorAnalysisController extends Controller
 {
@@ -16,20 +15,15 @@ class RunIndicatorAnalysisController extends Controller
     {
         try {
             $requestData = $request->all();
-            $binary_data = Redis::get('run:indicator|'.$slug.'|'.json_encode($requestData['uuids']));
-            if (! $binary_data) {
-                $delayedJob = DelayedJob::create();
-                $job = new RunIndicatorAnalysisJob(
-                    $delayedJob->id,
-                    $requestData,
-                    $slug
-                );
-                dispatch($job);
+            $delayedJob = DelayedJob::create();
+            $job = new RunIndicatorAnalysisJob(
+                $delayedJob->id,
+                $requestData,
+                $slug
+            );
+            dispatch($job);
 
-                return (new DelayedJobResource($delayedJob))->additional(['message' => 'Analysis for '.$slug.' is being processed']);
-            } else {
-                return response()->json(['message' => 'Analysis for '.$slug.' is already processed'], 200);
-            }
+            return (new DelayedJobResource($delayedJob))->additional(['message' => 'Analysis for '.$slug.' is being processed']);
         } catch (\Exception $e) {
             Log::error('Error during analysis for ' . $slug . ' : ' . $e->getMessage());
 
