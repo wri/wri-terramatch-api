f637124cb2facea5aecb97615d4ddcf785cf516d
JORGE <egrojmonroy@gmail.com>
2025-01-15 11:17:59 -0400

    add per page value for tree species list

--- app/Models/V2/TreeSpecies/TreeSpecies.php
+++ app/Models/V2/TreeSpecies/TreeSpecies.php
@@ -69,8 +69,8 @@ class TreeSpecies extends Model implements EntityRelationModel
         if (! empty($filter['collection'])) {
             $query->where('collection', $filter['collection']);
         }
-
-        return new TreeSpeciesCollection($query->paginate());
+        $perPage = request()->query('per_page', 15);
+        return new TreeSpeciesCollection($query->paginate($perPage));
     }
 
     public function scopeVisible($query): Builder
