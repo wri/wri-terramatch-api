ef5bd7f3949f1b295bc655e8cdbe702eafb5f4c7
Jose Carlos Laura Ramirez <pachonjcl@gmail.com>
2025-01-22 12:01:19 -0400

    [TM-1629] add conditional default for questions (#652)

--- app/Models/V2/Forms/FormQuestion.php
+++ app/Models/V2/Forms/FormQuestion.php
@@ -43,6 +43,7 @@ class FormQuestion extends Model
         'options_list',
         'options_other',
         'show_on_parent_condition',
+        'conditional_default',
     ];
 
     protected $with = [
@@ -55,6 +56,7 @@ class FormQuestion extends Model
         'additional_props' => 'json',
         'options_other' => 'boolean',
         'show_on_parent_condition' => 'boolean',
+        'conditional_default' => 'boolean',
     ];
 
     public function section(): BelongsTo
---
+++ database/migrations/2025_01_21_184458_add_conditional_default_to_form_questions.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('form_questions', function (Blueprint $table) {
+            $table->boolean('conditional_default')->default(true);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('form_questions', function (Blueprint $table) {
+            $table->dropColumn('conditional_default');
+        });
+    }
+};
