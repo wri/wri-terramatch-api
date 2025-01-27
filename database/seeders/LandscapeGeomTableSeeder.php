93b0e554f7432c5b33e2a7dfb0683db6094fd064
JORGE <egrojmonroy@gmail.com>
2025-01-26 20:35:16 -0400

    [TM-1634] create seeder for landscape geom

---
+++ database/seeders/LandscapeGeomTableSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class LandscapeGeomTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+      DB::table('landscape_geom')->delete();
+
+      $geojson = json_decode(file_get_contents(database_path('seeders/Landscapes_polygons.geojson')), true);
+
+      foreach ($geojson['features'] as $feature) {
+          $geometry = json_encode($feature['geometry']);
+          $landscape = $feature['properties']['landscape'];
+
+          DB::table('landscape_geom')->insert([
+              'geometry' => DB::raw("ST_GeomFromGeoJSON('$geometry')"),
+              'landscape' => $landscape,
+          ]);
+      }
+    }
+}
