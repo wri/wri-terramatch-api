9f5c53288946907e06b11f190deed9fcb791ef23
JORGE <egrojmonroy@gmail.com>
2025-01-26 20:24:13 -0400

    [TM-1634] add migration for landscape geometri

---
+++ database/migrations/2025_01_27_002053_create_landscape_geom_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        if (Schema::hasTable('lanscape_geom')) {
+            // assume that if the table already exists in this env we don't want to recreate it.
+            return;
+        }
+        Schema::create('landscape_geom', function (Blueprint $table) {
+            $table->id();
+            $table->geometry('geometry')->spatialIndex('geometry');
+            $table->string('landscape', 50)->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('landscape_geom');
+    }
+};
