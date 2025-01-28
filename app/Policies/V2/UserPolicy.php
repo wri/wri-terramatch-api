f857dc9415f980a850fb2e639acb327878d30c30
Jose Carlos Laura Ramirez <pachonjcl@gmail.com>
2025-01-28 16:22:58 -0400

    [TM-1637] allow framework admins to create edit users

--- app/Policies/V2/UserPolicy.php
+++ app/Policies/V2/UserPolicy.php
@@ -39,7 +39,7 @@ class UserPolicy extends Policy
 
     public function updateRole(?User $user, ?User $model = null): bool
     {
-        return $this->isVerifiedAdmin($user) && ($user->id !== $model->id) && $user->hasRole('admin-super');
+        return $this->isVerifiedAdmin($user) && ($user->id !== $model->id);
     }
 
     public function update(?User $user, ?User $model = null): bool
