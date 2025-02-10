356fa60dcc4bc55f6a2031d36da453a2231e2cb6
JORGE <egrojmonroy@gmail.com>
2025-02-10 12:25:54 -0400

    [TM-1717] add model impact stories

---
+++ app/Models/ImpactStory.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\V2\Organisation;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ImpactStory extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+      'title',
+      'status',
+      'organization_id',
+      'date',
+      'category',
+      'thumbnail',
+      'content',
+  ];
+
+  protected $casts = [
+    'category' => 'array',
+    'content' => 'array',
+  ];
+
+  public function organization()
+  {
+      return $this->belongsTo(Organisation::class);
+  }
+}
