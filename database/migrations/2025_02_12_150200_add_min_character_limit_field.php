22fa8ecc04edc75385fc3c0ab2d0b829c34532eb
Limber Mamani <limbermv2@gmail.com>
2025-02-14 09:20:48 -0400

    [TM-532] add fields to formQuestion to limit option

---
+++ database/migrations/2025_02_12_150200_add_min_character_limit_field.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('form_questions', function (Blueprint $table) {
+            $table->unsignedInteger('min_character_limit')->nullable()->default(90000);
+            $table->unsignedInteger('max_character_limit')->nullable()->default(90000);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('form_questions', function (Blueprint $table) {
+            $table->dropColumn('min_character_limit');
+            $table->dropColumn('max_character_limit');
+        });
+    }
+};
