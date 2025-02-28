ecbc928c870c16fbd8dcf7d673be3ecd695211e0
Nathan Curtis <nathan.curtis@gmail.com>
2025-02-28 15:29:05 -0800

    [TM-1665] Update update request content to the new collection.

--- app/Console/Commands/OneOff/UpdateProjectReportTreeCollections.php
+++ app/Console/Commands/OneOff/UpdateProjectReportTreeCollections.php
@@ -5,6 +5,7 @@ namespace App\Console\Commands\OneOff;
 use App\Models\V2\Forms\FormQuestion;
 use App\Models\V2\Projects\ProjectReport;
 use App\Models\V2\TreeSpecies\TreeSpecies;
+use App\Models\V2\UpdateRequests\UpdateRequest;
 use Illuminate\Console\Command;
 
 class UpdateProjectReportTreeCollections extends Command
@@ -31,9 +32,27 @@ class UpdateProjectReportTreeCollections extends Command
         // The only framework that currently sets tree species on project report is PPC, so we can blanket update them
         TreeSpecies::withTrashed()
             ->where('speciesable_type', ProjectReport::class)
-            ->update(['collection' => 'nursery-seedling']);
+            ->update(['collection' => TreeSpecies::COLLECTION_NURSERY]);
 
         // Updating existing questions is also required to match changes to linked-fields.php; this doesn't happen automatically
-        FormQuestion::where('linked_field_key', 'pro-rep-rel-tree-species')->update(['collection' => 'nursery-seedling']);
+        FormQuestion::where('linked_field_key', 'pro-rep-rel-tree-species')->update(['collection' => TreeSpecies::COLLECTION_NURSERY]);
+
+        // Need to update all in-progress update requests that are affected by the collection change.
+        foreach (FormQuestion::where('linked_field_key', 'pro-rep-rel-tree-species')->pluck('uuid') as $questionUuid) {
+            $this->info("\nUpdating update requests with question: $questionUuid");
+
+            $query = UpdateRequest::where('content', 'like', "%\"$questionUuid\":[%");
+            $this->withProgressBar((clone $query)->count(), function ($progressBar) use ($query, $questionUuid) {
+                foreach (UpdateRequest::where('content', 'like', "%\"$questionUuid\":[%")->get() as $updateRequest) {
+                    $content = $updateRequest->content;
+                    data_set($content, "$questionUuid.*.collection", TreeSpecies::COLLECTION_NURSERY);
+                    $updateRequest->update(['content' => $content]);
+
+                    $progressBar->advance();
+                }
+            });
+        }
+
+        $this->info("\nFinished update request updates");
     }
 }
