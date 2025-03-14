6b4060fe6ebb7c650d2ffc268ad9e07ea46ea5cc
Limber Mamani <limbermv2@gmail.com>
2025-03-13 21:16:22 -0400

    fix

--- app/Console/Commands/OneOff/UpdateSitePolygonsPlantStartField.php
+++ app/Console/Commands/OneOff/UpdateSitePolygonsPlantStartField.php
@@ -5,7 +5,7 @@ namespace App\Console\Commands\OneOff;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\DB;
 
-class AssociateExactMatchTrees extends Command
+class UpdateSitePolygonsPlantStartField extends Command
 {
     /**
      * The name and signature of the console command.
