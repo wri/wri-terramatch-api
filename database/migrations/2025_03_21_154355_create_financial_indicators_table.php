889b98c6cf55e386047dae2b148e00fd5cbec990
JORGE <egrojmonroy@gmail.com>
2025-03-24 09:55:29 -0400

    [TM-1830] lint

--- database/migrations/2025_03_21_154355_create_financial_indicators_table.php
+++ database/migrations/2025_03_21_154355_create_financial_indicators_table.php
@@ -8,16 +8,16 @@ return new class extends Migration {
     public function up()
     {
         Schema::create('financial_indicators', function (Blueprint $table) {
-          $table->id();
-          $table->char('uuid', 36)->unique();
-          $table->unsignedBigInteger('organisation_id');
-          $table->string('collection', 255);
-          $table->decimal('amount', 15, 2);
-          $table->tinyInteger('year')->unsigned();
-          $table->string('documentation')->nullable();
-          $table->text('description')->nullable();
-          $table->timestamps();
-          $table->softDeletes();
+            $table->id();
+            $table->char('uuid', 36)->unique();
+            $table->unsignedBigInteger('organisation_id');
+            $table->string('collection', 255);
+            $table->decimal('amount', 15, 2);
+            $table->tinyInteger('year')->unsigned();
+            $table->string('documentation')->nullable();
+            $table->text('description')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
         });
     }
 
