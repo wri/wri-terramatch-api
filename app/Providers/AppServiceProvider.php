808a1e5a770a4601dde9a13257dd05b57acff2fa
Jorge Monroy <egrojmonroy@gmail.com>
2025-03-28 14:51:12 -0400

    [TM-1869] better handling of failed job (#780)
    * [TM-1869] better handling of failed job
    
    * [TM-1869] lint fix
    
    * [TM-1869] lint
    
    * [TM-1869] remove comment
    
    * [TM-1869] validate null

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,13 +3,18 @@
 namespace App\Providers;
 
 use App\Auth\ServiceAccountGuard;
+use App\Models\DelayedJob;
 use App\Models\V2\Sites\SitePolygon;
 use App\Observers\MediaObserver;
 use App\Observers\SitePolygonObserver;
 use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Queue\Events\JobFailed;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Queue;
 use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
+use ReflectionClass;
 use Spatie\MediaLibrary\MediaCollections\Models\Media;
 
 class AppServiceProvider extends ServiceProvider
@@ -36,5 +41,36 @@ class AppServiceProvider extends ServiceProvider
 
         SitePolygon::observe(SitePolygonObserver::class);
         Media::observe(MediaObserver::class);
+        Queue::failing(function (JobFailed $event) {
+            $jobName = $event->job->resolveName();
+            $jobId = $event->job->uuid();
+            $exceptionMessage = $event->exception->getMessage();
+            $rawBody = $event->job->getRawBody();
+            $payload = json_decode($rawBody, true);
+
+            try {
+                $command = unserialize($payload['data']['command']);
+                $reflection = new ReflectionClass($command);
+                if ($reflection->hasProperty('delayed_job_id')) {
+                    $property = $reflection->getProperty('delayed_job_id');
+                    $property->setAccessible(true);
+                    $delayedJobId = $property->getValue($command);
+                } else {
+                    $delayedJobId = null;
+                }
+            } catch (\Exception $e) {
+                Log::error('Error deserializing command: ' . $e->getMessage());
+
+                return;
+            }
+            $delayedJob = $delayedJobId == null ? null : DelayedJob::where('id', $delayedJobId)->first();
+            if ($delayedJob) {
+                $delayedJob->update([
+                    'status' => DelayedJob::STATUS_FAILED,
+                    'payload' => json_encode(['error' => $exceptionMessage]),
+                    'status_code' => 500,
+                ]);
+            }
+        });
     }
 }
