08a9cb1ab090822c6f42115b1baf97c5a2e3ebd8
Jose Carlos Laura Ramirez <pachonjcl@gmail.com>
2025-04-01 12:01:09 -0400

    [TM-1860] exclude project managers

--- app/Http/Controllers/V2/Projects/ViewProjectMonitoringPartnersController.php
+++ app/Http/Controllers/V2/Projects/ViewProjectMonitoringPartnersController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Http\Resources\V2\User\AssociatedUserResource;
 use App\Models\V2\Projects\Project;
 use App\Models\V2\Projects\ProjectInvite;
+use App\Models\V2\User;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 use Illuminate\Support\Str;
@@ -21,10 +22,9 @@ class ViewProjectMonitoringPartnersController extends Controller
             ->orderByDesc('id')
             ->orderByDesc('accepted_at')
             ->get();
-        
+
         $projectUsersEmails = $project->users()->wherePivot('is_monitoring', true)->pluck('email_address');
 
-        $invitesToProcess = [];
         foreach ($projectUsersEmails as $email_address) {
             $invite = $invites->where('email_address', '==', $email_address)->first();
             if (!$invite) {
@@ -33,18 +33,24 @@ class ViewProjectMonitoringPartnersController extends Controller
                 $data['token'] = $token;
                 $data['email_address'] = $email_address;
                 $newInvite = $project->invites()->create($data);
-                $invitesToProcess[] = $newInvite;
-            } else {
-                $invitesToProcess[] = $invite;
+                $invites->push($newInvite);
             }
         }
 
-        $results = collect($invitesToProcess)->map(function ($invite) use ($invites){
-            if(!is_null($invite->accepted_at)){
-                return $invite;
+        $uniques = $invites->unique('email_address');
+
+        $results = $uniques
+            ->filter(function ($invite) use ($projectUsersEmails){
+                $user = User::where('email_address', $invite->email_address)->first();
+                return $user?->primaryRole?->name != 'project-manager';
+            })
+            ->map(function ($invite) use ($invites){
+                if(!is_null($invite->accepted_at)){
+                    return $invite;
             }
 
             $accepted = $invites->where('email_address', $invite->email_address)->whereNotNull('accepted_at')->first();
+
             return is_null($accepted) ? $invite : $accepted;
         });
 
