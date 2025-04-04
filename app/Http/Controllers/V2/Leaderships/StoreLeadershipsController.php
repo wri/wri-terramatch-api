9de0ba9694fef44a03907de6bc002cf2b3bf9080
LimberHope <limbermv2@gmail.com>
2025-04-03 23:20:00 -0400

    fix lint

--- app/Http/Controllers/V2/Leaderships/StoreLeadershipsController.php
+++ app/Http/Controllers/V2/Leaderships/StoreLeadershipsController.php
@@ -7,8 +7,6 @@ use App\Http\Requests\V2\StoreLeadershipsRequest;
 use App\Http\Resources\V2\LeadershipsResource;
 use App\Models\V2\Leaderships;
 use App\Models\V2\Organisation;
-use \Illuminate\Support\Facades\Log;
-
 
 class StoreLeadershipsController extends Controller
 {
@@ -16,7 +14,7 @@ class StoreLeadershipsController extends Controller
     {
         $model = Organisation::isUuid($storeLeadershipsRequest->organisation_id)->firstOrFail();
         $this->authorize('read', $model);
-        Log::info($model);
+
         $storeLeadershipsRequest['organisation_id'] = $model->id;
         $leaderships = Leaderships::create($storeLeadershipsRequest->all());
 
