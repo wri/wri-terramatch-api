89d27c9b2ba3463e86a976e664532d3fa5d37cfa
cesarLima1 <cesmaulim@gmail.com>
2025-04-09 16:08:01 -0400

    [-TM1902] fix excluded criteria ids in command

--- app/Console/Commands/UpdateSitePolygonValidityCommand.php
+++ app/Console/Commands/UpdateSitePolygonValidityCommand.php
@@ -28,7 +28,7 @@ class UpdateSitePolygonValidityCommand extends Command
      *
      * @var array
      */
-    protected const EXCLUDED_VALIDATION_CRITERIA = [3, 12, 14];
+    protected const EXCLUDED_VALIDATION_CRITERIA = [7, 12, 14];
 
     /**
      * Execute the console command.
