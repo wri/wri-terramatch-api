b9e111df23dd374e880a7ce63a4f3fb95db7f01a
Limber Mamani <154026979+LimberHope@users.noreply.github.com>
2025-04-10 11:06:23 -0400

    [TM-1829] add new field to organisations and project pitches (#806)
    * [TM-1829] add new field to organisations and project pitches
    
    * fix lint

---
+++ database/migrations/2025_04_08_161700_seed_options_list_to_new_fields.php
@@ -0,0 +1,57 @@
+<?php
+
+use App\Models\V2\Forms\FormOptionList;
+use App\Models\V2\Forms\FormOptionListOption;
+use App\Models\V2\I18n\I18nItem;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Str;
+
+return new class () extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        //
+        $collections = [
+            'barriers-project-activity-collection' => ['financial or economic barriers', 'harmful cultural norms', 'inequities in land and tenure rights'],
+            'landowner-agreement-collection' => ['yes i have a written letter of support', 'yes i have secured a verbal agreement', 'no i have not secured prior agreement', 'other'],
+        ];
+
+        foreach ($collections as $key => $items) {
+            $formOptionList = FormOptionList::create(['key' => $key]);
+
+            foreach ($items as $item) {
+                $options = FormOptionListOption::create([
+                    'form_option_list_id' => $formOptionList->id,
+                    'label' => $item,
+                    'slug' => Str::slug($item),
+                ]);
+            }
+        }
+
+        foreach (FormOptionListOption::all() as $option) {
+            $option->label_id = $this->generateIfMissingI18nItem($option, 'label');
+            $option->save();
+        }
+    }
+
+    private function generateIfMissingI18nItem(Model $target, string $property): ?int
+    {
+        $value = trim(data_get($target, $property, false));
+        $short = strlen($value) <= 256;
+        if ($value && data_get($target, $property . '_id', true)) {
+            $i18nItem = I18nItem::create([
+                'type' => $short ? 'short' : 'long',
+                'status' => I18nItem::STATUS_DRAFT,
+                'short_value' => $short ? $value : null,
+                'long_value' => $short ? null : $value,
+            ]);
+
+            return $i18nItem->id;
+        }
+
+        return data_get($target, $property . '_id');
+    }
+};
