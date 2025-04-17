797b8c7672b5dc66dd6c60db2513771fb170c4ed
LimberHope <limbermv2@gmail.com>
2025-04-17 02:42:15 -0400

    [TM-1924] fix lint

--- app/Http/Controllers/V2/FinancialIndicators/GetFinancialIndicatorsController.php
+++ app/Http/Controllers/V2/FinancialIndicators/GetFinancialIndicatorsController.php
@@ -5,8 +5,8 @@ namespace App\Http\Controllers\V2\FinancialIndicators;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\V2\FinancialIndicatorsResource;
 use App\Models\V2\FinancialIndicators;
-use Illuminate\Http\Request;
 use App\Models\V2\Organisation;
+use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\ResourceCollection;
 
 class GetFinancialIndicatorsController extends Controller
@@ -14,7 +14,7 @@ class GetFinancialIndicatorsController extends Controller
     public function __invoke(Request $request, string $organisationId): ResourceCollection
     {
         $organisationId = Organisation::isUuid($organisationId)->value('id');
-        $financialIndicators = FinancialIndicators::where("organisation_id", $organisationId)
+        $financialIndicators = FinancialIndicators::where('organisation_id', $organisationId)
             ->where('collection', $request->collection)->get();
 
         return FinancialIndicatorsResource::collection($financialIndicators);
