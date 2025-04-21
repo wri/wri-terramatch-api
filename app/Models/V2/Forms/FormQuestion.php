33269d803ef867c4ca9d55833237fab32e1a5940
Limber Mamani <154026979+LimberHope@users.noreply.github.com>
2025-04-21 12:48:08 -0400

    [TM-1829] add new field organisation and project pitch (#818)
    * [TM-1829] update input_type
    
    * [TM-1829] add new field to form questions
    
    * [TM-1829] add new options to collections
    
    * [TM-1829] add new options strategy, land use and tenure fields

--- app/Models/V2/Forms/FormQuestion.php
+++ app/Models/V2/Forms/FormQuestion.php
@@ -47,6 +47,8 @@ class FormQuestion extends Model
         'is_parent_conditional_default',
         'min_character_limit',
         'max_character_limit',
+        'min_number_limit',
+        'max_number_limit',
     ];
 
     protected $with = [
@@ -62,6 +64,8 @@ class FormQuestion extends Model
         'is_parent_conditional_default' => 'boolean',
         'min_character_limit' => 'integer',
         'max_character_limit' => 'integer',
+        'min_number_limit' => 'integer',
+        'max_number_limit' => 'integer',
     ];
 
     public function section(): BelongsTo
