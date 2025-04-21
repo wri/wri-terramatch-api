dffb79f9b0be73608edebe3133399de37819ac4d
Jorge Monroy <egrojmonroy@gmail.com>
2025-04-21 10:56:53 -0400

    [TM-1935] add site name (#821)
    * [TM-1935] add site name
    
    * [TM-1935] lint

--- app/Validators/Extensions/Polygons/NotOverlapping.php
+++ app/Validators/Extensions/Polygons/NotOverlapping.php
@@ -52,6 +52,8 @@ class NotOverlapping extends Extension
 
         $mainPolygonArea = PolygonGeometry::where('uuid', $polygonUuid)
             ->value(DB::raw('ST_Area(geom)'));
+
+
         $extra_info = $intersects
             ->filter(function ($intersect) {
                 return $intersect->intersects && $intersect->intersection_area > 1e-10;
@@ -61,12 +63,14 @@ class NotOverlapping extends Extension
                 $percentage = $minArea > 0
                   ? round(($intersect->intersection_area / $minArea) * 100, 2)
                   : 100;
+                $siteInfo = SitePolygon::where('poly_id', $intersect->uuid)->first();
 
                 return [
                     'poly_uuid' => $intersect->uuid,
                     'poly_name' => $intersect->poly_name,
                     'percentage' => $percentage,
                     'intersectSmaller' => ($intersect->area < $mainPolygonArea),
+                    'site_name' => $siteInfo->site->name,
                 ];
             })
             ->values()
