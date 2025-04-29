5dcc0008117a8f9399206ea88ba70c23cbc3c4df
LimberHope <limbermv2@gmail.com>
2025-04-29 10:18:08 -0400

    [TM-1943] remove documentation field

--- app/Http/Requests/V2/StoreFinancialIndicatorsRequest.php
+++ app/Http/Requests/V2/StoreFinancialIndicatorsRequest.php
@@ -16,7 +16,6 @@ class StoreFinancialIndicatorsRequest extends FormRequest
         return [
             'amount' => ['sometimes', 'integer', 'nullable'],
             'year' => ['sometimes', 'integer', 'nullable'],
-            'documentation' => ['sometimes', 'string', 'nullable'],
             'description' => ['sometimes', 'string', 'nullable'],
             'organisation_id' => ['sometimes', 'string', 'exists:organisations,uuid'],
         ];
