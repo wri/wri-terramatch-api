07ddc11715514571429ab40aa9a3841b74045de5
JORGE <egrojmonroy@gmail.com>
2025-05-02 13:44:46 -0400

    [TM-2003] change aff_iso attribute in table and move old iso to there

--- database/migrations/2025_05_02_170530_alter_iso_columns_in_world_countries_generalized_table.php
+++ database/migrations/2025_05_02_170530_alter_iso_columns_in_world_countries_generalized_table.php
@@ -1,19 +1,17 @@
 <?php
 
 use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\Facades\DB;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
-        // Update only the 'iso' column to VARCHAR(3)
         DB::statement('ALTER TABLE world_countries_generalized MODIFY COLUMN iso VARCHAR(3)');
+        
+        DB::statement('ALTER TABLE world_countries_generalized CHANGE COLUMN aff_iso alpha_2_iso VARCHAR(2)');
     }
 
     /**
@@ -21,7 +19,8 @@ return new class extends Migration
      */
     public function down(): void
     {
-        // Revert 'iso' column back to VARCHAR(2)
+        DB::statement('ALTER TABLE world_countries_generalized CHANGE COLUMN alpha_2_iso aff_iso VARCHAR(2)');
+        
         DB::statement('ALTER TABLE world_countries_generalized MODIFY COLUMN iso VARCHAR(2)');
     }
 };
