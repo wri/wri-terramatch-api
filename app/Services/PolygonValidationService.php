b992a8d3ebfc5a077d843f359d3cb522f7646896
Jorge Monroy <egrojmonroy@gmail.com>
2025-05-05 15:26:58 -0400

    [TM-1981] remove plant end (#842)
    * [TM-1981] add migration to remove plant end and remove from code
    
    * [TM-1981] move attributes for polygons to a source of truth constant
    
    * [TM-1981] last places for plantend
    
    * [TM-1981] lint
    
    * [TM-1981] good name of migration file
    
    * [TM-1981] good name of migration file

--- app/Services/PolygonValidationService.php
+++ app/Services/PolygonValidationService.php
@@ -2,6 +2,7 @@
 
 namespace App\Services;
 
+use App\Constants\PolygonFields;
 use App\Models\V2\PolygonGeometry;
 use App\Models\V2\Sites\SitePolygon;
 use App\Validators\Extensions\Polygons\EstimatedArea;
@@ -155,7 +156,7 @@ class PolygonValidationService
     public function validateDataInDB(Request $request)
     {
         $polygonUuid = $request->input('uuid');
-        $fieldsToValidate = ['poly_name', 'plantstart', 'plantend', 'practice', 'target_sys', 'distr', 'num_trees'];
+        $fieldsToValidate = PolygonFields::BASIC_FIELDS;
 
         $sitePolygon = SitePolygon::forPolygonGeometry($polygonUuid)->first();
         if (! $sitePolygon) {
