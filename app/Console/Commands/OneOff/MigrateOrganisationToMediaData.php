3d812b4fe1c3446c1574b2adf497e3b088ea80bb
Jose Carlos Laura Ramirez <pachonjcl@gmail.com>
2025-05-27 03:02:16 -0400

    add one off command

---
+++ app/Console/Commands/OneOff/MigrateOrganisationToMediaData.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Console\Commands\OneOff;
+
+use App\Models\V2\Organisation;
+use App\Models\V2\FundingProgramme;
+use App\Models\V2\FinancialIndicators;
+use Spatie\MediaLibrary\MediaCollections\Models\Media;
+use Illuminate\Console\Command;
+
+class MigrateOrganisationToMediaData extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'one-off:migrate-organisation-to-media-data';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Migrate organisation to media data';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $haritBharatEnterprisesProgrammeUuid = "86b3ea32-8541-4525-b342-2d8010b3cdf7";
+
+        $revenueLabel = 'revenue';
+        $budgetLabel = 'budget';
+
+        $programme = FundingProgramme::isUuid($haritBharatEnterprisesProgrammeUuid)->first();
+        $organisations = $programme->organisations;
+        $forProfitOrganisations = $organisations->where('type', 'for-profit-organization');
+        $nonProfitOrganisations = $organisations->where('type', 'non-profit-organization');
+
+        $this->processOrganisations($forProfitOrganisations, $revenueLabel);
+        $this->processOrganisations($nonProfitOrganisations, $budgetLabel);
+    }
+
+    private function processOrganisations($organisations, $label)
+    {
+
+        $map = [
+            'op_budget_next_year' => 2023,
+            'op_budget_1year' => 2022,
+            'op_budget_2year' => 2021,
+            'op_budget_3year' => 2020,
+        ];
+
+        $organisations->each(function (Organisation $organisation) use ($label, $map) {
+            foreach ($map as $key => $year) {
+                $this->safeUpdate($organisation->id, $label, $key, $year);
+            }
+        });
+    }
+
+    public function safeUpdate($orgId, $label, $key, $year)
+    {
+        $medias = Media::where('model_type', 'App\\Models\\V2\\Organisation')->where('model_id', $orgId)->where('collection', $key)->get();
+        $medias->each(function (Media $media) use ($year, $key, $label) {
+            $financial = FinancialIndicators::where('organisation_id', $media->model_id)->where('year', $year)->where('collection', $label)->update(['amount' => $media->name]);
+            $media->model_type = 'App\\Models\\V2\\FinancialIndicators';
+            $media->model_id = $financial->id;
+            $media->collection = $key;
+            $media->save();
+        });
+    }
+
+}
