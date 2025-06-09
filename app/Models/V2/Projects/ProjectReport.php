929ccf5c58df0fc79e6a6dbdf05484cb52e6c18b
Nathan Curtis <nathan.curtis@gmail.com>
2025-06-09 15:20:36 -0700

    [TM-1994] Add new collections for Fundo Flora

--- app/Models/V2/Projects/ProjectReport.php
+++ app/Models/V2/Projects/ProjectReport.php
@@ -327,14 +327,17 @@ class ProjectReport extends Model implements MediaModel, AuditableContract, Repo
         Demographic::JOBS_TYPE => [
             'full-time' => [
                 DemographicCollections::FULL_TIME,
+                DemographicCollections::FULL_TIME_CLT,
             ],
             'part-time' => [
                 DemographicCollections::PART_TIME,
+                DemographicCollections::PART_TIME_CLT,
             ],
         ],
         Demographic::VOLUNTEERS_TYPE => DemographicCollections::VOLUNTEER,
         Demographic::ALL_BENEFICIARIES_TYPE => DemographicCollections::ALL,
         Demographic::TRAINING_BENEFICIARIES_TYPE => DemographicCollections::TRAINING,
+        Demographic::ASSOCIATES_TYPE => DemographicCollections::ALL,
     ];
 
     public function registerMediaConversions(Media $media = null): void
