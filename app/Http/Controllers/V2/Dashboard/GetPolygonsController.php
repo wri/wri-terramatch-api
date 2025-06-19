0a50293e292f09bb9749d95954709973c76605b6
Jorge Monroy <egrojmonroy@gmail.com>
2025-06-19 15:01:52 -0400

    Revert "[TM-2139] remove endpoint for dashboard polygon statuses (#906)"
    This reverts commit 8e86715f4e37ac1fea299ffaed08fa78c03ff0f0.

--- app/Http/Controllers/V2/Dashboard/GetPolygonsController.php
+++ app/Http/Controllers/V2/Dashboard/GetPolygonsController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers\V2\Dashboard;
 
 use App\Helpers\GeometryHelper;
+use App\Helpers\TerrafundDashboardQueryHelper;
 use App\Http\Controllers\Controller;
+use App\Http\Resources\V2\Dashboard\GetPolygonsResource;
+use Illuminate\Http\Request;
 
 class GetPolygonsController extends Controller
 {
@@ -12,5 +15,17 @@ class GetPolygonsController extends Controller
         $centroid = GeometryHelper::centroidOfPolygon($polyUuid);
 
         return response()->json(['centroid' => $centroid]);
+    }
+
+    public function getPolygonsDataByStatusOfProject(Request $request): GetPolygonsResource
+    {
+        $polygonsIdsByStatus = TerrafundDashboardQueryHelper::getPolygonsByStatusOfProjects($request);
+        $polygonsIds = array_values($polygonsIdsByStatus)[0];
+        $centroids = GeometryHelper::getCentroidsOfPolygons($polygonsIds);
+
+        return new GetPolygonsResource([
+          'data' => $polygonsIdsByStatus,
+          'centroids' => $centroids,
+        ]);
     }
 }
