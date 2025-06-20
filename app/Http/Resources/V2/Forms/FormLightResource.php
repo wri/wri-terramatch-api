42e5da51f9408ae1610f7b56c9f1309941c24c6a
Jose Carlos Laura Ramirez <pachonjcl@gmail.com>
2025-06-20 17:15:17 -0400

    [TM-2163] forms admin side timing out (#914)
    * [TM-2163] forms admin side timing out
    
    * fix lint

---
+++ app/Http/Resources/V2/Forms/FormLightResource.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Resources\V2\Forms;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class FormLightResource extends JsonResource
+{
+    public function toArray($request)
+    {
+        $data = [
+            'uuid' => $this->uuid,
+            'title' => $this->title,
+            'type' => $this->type,
+            'published' => $this->published,
+        ];
+
+        return $this->appendFilesToResource($data);
+    }
+}
