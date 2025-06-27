8fd424c136d6fecf747c5f5c4a82f0e5403fe773
JORGE <egrojmonroy@gmail.com>
2025-06-27 10:51:33 -0400

    [TM-2204] add migration for lat long centroid site polygon and command to fill data

---
+++ database/migrations/2025_06_27_140329_add_lat_lng_to_site_polygons.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('site_polygon', function (Blueprint $table) {
+            $table->decimal('lat', 20, 16)->nullable()->after('status');
+            $table->decimal('lng', 20, 16)->nullable()->after('lat');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('site_polygon', function (Blueprint $table) {
+            $table->dropColumn(['lat', 'lng']);
+        });
+    }
+};
