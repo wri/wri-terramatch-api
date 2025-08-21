acb909c0aabcb30f1f7700e32e6f9b8940d8d25c
cesarLima1 <cesmaulim@gmail.com>
2025-08-21 11:19:14 -0400

    [TM-2400] update function to calculate area instead of using python script

--- app/Services/AreaCalculationService.php
+++ app/Services/AreaCalculationService.php
@@ -11,17 +11,18 @@ class AreaCalculationService
     {
         try {
             $geojson = json_encode($geometry);
-            $result = DB::selectOne("
+            $result = DB::selectOne('
                 SELECT 
                     ST_Area(ST_GeomFromGeoJSON(?)) * 
                     POW(6378137 * PI() / 180, 2) * 
                     COS(RADIANS(ST_Y(ST_Centroid(ST_GeomFromGeoJSON(?))))) / 10000 as area_hectares
-            ", [$geojson, $geojson]);
+            ', [$geojson, $geojson]);
 
             return (float) $result->area_hectares;
 
         } catch (\Exception $e) {
             Log::error('Error calculating area: ' . $e->getMessage());
+
             throw new \RuntimeException('Area calculation failed: ' . $e->getMessage());
         }
     }
@@ -30,21 +31,22 @@ class AreaCalculationService
     {
         try {
             $geojson = json_encode($geometry);
-            $result = DB::selectOne("
+            $result = DB::selectOne('
                 SELECT 
                     ST_GeomFromGeoJSON(?) as geom,
                     ST_Area(ST_GeomFromGeoJSON(?)) * 
                     POW(6378137 * PI() / 180, 2) * 
                     COS(RADIANS(ST_Y(ST_Centroid(ST_GeomFromGeoJSON(?))))) / 10000 as area_hectares
-            ", [$geojson, $geojson, $geojson]);
+            ', [$geojson, $geojson, $geojson]);
 
             return [
                 'geom' => DB::raw("ST_GeomFromGeoJSON('" . addslashes($geojson) . "')"),
-                'area' => (float) $result->area_hectares
+                'area' => (float) $result->area_hectares,
             ];
 
         } catch (\Exception $e) {
             Log::error('Error calculating geometry and area: ' . $e->getMessage());
+
             throw new \RuntimeException('Geometry and area calculation failed: ' . $e->getMessage());
         }
     }
