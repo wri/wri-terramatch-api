0aa30272937185f6d6e03fd49c551214205984e9
JORGE <egrojmonroy@gmail.com>
2025-08-22 16:27:38 -0400

    [TM-2398] batch processing for multiple instances, remove indicators

--- app/Services/AreaCalculationService.php
+++ app/Services/AreaCalculationService.php
@@ -33,8 +33,10 @@ class AreaCalculationService
             $geojson = json_encode($geometry);
             $result = DB::selectOne('
                 SELECT 
-                    ST_GeomFromGeoJSON(?) as geom,
-                    ST_Area(ST_GeomFromGeoJSON(?)) * 
+                    ? as geom_json,
+                    ST_Area(
+                        ST_GeomFromGeoJSON(?)
+                    ) * 
                     POW(6378137 * PI() / 180, 2) * 
                     COS(RADIANS(ST_Y(ST_Centroid(ST_GeomFromGeoJSON(?))))) / 10000 as area_hectares
             ', [$geojson, $geojson, $geojson]);
@@ -48,6 +50,54 @@ class AreaCalculationService
             Log::error('Error calculating geometry and area: ' . $e->getMessage());
 
             throw new \RuntimeException('Geometry and area calculation failed: ' . $e->getMessage());
+        }
+    }
+    public function batchGetGeomsAndAreas(array $geometries): array
+    {
+        if (empty($geometries)) {
+            return [];
+        }
+
+        try {
+            $placeholders = [];
+            $params = [];
+            $results = [];
+
+            foreach ($geometries as $index => $geometry) {
+                $geojson = json_encode($geometry);
+                $placeholders[] = "
+                    SELECT 
+                        ? as geom_json,
+                        ST_Area(
+                            ST_GeomFromGeoJSON(?)
+                        ) * 
+                        POW(6378137 * PI() / 180, 2) * 
+                        COS(RADIANS(ST_Y(ST_Centroid(ST_GeomFromGeoJSON(?))))) / 10000 as area_hectares,
+                        ? as batch_index
+                ";
+                $params = array_merge($params, [$geojson, $geojson, $geojson, $index]);
+            }
+
+            $sql = implode(' UNION ALL ', $placeholders);
+            $dbResults = DB::select($sql, $params);
+
+            foreach ($dbResults as $dbResult) {
+                $index = $dbResult->batch_index;
+                $geojson = $dbResult->geom_json;
+                $results[$index] = [
+                    'geom' => DB::raw("ST_GeomFromGeoJSON('" . addslashes($geojson) . "')"),
+                    'area' => (float) $dbResult->area_hectares,
+                ];
+            }
+
+            return $results;
+
+        } catch (\Exception $e) {
+            $results = [];
+            foreach ($geometries as $index => $geometry) {
+                $results[$index] = $this->getGeomAndArea($geometry);
+            }
+            return $results;
         }
     }
 }
