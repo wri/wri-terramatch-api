44ed898ef1c531538dfaa3f7be1f4f575106e8d5
LimberHope <limbermv2@gmail.com>
2025-09-03 10:15:43 -0400

    [TM-2367] update fields

--- app/Models/V2/DisturbanceReport.php
+++ app/Models/V2/DisturbanceReport.php
@@ -3,7 +3,11 @@
 namespace App\Models\V2;
 
 use App\Models\Traits\HasEntityResources;
+use App\Models\Traits\HasReportStatus;
+use App\Models\Traits\HasUpdateRequests;
 use App\Models\Traits\HasUuid;
+use App\Models\Traits\HasV2MediaCollections;
+use App\Models\Traits\UsesLinkedFields;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\MorphTo;
@@ -12,43 +16,43 @@ use OwenIt\Auditing\Auditable;
 use OwenIt\Auditing\Contracts\Auditable as AuditableContract;
 use Spatie\MediaLibrary\InteractsWithMedia;
 use Spatie\MediaLibrary\MediaCollections\Models\Media;
+use Znck\Eloquent\Traits\BelongsToThrough as BelongsToThroughTrait;
 
-class DisturbanceReport extends Model implements AuditableContract
+class DisturbanceReport extends Model implements MediaModel, ReportModel, AuditableContract
 {
     use HasFactory;
     use HasUuid;
     use SoftDeletes;
+    use HasReportStatus;
+    use HasUpdateRequests;
+    use UsesLinkedFields;
     use InteractsWithMedia;
+    use HasV2MediaCollections;
     use HasEntityResources;
     use Auditable;
+    use BelongsToThroughTrait;
 
     protected $table = 'disturbance_reports';
 
     protected $fillable = [
-        'uuid',
-        'disturbanceable_type',
-        'disturbanceable_id',
-        'disturbance_date',
-        'collection',
-        'type',
-        'subtype',
+        'status',
+        'date_of_incident',
         'intensity',
-        'extent',
-        'people_affected',
-        'monetary_damage',
-        'description',
-        'action_description',
-        'property_affected',
-        'old_id',
-        'old_model',
-        'hidden',
+        'title',
+        'update_request_status',
+        'submitted_at',
+        'due_at',
+        'feedback',
+        'feedback_fields',
+        'answers',
     ];
 
     protected $casts = [
-        'disturbance_date' => 'date',
-        'people_affected'  => 'integer',
-        'monetary_damage'  => 'decimal:2',
-        'hidden'           => 'boolean',
+        'due_at' => 'datetime',
+        'submitted_at' => 'datetime',
+        'date_of_incident' => 'date',
+        'nothing_to_report' => 'boolean',
+        'answers' => 'array',
     ];
 
     public $shortName = 'disturbance-report';
