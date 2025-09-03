a366b6db498960a5c4642b780d06c44c6b07e45c
Nathan Curtis <nathan.curtis@gmail.com>
2025-09-03 10:29:21 -0700

    [TM-2414] Remove form from the entity with schema resource, and replace with form uuid.

--- app/Http/Resources/V2/Entities/EntityWithSchemaResource.php
+++ app/Http/Resources/V2/Entities/EntityWithSchemaResource.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Resources\V2\Entities;
 
-use App\Http\Resources\V2\Forms\FormResource;
 use App\Http\Resources\V2\UpdateRequests\UpdateRequestResource;
 use Illuminate\Http\Resources\Json\JsonResource;
 
@@ -10,17 +9,12 @@ class EntityWithSchemaResource extends JsonResource
 {
     public function toArray($request)
     {
-        $params = [
-            'model_uuid' => $this->uuid,
-            'model' => $this,
-        ];
-
         $updateRequest = $this->updateRequests()->isUnapproved()->first();
 
         return [
             'uuid' => $this->uuid,
             'name' => $this->name,
-            'form' => (new FormResource($this->getForm()))->params($params),
+            'form_uuid' => $this->getForm()->uuid,
             'answers' => $this->getEntityAnswers($this->getForm()),
             'status' => $this->status,
             'form_title' => $this->report_title ?? $this->title ?? $this->name,
