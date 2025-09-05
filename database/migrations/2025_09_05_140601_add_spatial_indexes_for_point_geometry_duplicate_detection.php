2ab0422eb1f9cde4be6dafe5bb4ba9df5c50f9cb
JORGE <egrojmonroy@gmail.com>
2025-09-05 15:58:32 -0400

    [TM-2456] remove comments

--- database/migrations/2025_09_05_140601_add_spatial_indexes_for_point_geometry_duplicate_detection.php
+++ database/migrations/2025_09_05_140601_add_spatial_indexes_for_point_geometry_duplicate_detection.php
@@ -9,10 +9,7 @@ return new class extends Migration {
     public function up(): void
     {
 
-        // Modificar columna para que sea NOT NULL (sin SRID en MariaDB)
         DB::statement('ALTER TABLE point_geometry MODIFY geom GEOMETRY NOT NULL');
-
-        // Crear índice espacial
         DB::statement('CREATE SPATIAL INDEX point_geometry_geom_spatial_idx ON point_geometry(geom)');
 
         Schema::table('site_polygon', function (Blueprint $table) {
