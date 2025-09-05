721e3ff4af79632eb6816e5000dc90539fff4c4a
JORGE <egrojmonroy@gmail.com>
2025-09-05 15:29:58 -0400

    [TM-2456] add not null to geom as it is needed for spatial index

--- database/migrations/2025_09_05_140601_add_spatial_indexes_for_point_geometry_duplicate_detection.php
+++ database/migrations/2025_09_05_140601_add_spatial_indexes_for_point_geometry_duplicate_detection.php
@@ -7,8 +7,14 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration {
     public function up(): void
-    {
+    { 
+        
+        // Modificar columna para que sea NOT NULL (sin SRID en MariaDB)
+        DB::statement('ALTER TABLE point_geometry MODIFY geom GEOMETRY NOT NULL');
+        
+        // Crear índice espacial
         DB::statement('CREATE SPATIAL INDEX point_geometry_geom_spatial_idx ON point_geometry(geom)');
+        
         Schema::table('site_polygon', function (Blueprint $table) {
             $table->index('point_id', 'idx_site_polygon_point_id');
         });
@@ -17,8 +23,8 @@ return new class extends Migration {
     public function down(): void
     {
         DB::statement('DROP INDEX point_geometry_geom_spatial_idx ON point_geometry');
-
-        Schema::table('site_polygon', function (Blueprint $table) {
+        DB::statement('ALTER TABLE point_geometry MODIFY geom GEOMETRY NULL');
+        Schema::table('site_polygon', function (Blueprint $table) { 
             $table->dropIndex('idx_site_polygon_point_id');
         });
     }
