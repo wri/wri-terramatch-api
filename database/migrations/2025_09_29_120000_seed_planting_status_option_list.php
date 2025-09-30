43449ffc464da0e3fec99221b9a1b72cd00dab99
Marcolr-dev <marc.antoni.lr@gmail.com>
2025-09-30 16:02:49 -0400

    Fix migration

--- database/migrations/2025_09_29_120000_seed_planting_status_option_list.php
+++ database/migrations/2025_09_29_120000_seed_planting_status_option_list.php
@@ -15,7 +15,7 @@ return new class () extends Migration {
     {
         $key = 'planting-status';
 
-        if (!FormOptionList::where('key', $key)->exists()) {
+        if (! FormOptionList::where('key', $key)->exists()) {
             $formOptionList = FormOptionList::create(['key' => $key]);
 
             $items = [
