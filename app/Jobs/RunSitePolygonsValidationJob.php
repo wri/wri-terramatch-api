07bc82e9c49872007653740285db687b2a1b699e
Jorge Monroy <egrojmonroy@gmail.com>
2025-09-30 19:08:43 -0400

    [TM-2337] revert changes and change only variables (#1073)
    * [TM-2337] revert changes and change only variables
    
    * [TM-2337] remove should be uniquer
    
    * [TM-2337] make changes remove unique

--- app/Jobs/RunSitePolygonsValidationJob.php
+++ app/Jobs/RunSitePolygonsValidationJob.php
@@ -10,7 +10,6 @@ use App\Services\PolygonService;
 use App\Services\PolygonValidationService;
 use Exception;
 use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldBeUnique;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Http\Request;
@@ -21,7 +20,7 @@ use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Mail;
 
-class RunSitePolygonsValidationJob implements ShouldQueue, ShouldBeUnique
+class RunSitePolygonsValidationJob implements ShouldQueue
 {
     use Dispatchable;
     use InteractsWithQueue;
@@ -32,38 +31,26 @@ class RunSitePolygonsValidationJob implements ShouldQueue, ShouldBeUnique
 
     public $tries = 1;
 
-    public $uniqueFor = 3600;
-
     protected $uuid;
 
     protected $delayed_job_id;
 
     protected $sitePolygonsUuids;
 
-    protected $chunkSize = 3;
+    protected $chunkSize = 5;
 
-    protected $memoryClearFrequency = 2;
-
-    protected $safetyBuffer = 20;
+    protected $memoryClearFrequency = 10;
 
     /**
      * Create a new job instance.
      *
      * @return void
      */
-    public function __construct(string $delayed_job_id, array $sitePolygonsUuids, int $chunkSize = 3)
+    public function __construct(string $delayed_job_id, array $sitePolygonsUuids, int $chunkSize = 5)
     {
         $this->sitePolygonsUuids = $sitePolygonsUuids;
         $this->delayed_job_id = $delayed_job_id;
         $this->chunkSize = $chunkSize;
-    }
-
-    /**
-     * The unique ID of the job.
-     */
-    public function uniqueId(): string
-    {
-        return $this->delayed_job_id;
     }
 
     /**
@@ -101,7 +88,7 @@ class RunSitePolygonsValidationJob implements ShouldQueue, ShouldBeUnique
                 try {
                     foreach ($polygonChunk as $polygonIndex => $polygonUuid) {
                         $elapsedTime = microtime(true) - $jobStartTime;
-                        if ($elapsedTime > ($this->timeout - $this->safetyBuffer)) {
+                        if ($elapsedTime > 40) {
                             Log::warning("Job approaching timeout after {$elapsedTime}s, stopping at polygon {$processedCount}");
 
                             break 2;
@@ -141,7 +128,7 @@ class RunSitePolygonsValidationJob implements ShouldQueue, ShouldBeUnique
                 $continuationJob = new RunSitePolygonsValidationJob($this->delayed_job_id, $remainingPolygons, $this->chunkSize);
                 dispatch($continuationJob)->delay(now()->addSeconds(2));
 
-                Log::info("Partial completion: processed {$processedCount}/{$totalPolygons} polygons. Scheduled continuation job with {$this->chunkSize} chunk size.");
+                Log::info("Partial completion: processed {$processedCount}/{$totalPolygons} polygons. Scheduled continuation job.");
 
                 $delayedJob->update([
                     'status' => DelayedJobProgress::STATUS_PENDING,
@@ -203,7 +190,6 @@ class RunSitePolygonsValidationJob implements ShouldQueue, ShouldBeUnique
             $polygonService->updateSitePolygonValidity($polygonUuid);
         } catch (Exception $e) {
             Log::error("Error validating polygon {$polygonUuid}: " . $e->getMessage());
-            Log::error('Polygon validation error trace: ' . $e->getTraceAsString());
 
             throw $e;
         }
