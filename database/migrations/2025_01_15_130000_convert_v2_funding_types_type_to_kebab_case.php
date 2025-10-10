bbb288dfc61bb3eddc6c9f528dbc5055e4a0939f
Marcolr-dev <marc.antoni.lr@gmail.com>
2025-10-10 16:04:42 -0400

    Fix

--- database/migrations/2025_01_15_130000_convert_v2_funding_types_type_to_kebab_case.php
+++ database/migrations/2025_01_15_130000_convert_v2_funding_types_type_to_kebab_case.php
@@ -1,12 +1,9 @@
 <?php
 
 use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\Facades\DB;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
