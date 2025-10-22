c2c82b2efa88226d03277194520b528b0748135f
LimberHope <limbermv2@gmail.com>
2025-10-22 14:00:27 -0400

    [TM-2504] add delayed job for view application

--- app/Http/Controllers/V2/Applications/AdminViewApplicationController.php
+++ app/Http/Controllers/V2/Applications/AdminViewApplicationController.php
@@ -3,16 +3,33 @@
 namespace App\Http\Controllers\V2\Applications;
 
 use App\Http\Controllers\Controller;
-use App\Http\Resources\V2\Applications\ApplicationResource;
 use App\Models\V2\Forms\Application;
+use App\Jobs\RunGetApplicationJob;
+use App\Models\DelayedJob;
+use App\Http\Resources\DelayedJobResource;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
 
 class AdminViewApplicationController extends Controller
 {
-    public function __invoke(Request $request, Application $application): ApplicationResource
+    public function __invoke(Request $request, Application $application)
     {
-        $this->authorize('readAll', Application::class);
+        try {
+            $this->authorize('readAll', Application::class);
 
-        return new ApplicationResource($application);
+            $delayedJob = DelayedJob::create();
+            $job = new RunGetApplicationJob(
+                $delayedJob->id,
+                $application
+            );
+            dispatch($job);
+
+            return (new DelayedJobResource($delayedJob))->additional(['message' => 'Application is being processed']);
+        } catch (\Exception $e) {
+            Log::error('Error during application : ' . $e->getMessage());
+
+            return response()->json(['error' => 'An error occurred during application'], 500);
+        }
     }
 }
---
+++ app/Jobs/RunGetApplicationJob.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Models\DelayedJob;
+use App\Services\ApplicationService;
+use Exception;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Http\Response;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Log;
+use App\Models\V2\Forms\Application;
+
+class RunGetApplicationJob implements ShouldQueue
+{
+    use Dispatchable;
+    use InteractsWithQueue;
+    use Queueable;
+    use SerializesModels;
+
+    public $timeout = 0;
+
+    protected string $delayed_job_id;
+
+    protected Application $application;
+
+    public function __construct(string $delayed_job_id, Application $application)
+    {
+        $this->delayed_job_id = $delayed_job_id;
+        $this->application = $application;
+    }
+
+    public function handle(ApplicationService $service): void
+    {
+        try {
+            $delayedJob = DelayedJob::findOrFail($this->delayed_job_id);
+
+            $payload = $service->getApplicationPayload($this->application);
+
+            $delayedJob->update([
+                'status' => DelayedJob::STATUS_SUCCEEDED,
+                'payload' => ['data' => $payload],
+                'status_code' => Response::HTTP_OK,
+            ]);
+        } catch (Exception $e) {
+            Log::error('Error in RunGetApplicationJob: ' . $e->getMessage());
+
+            DelayedJob::where('id', $this->delayed_job_id)->update([
+                'status' => DelayedJob::STATUS_FAILED,
+                'payload' => json_encode(['error' => $e->getMessage()]),
+                'status_code' => Response::HTTP_INTERNAL_SERVER_ERROR,
+            ]);
+        }
+    }
+}
