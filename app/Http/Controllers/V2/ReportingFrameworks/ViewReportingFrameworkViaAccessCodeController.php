7cd6826c199bf2e01bd690ce6d45989859f3d073
Nathan Curtis <nathan.curtis@gmail.com>
2025-10-31 16:39:50 -0700

    [TM-2628] Return a 404 for missing frameworks.

--- app/Http/Controllers/V2/ReportingFrameworks/ViewReportingFrameworkViaAccessCodeController.php
+++ app/Http/Controllers/V2/ReportingFrameworks/ViewReportingFrameworkViaAccessCodeController.php
@@ -5,13 +5,15 @@ namespace App\Http\Controllers\V2\ReportingFrameworks;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\V2\ReportingFrameworks\ReportingFrameworkResource;
 use App\Models\Framework;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class ViewReportingFrameworkViaAccessCodeController extends Controller
 {
-    public function __invoke(Request $request, string $accessCode): ReportingFrameworkResource
+    public function __invoke(Request $request, string $accessCode)
     {
         $framework = Framework::where('access_code', $accessCode)->first();
+        if ($framework == null) return new JsonResponse('No reporting framework found', 404);
 
         return new ReportingFrameworkResource($framework);
     }
