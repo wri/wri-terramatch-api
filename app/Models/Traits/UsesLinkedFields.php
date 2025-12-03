39a752db3b0abc4ef2c5ae9b1cd89627f9a3ca04
Limber Mamani <154026979+LimberHope@users.noreply.github.com>
2025-12-03 07:33:30 -0400

    [TM-2635] add cleando conditional answers for workdays and treeSpecies (#1184)
    * [TM-2635] add cleando conditional answers for workdays and treeSpecies
    
    * [TM-2635] Refactor UsesLinkedFields trait to clean multiple conditional relation types
    
    ---------
    
    Co-authored-by: Marcolr-dev <marc.antoni.lr@gmail.com>

--- app/Models/Traits/UsesLinkedFields.php
+++ app/Models/Traits/UsesLinkedFields.php
@@ -8,6 +8,7 @@ use App\Models\V2\Forms\FormQuestion;
 use App\Models\V2\PolygonGeometry;
 use App\Models\V2\Projects\ProjectPolygon;
 use App\StateMachines\EntityStatusStateMachine;
+use Illuminate\Support\Facades\Log;
 
 trait UsesLinkedFields
 {
@@ -400,6 +401,31 @@ trait UsesLinkedFields
                 $property = data_get($fieldConfig, 'property');
                 if ($this->isPlainField($child->input_type) && ! empty($property)) {
                     $entityProps[$property] = null;
+                }
+
+                $relationInputTypes = [
+                    'workdays',
+                    'jobs',
+                    'employees',
+                    'volunteers',
+                    'allBeneficiaries',
+                    'trainingBeneficiaries',
+                    'indirectBeneficiaries',
+                    'treeSpecies',
+                    'disturbances',
+                    'seedings',
+                    'invasive',
+                ];
+
+                if (in_array($child->input_type, $relationInputTypes, true)) {
+                    $property = data_get($relationsConfig, "$child->linked_field_key.property");
+                    if (! empty($property)) {
+                        Log::info('Cleaning conditional relation data for property: ' . $property);
+                        $inputType = data_get($relationsConfig, "$child->linked_field_key.input_type");
+                        $hidden = ! empty($child->parent_id) && $child->show_on_parent_condition &&
+                            data_get($modelAnswers, $child->parent_id) === false;
+                        $this->syncRelation($property, $inputType, collect(null), $hidden, $this, true);
+                    }
                 }
             }
         }
