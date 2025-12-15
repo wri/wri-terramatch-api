22659e0fff26f3ff18cd68da304d18a232d73613
Nathan Curtis <nathan.curtis@gmail.com>
2025-12-15 15:29:06 -0800

    [TM-2655] Remove index / show for funding programmes in v2.

--- app/Http/Controllers/V2/FundingProgramme/AdminFundingProgrammeController.php
+++ app/Http/Controllers/V2/FundingProgramme/AdminFundingProgrammeController.php
@@ -6,37 +6,11 @@ use App\Helpers\I18nHelper;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\V2\StoreFundingProgrammeRequest;
 use App\Http\Requests\V2\UpdateFundingProgrammeRequest;
-use App\Http\Resources\V2\FundingProgrammes\FundingProgrammeCollection;
 use App\Http\Resources\V2\FundingProgrammes\FundingProgrammeResource;
 use App\Models\V2\FundingProgramme;
-use Illuminate\Http\JsonResponse;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\App;
-use Spatie\QueryBuilder\QueryBuilder;
 
 class AdminFundingProgrammeController extends Controller
 {
-    public function index(Request $request): FundingProgrammeCollection
-    {
-        $perPage = $request->query('per_page') ?? config('app.pagination_default', 15);
-
-        $query = FundingProgramme::query()->with('stages');
-
-        QueryBuilder::for($query)
-            ->allowedFilters([
-                'uuid',
-            ]);
-
-        $collection = $query->paginate($perPage)
-            ->appends(request()->query());
-
-        if ($request->query('lang')) {
-            App::setLocale($request->query('lang'));
-        }
-
-        return new FundingProgrammeCollection($collection);
-    }
-
     public function store(StoreFundingProgrammeRequest $storeFundingProgrammeRequest): FundingProgrammeResource
     {
         $fundingProgramme = FundingProgramme::create($storeFundingProgrammeRequest->validated());
@@ -45,11 +19,6 @@ class AdminFundingProgrammeController extends Controller
         $fundingProgramme->location_id = I18nHelper::generateI18nItem($fundingProgramme, 'location');
         $fundingProgramme->save();
 
-        return new FundingProgrammeResource($fundingProgramme);
-    }
-
-    public function show(FundingProgramme $fundingProgramme): FundingProgrammeResource
-    {
         return new FundingProgrammeResource($fundingProgramme);
     }
 
@@ -62,12 +31,5 @@ class AdminFundingProgrammeController extends Controller
         $fundingProgramme->save();
 
         return new FundingProgrammeResource($fundingProgramme);
-    }
-
-    public function destroy(FundingProgramme $fundingProgramme): JsonResponse
-    {
-        $fundingProgramme->delete();
-
-        return response()->json([], 202);
     }
 }
