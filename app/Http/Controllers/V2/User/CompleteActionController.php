82ec4b2d087b09343d7db5d7735a5ca4b609e077
Limber Mamani <154026979+LimberHope@users.noreply.github.com>
2026-01-08 10:31:15 -0400

    [TM-2773] Add a conditional for different organizations (#1213)

--- app/Http/Controllers/V2/User/CompleteActionController.php
+++ app/Http/Controllers/V2/User/CompleteActionController.php
@@ -5,11 +5,16 @@ namespace App\Http\Controllers\V2\User;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\V2\User\ActionResource;
 use App\Models\V2\Action;
+use Illuminate\Support\Facades\Auth;
 
 class CompleteActionController extends Controller
 {
     public function __invoke(Action $action): ActionResource
     {
+        $user = Auth::user();
+        if ($user->organisation->id != $action->organisation_id) {
+            return new ActionResource($action);
+        }
         $this->authorize('read', $action);
 
         $action->update([
