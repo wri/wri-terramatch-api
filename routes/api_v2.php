b99749b2421bcb68fe0a691a1528261d8e90dcd4
Nathan Curtis <nathan.curtis@gmail.com>
2026-04-29 14:55:59 -0700

    [TM-2896] Remove the last PHP API endpoint

--- routes/api_v2.php
+++ routes/api_v2.php
@@ -1,8 +1,5 @@
 <?php
 
-use App\Http\Controllers\V2\Applications\ExportApplicationController;
-use Illuminate\Support\Facades\Route;
-
 /*
 |--------------------------------------------------------------------------
 | API V2 Routes
@@ -13,6 +10,3 @@ use Illuminate\Support\Facades\Route;
 | is assigned the 'api' middleware group. Enjoy building your API!
 |
 */
-
-/** ADMIN ONLY ROUTES */
-Route::get('applications/{application}/export', ExportApplicationController::class);
